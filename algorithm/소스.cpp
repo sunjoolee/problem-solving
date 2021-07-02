499f25043ac976d32725091c18c0368ba7270558
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-02 17:22:20 +0900

    feat&refactor : Feature promising function & Refactor code to use bitmask
    But exceeded time limit

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
+#include <bitset>
 using namespace std;
 
 //64 bit (0 ~ 18,446,744,073,709,551,615)
 typedef unsigned long long ull;
 
 int n;
-ull a[41] = { 0 };
-ull s; // 0 ~ 2000000000
-int msg[41] = { 0 };
+int a[41] = { 0 };
+unsigned sum;	
 
-bool decrypt(int start, ull sum) {
-	//base case
-	if (s == sum) {
-		for (int j = start + 1; j <= n; ++j)
-			msg[j] = 0;
+//비트마스크 기법 이용
+//오른쪽에서 i번째 비트 = msg[i]
+ull msg = 0;
+
+bool promising(int start, unsigned nowSum) {
+	unsigned maxSum = nowSum;
+	
+	for (int i = start; i <= n; ++i) {
+		maxSum += a[i];
+		if (maxSum >= sum)
+			return true;
+	}
+
+	return false;
+}
+
+bool decrypt(int start, unsigned nowSum) {
+	//base case 
+	if (sum < nowSum)
+		return false;
+	if (sum == nowSum) {
+		//msg[start + 1] ~ msg[n] 0으로 set
+		msg &= ((1LL << (start + 1)) - 1);
 		return true;
 	}
-	if (s < sum || start > n)
+
+	//pruning
+	if (!promising(start, nowSum))
 		return false;
 
 	//recursive call
-	if (decrypt(start + 1, sum + a[start])) {
-		msg[start] = 1;
+	if (decrypt(start + 1, nowSum + a[start])) {
+		//msg[start] = 1
+		msg |= (1LL << start);
 		return true;
 	}
-	else if (decrypt(start + 1, sum)) {
-		msg[start] = 0;
+	else if (decrypt(start + 1, nowSum)) {
+		//msg[start] =0
+		msg &= ~(1LL << start);
 		return true;
 	}
 	return false;
@@ -39,12 +61,14 @@ int main() {
 	cin >> n;
 	for (int i = 1; i <= n; ++i)
 		cin >> a[i];
-	cin >> s;
+	cin >> sum;
 
 	decrypt(1, 0);
 
-	for (int i = 1; i <= n; ++i)
-		cout << msg[i];
+	for (int i = 1; i <= n; ++i){
+		msg /= 2;
+		cout << msg % 2;
+	}
 
 	return 0;
 }
