164b49e3b53104d87ac3d6ba0197ba7814315f2c
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-04 23:08:36 +0900

    fix : Fix exceed time limit problem
    In the previous version, I implemented a function "getMaxVolume(int nowVolume, int i")
    which returns the maximum volume possible
    when the starting volume is "nowVolume"
    and the index of the arry V[] starts from integer "i".
    In this new version, I implemented a function "getVolumeChange(int i, int sumOfVolumeChange)"
    which returns the maximum volume change possible
    when the sum of previous volume changes is "sumOfVolumeChange"
    and the index of the arry V[] starts from integer "i".
    
    Honestly, don't know why the exceeded time limit problem was solved
    because both version use the same cache size 100 * 1000.

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,20 +3,24 @@
 #include <algorithm>
 using namespace std;
 
+const int MIN = - 1001;
+
 int N, S, M;
 int V[100];
-int cache[1000][100];
+int cache[100][1000];
 
-int getMaxVolume(int nowVolume, int i) {
+//i번째 곡을 연주하기 전~마지막 곡을 연주할 때 까지 볼륨의 변화의 최대값
+int getVolumeChange(int i, int sumOfVolumeChange) {
 	//base case
-	if (nowVolume > M || nowVolume < 0) return -1;
-	if (i == N) return nowVolume;
+	if (sumOfVolumeChange > (M-S)) return MIN;
+	if (sumOfVolumeChange < (0-S)) return MIN;
+	if (i == N) return sumOfVolumeChange;
 
 	//메모이제이션
-	int& ret = cache[nowVolume][i];
+	int& ret = cache[i][sumOfVolumeChange];
 	if (ret != -1) return ret;
 
-	return ret = max(getMaxVolume(nowVolume + V[i], i+1), getMaxVolume(nowVolume - V[i], i + 1));
+	return ret = max(getVolumeChange(i+1, sumOfVolumeChange + V[i]), getVolumeChange(i + 1, sumOfVolumeChange - V[i]));
 }
 
 int main() {
@@ -24,13 +28,16 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	for(int i = 0; i<1000; ++i)
+	for(int i = 0; i<100; ++i)
 		memset(cache[i], -1, sizeof(cache[i]));
 	
 	cin >> N >> S >> M;
 	for (int i = 0; i < N; ++i)
 		cin >> V[i];
 
-	cout << getMaxVolume(S, 0);
+	int volumeChange = getVolumeChange(0, 0);
+
+	if (volumeChange == MIN) cout << -1;
+	else cout << S + volumeChange;
 	return 0;
 }
