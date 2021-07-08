bb56cbaa04da93aa70f480513d3651beff3fa495
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-08 10:10:46 +0900

    BOJ 18244 변형 계단 수

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -4,23 +4,23 @@
 
 using namespace std;
 
-int N =1;
-int cache[101][10][1024];
+const int MOD = 1000000007;
+int N;
+int cache[101][10][3][3];
 
-int getCnt(int len, int num, int bitmask) {
+int getCnt(int len, int num, int add, int sub) {
 	//base case
+	if (add >= 3 || sub >= 3) return 0;
 	if (num < 0 || num > 9) return 0;
-	if (len == N)
-		if(bitmask == 1023) return 1;
-		else return 0;
+	if (len == N) return 1;
 
-	int&ret = cache[len][num][bitmask];
+	int&ret = cache[len][num][add][sub];
 	if (ret != -1) return ret;
 
-	return ret = (getCnt(len + 1, num + 1, bitmask | (1 << (num+1))) + getCnt(len + 1, num - 1, bitmask | (1 << (num - 1)))) % 1000000000;
+	return ret = (getCnt(len + 1, num + 1, add + 1, 0) + getCnt(len + 1, num - 1, 0, sub + 1)) % MOD;
 }
 
-int main(void){
+int main(void) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
@@ -30,10 +30,11 @@ int main(void){
 	cin >> N;
 
 	int cnt = 0;
-	for (int num = 1; num <= 9; num++)
-		cnt = (cnt + getCnt(1, num, 1 << num)) % 1000000000;
+
+	//변형 계단 수는 0으로 시작할 수 있음
+	for (int num = 0; num <= 9; num++)
+		cnt = (cnt + getCnt(1, num, 0, 0)) % MOD;
 
 	cout << cnt;
-	
 	return 0;
 }
