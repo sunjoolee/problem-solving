d6901895d040880ae99f8f3abb7eb60e4fb1272d
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-08 23:28:40 +0900

    refactor : Refactor BOJ 2056 to use topology sort

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,63 +1,67 @@
-
 #include <iostream>
 #include <vector>
 #include <queue>
 using namespace std;
-const int MAXN = 10;
+
+const int MAXN = 101;
+
 int n;
-//inDegree[i]: 정점i에 들어가는 간선의 수
 int inDegree[MAXN] = { 0 };
-//vector<int> child[i]: 노드i를 선행 노드로 하는 노드들의 집합
+int time[MAXN] = { 0 };
 vector<int> child[MAXN];
-//위상 정렬 함수
-void topologySort() {
-	int result[MAXN];
+
+//dp[node]
+int dp[MAXN] = { 0 };
+
+int topologySort() {
+	//pair<node>
 	queue<int> q;
-	//진입 차수 0인 정점 큐에 삽입
+
 	for (int i = 1; i <= n; ++i)
-		if (inDegree[i] == 0) q.push(i);
-	//정렬이 완전히 수행되기까지 n개의 정점을 방문한다
-	for (int i = 1; i <= n; ++i) {
-		//n개의 정점을 방문하기 전에 큐가 비어버리면 
-		//사이클이 발생한 것이다
-		if (q.empty()) return;
-		//진입 차수가 0인 정점 선택
+		if (inDegree[i] == 0) {
+			dp[i] = time[i];
+			q.push(i);
+		}
+
+	for (int i = 1; i <= n; ++i ){
+		if (q.empty()) break;
+
 		int parent = q.front();
-		result[i] = parent;
-		//선택된 정점과 여기에 부속된 모든 간선들 삭제
 		q.pop();
+
 		for (int i = 0; i < child[parent].size(); ++i) {
 			int childnode = child[parent][i];
-			//새롭게 진입차수가 0이 된 정점을 큐에 삽입
-			if (--inDegree[childnode] == 0)
+
+			dp[childnode] = max(dp[childnode], dp[parent] + time[childnode]);
+
+			if (--inDegree[childnode] == 0) {
 				q.push(childnode);
+			}
 		}
 	}
-	//위상 정렬 결과 출력
+
+	int res = 0;
 	for (int i = 1; i <= n; ++i)
-		cout << result[i] << " ";
-	return;
+		res = max(res, dp[i]);
+
+	return res;
 }
-int main() {
-	//정점의 수
-	n = 7;
-	//간선 추가
-	//child[parent].push_back(child);
-	//inDegree[child]++;
-	child[1].push_back(2);
-	inDegree[2]++;
-	child[1].push_back(5);
-	inDegree[5]++;
-	child[2].push_back(3);
-	inDegree[3]++;
-	child[3].push_back(4);
-	inDegree[4]++;
-	child[4].push_back(6);
-	inDegree[6]++;
-	child[5].push_back(6);
-	inDegree[6]++;
-	child[6].push_back(7);
-	inDegree[7]++;
-	topologySort();
+
+
+int main(){
+
+	cin >> n;
+
+	for (int i = 1; i <= n; ++i) {
+		cin >> time[i];
+		cin >> inDegree[i];
+		for (int j = 0; j < inDegree[i]; ++j) {
+			int parent;
+			cin >> parent;
+			child[parent].push_back(i);
+		}
+	}
+
+	cout << topologySort();
 	return 0;
 }
