727675c7cd5d3182001a50ac30c2776b06fe53b1
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-12 23:59:50 +0900

    BOJ 2003 수들의 합 2
    Solution w/o using two pointer algorithm.

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -2,45 +2,39 @@
 using namespace std;
 
 typedef unsigned long int ul;
-const int MAXN = 100000 + 1;
+const int MAXN = 10000 + 1;
 
 int N;
-ul S;
-int num[MAXN];
+ul M;
+int A[MAXN];
 ul psum[MAXN];
 
 void getpsum() {
-	psum[0] = num[0];
-	for (int i = 1; i < N; ++i)
-		psum[i] = psum[i - 1] + num[i];
+	psum[0] = 0;
+	for (int i = 1; i <= N; ++i)
+		psum[i] = psum[i - 1] + A[i];
 	return;
 }
 
-int getlen() {
-	if (psum[N-1] < S) return 0;
-
-	for (int len = 1; len <= N; ++len) {
-		//start = 0
-		if (psum[len - 1] > S) return len;
-		//start = 1 ~
-		for (int start = 1; start + len - 1 < N; ++start)
-			if (psum[start + len - 1] - psum[start-1] > S)
-				return len;
-	}
-
+ul getCnt() {
+	ul ret = 0;
+	for (int i = 1; i <= N; ++i)
+		for (int j = i; j <= N; ++j)
+			if (psum[j] - psum[i - 1] == M)
+				++ret;
+	return ret;
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> S;
-	for (int i = 0; i < N; ++i)
-		cin >> num[i];
+	cin >> N >> M;
+	for (int i = 1; i <= N; ++i)
+		cin >> A[i];
 
 	getpsum();
-	cout << getlen();
+	cout << getCnt();
 	return 0;
 }
