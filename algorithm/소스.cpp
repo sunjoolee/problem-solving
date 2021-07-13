cf64dbf9e12d6c5284305658de3b52959e8838bf
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-13 14:12:03 +0900

    BOJ 1208 부분 수열의 합 2
    Solution using meet in the middle algorithm

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,70 +1,47 @@
-#include<iostream>
-#include <algorithm>
-#include <math.h>
+#include <iostream>
+#include <map>
 using namespace std;
 
-typedef long long int ll;
-const int MAXN = 5000;
-const ll MAXSUM = 1000000000 * MAXN * 3;
+const int MAXN = 40;
 
-int N;
-int A[MAXN + 1] = { 0 };
+int n, s;
+long long cnt = 0;
+int num[MAXN + 1]; 
+map<int, int> mp;
+
+void dfsLeft(int index, int sum) {
+	if (index == n/2) {
+		++mp[sum];
+		return;
+	}
+	dfsLeft(index + 1, sum);
+	dfsLeft(index + 1, sum + num[index]);
+	return;
+}
+
+void dfsRight(int index, int sum) {
+	if (index >= n) {
+		cnt += mp[s-sum];
+		return;
+	}
+	dfsRight(index + 1, sum);
+	dfsRight(index + 1, sum + num[index]);
+	return;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
-	for (int i = 0; i < N; i++)
-		cin >> A[i];
+	cin >> n >> s;
+	for (int i = 0; i < n; ++i)
+		cin >> num[i];
 
-	sort(A, A+N);
+	dfsLeft(0, 0);
+	dfsRight(n/2, 0);
 
-	//산성 용액으로만 주어지는 경우
-	if ((A[0] >= 0) && (A[N - 1] >= 0)) {
-		cout << A[0] << " " << A[1] << " " << A[2];
-		return 0;
-	}
-	//염기성 용액으로만 주어지는 경우
-	if ((A[0] < 0) && (A[N - 1] < 0)) {
-		cout << A[N - 3] <<" "<< A[N - 2] << " " << A[N - 1];
-		return 0;
-	}
-
-	long long minsum = MAXSUM;
-	int minleft, minright, minmid;
-	minleft = minright = minmid = N - 1;
-
-	for (int mid = 1; mid < N - 1; mid++) {
-
-		long long sum;
-		int left = left = mid - 1;
-		int right = mid + 1;
-
-		while (left < mid && mid < right) {
-			sum = A[left] + A[mid] + A[right];
-			if (minsum > abs(sum)) {
-				minsum = abs(sum);
-				minleft = left;
-				minmid = mid;
-				minright = right;
-			}
-
-			if (sum == 0) break;
-			if (sum > 0) {
-				if (left == 0) break;
-				left--;
-			}
-			else {
-				if (right == N - 1) break;
-				right++;
-			}
-		}
-	}
-
-	cout << A[minleft] << " " << A[minmid]<<" "<< A[minright];
+	if (s == 0) cnt--;
+	cout << cnt;
 	return 0;
 }
-
-
