cf1d92826a0de51a2d0096f5b9a54ce370f78e82
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-13 14:47:55 +0900

    BOJ 17243 Almost-K Increasing Subsequence

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,70 +1,41 @@
-#include<iostream>
+#include <iostream>
+#include <cstring>
 #include <algorithm>
-#include <math.h>
 using namespace std;
 
-typedef long long int ll;
-const int MAXN = 5000;
-const ll MAXSUM = 1000000000 * MAXN * 3;
+const int MAXN = 500 + 1;
 
-int N;
-int A[MAXN + 1] = { 0 };
+int N, K;
+int a[MAXN];
+int cache[MAXN][MAXN];
+
+int getMaxLen(int index, int k) {
+	if (index >= N || k > K) return 0;
+
+	int& ret = cache[index][k];
+	if (ret != -1) return ret;
+
+	ret = 1;
+	for (int next = index + 1; next < N; ++next) {
+		if (a[index] <= a[next])
+			ret = max(ret, 1 + getMaxLen(next, k));
+		else if (a[index] > a[next])
+			ret = max(ret, 1 + getMaxLen(next, k+1));
+	}
+	return ret;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
-	for (int i = 0; i < N; i++)
-		cin >> A[i];
+	memset(cache, -1, sizeof(cache));
 
-	sort(A, A+N);
-
-	//산성 용액으로만 주어지는 경우
-	if ((A[0] >= 0) && (A[N - 1] >= 0)) {
-		cout << A[0] << " " << A[1] << " " << A[2];
-		return 0;
-	}
-	//염기성 용액으로만 주어지는 경우
-	if ((A[0] < 0) && (A[N - 1] < 0)) {
-		cout << A[N - 3] <<" "<< A[N - 2] << " " << A[N - 1];
-		return 0;
-	}
-
-	long long minsum = MAXSUM;
-	int minleft, minright, minmid;
-	minleft = minright = minmid = N - 1;
-
-	for (int mid = 1; mid < N - 1; mid++) {
-
-		long long sum;
-		int left = left = mid - 1;
-		int right = mid + 1;
-
-		while (left < mid && mid < right) {
-			sum = A[left] + A[mid] + A[right];
-			if (minsum > abs(sum)) {
-				minsum = abs(sum);
-				minleft = left;
-				minmid = mid;
-				minright = right;
-			}
-
-			if (sum == 0) break;
-			if (sum > 0) {
-				if (left == 0) break;
-				left--;
-			}
-			else {
-				if (right == N - 1) break;
-				right++;
-			}
-		}
-	}
-
-	cout << A[minleft] << " " << A[minmid]<<" "<< A[minright];
+	cin >> N >> K;
+	for (int i = 0; i < N; ++i)
+		cin >> a[i];
+	
+	cout << getMaxLen(0, 0);
 	return 0;
 }
-
-
