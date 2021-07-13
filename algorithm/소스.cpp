bc43d59bf3eafa5731e353962319e52d7e947eff
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-13 09:22:59 +0900

    BOJ 1644 소수의 연속합

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 typedef unsigned long int ul;
-const int MAXN = 10000 + 1;
+const ul MAXN = 4000000;
 
 int N;
-ul M;
-int A[MAXN] = { 0 };
+vector <int> prime;
+int isPrime[MAXN + 1];
 
+//에라토스테네스의 체
+void getPrime(int N) {
+	memset(isPrime, 1, sizeof(isPrime));
+	isPrime[0] = isPrime[1] = 0;
 
-ul getCnt() {
-	ul ret = 0;
+	for (int i = 2; i <= sqrt(N); i++) {
+		if (isPrime[i] == 0) continue;
+
+		prime.push_back(i);
+		for (int j = i * i; j <= N; j += i)
+			isPrime[j] = 0;
+	}
+
+	for (int i = sqrt(N)+1; i <= N; i++)
+		if (isPrime[i])
+			prime.push_back(i);
+	return;
+}
+
+int getCnt() {
+	int ret = 0;
 	int start = 0;
 	int end = 0;
 	ul psum = 0;
 
-	while (end <= N) {
-		if (psum >= M)
-			psum -= A[start++];
-		else if (psum < M)
-			psum += A[end++];
+	prime.push_back(0);
+	while (end < prime.size()) {
+		if (psum >= N)
+			psum -= prime[start++];
 
-		if (psum == M)
+		else if (psum < N)
+			psum += prime[end++];
+
+		if (psum == N)
 			ret++;
 	}
-
 	return ret;
 }
 
@@ -33,10 +53,9 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> M;
-	for (int i = 0; i < N; ++i)
-		cin >> A[i];
-
+	cin >> N;
+	getPrime(N);
 	cout << getCnt();
+
 	return 0;
 }
