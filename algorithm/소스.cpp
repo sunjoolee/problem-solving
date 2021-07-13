d4f589a8a3cdbcbf21b305a1af7b8f8283f8278b
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-13 15:28:35 +0900

    BOJ 17351 3루수는 몰라

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,25 +3,25 @@
 #include <algorithm>
 using namespace std;
 
-const int MAXN = 500 + 1;
+// M : 0, O : 1, L : 2, A : 3, ±×¿Ü ¾ËÆÄºª: 4
 
-int N, K;
-int a[MAXN];
-int cache[MAXN][MAXN];
+int N;
+int grnd[501][501];
+int cache[501][501][5][5][5];
 
-int getMaxLen(int index, int k) {
-	if (index >= N || k > K) return 0;
+int cntMOLA(int r, int c, int prev3, int prev2, int prev1) {
+	if (r < 1 || r > N) return 0;
+	if (c < 1 || c > N) return 0;
 
-	int& ret = cache[index][k];
+	int& ret = cache[r][c][prev3][prev2][prev1];
 	if (ret != -1) return ret;
 
-	ret = 1;
-	for (int next = index + 1; next < N; ++next) {
-		if (a[index] <= a[next])
-			ret = max(ret, 1 + getMaxLen(next, k));
-		else if (a[index] > a[next])
-			ret = max(ret, 1 + getMaxLen(next, k+1));
-	}
+	ret = 0;
+
+	int now = grnd[r][c];
+	if (prev3 == 0 && prev2 == 1 && prev1 == 2 && now == 3) ++ret;
+
+	ret += max(cntMOLA(r, c + 1, prev2, prev1, now), cntMOLA(r + 1, c, prev2, prev1, now));
 	return ret;
 }
 
@@ -32,10 +32,19 @@ int main() {
 
 	memset(cache, -1, sizeof(cache));
 
-	cin >> N >> K;
-	for (int i = 0; i < N; ++i)
-		cin >> a[i];
-	
-	cout << getMaxLen(0, 0);
+	cin >> N;
+	char input;
+	for (int i = 1; i <= N; ++i)
+		for (int j = 1; j <= N; ++j) {
+			cin >> input;
+			if (input == 'M') grnd[i][j] = 0;
+			else if (input == 'O') grnd[i][j] = 1;
+			else if (input == 'L') grnd[i][j] = 2;
+			else if (input == 'A') grnd[i][j] = 3;
+			else grnd[i][j] = 4;
+		}
+
+	cout << cntMOLA(1, 1, 4, 4, 4);
+
 	return 0;
 }
