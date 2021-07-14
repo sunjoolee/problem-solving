c1411f2c5a19beb497f685506d5da16b71ee3117
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-14 09:28:58 +0900

    feat & refactor : Feature iteration to get canWin[]
    The recursive function canWin() in the previous version
    may be the reason of time delay.
    So as an alternative, featured a interative loop to get canWin[].
    refactered code to minimize number of function calls.
    
    +) The previous version of the code was also the correct answer in BOJ system.
    The cause of time delay may have been on my laptop speed.

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -7,96 +6,66 @@
 using namespace std;
 
 const int MAXN = 100000;
-int cache[MAXN + 1];
-int sum[MAXN + 1];
+int canWin[MAXN + 1] = { 0 };
+int sum[MAXN + 1] = { 0 };
 int isPrime[MAXN + 1];
 vector<int> prime;
-
-//에라토스테네스의 체
-void setIsPrime() {
-	memset(isPrime, 1, sizeof(isPrime));
-	isPrime[0] = isPrime[1] = 0;
-
-	for (int i = 2; i <= sqrt(MAXN); i++) {
-		if (isPrime[i] == 0) continue;
-
-		for (int j = i * i; j <= MAXN; j += i)
-			isPrime[j] = 0;
-	}
-	return;
-}
-
-void getPrime() {
-	for (int i = 2; i <= MAXN; ++i)
-		if (isPrime[i]) 
-			prime.push_back(i);
-	return;
-}
-
-//현재 종이에 적힌 숫자가 N일 때 
-//현재 차례가 이길 수 있으면 1, 지면 0 반환
-int canWin(int N) {
-	if (N == 0 || N == 1) return 0;
-	if (isPrime[N]) return 1;
-
-	int& ret = cache[N];
-	if (ret != -1) return ret;
-
-	ret = 0;
-	for (int i = 0; i < prime.size(); ++i) {
-		if (prime[i] > N) break;
-		//다음 차례에서 지는 경우에 현재 차례에서 이길 수 있다
-		if (!canWin(N - prime[i])) {
-			ret = 1;
-			break;
-		}
-	}
-	return ret;
-}
-
-void getSum() {
-	memset(sum, 0, sizeof(sum));
-
-	for (int N = 2; N <= MAXN; ++N)
-		sum[N] = sum[N - 1] + !canWin(N);
-
-	return;
-}
-
-void getX(int A, int k) {
-	int bestX = 0;
-	int bestwin = 0;
-
-	for (int X = A + 1 - k; X >= 2; --X) {
-		int win = sum[X + k - 1] - sum[X - 1];
-
-		if (bestwin <= win) {
-			bestwin = win;
-			bestX = X;
-		}
-	}
-
-	cout << bestwin << " " << bestX << "\n";
-	return;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	memset(cache, -1, sizeof(cache));
+	//에라토스테네스의 체
+	memset(isPrime, 1, sizeof(isPrime));
+	isPrime[0] = isPrime[1] = 0;
+	for (int i = 2; i <= sqrt(MAXN); i++) {
+		if (isPrime[i] == 0) continue;
 
-	setIsPrime();
-	getPrime();
-	getSum();
+		for (int j = i * i; j <= MAXN; j += i)
+			isPrime[j] = 0;
+	}
+
+	//소수 벡터에 저장
+	for (int i = 2; i <= MAXN; ++i)
+		if (isPrime[i])
+			prime.push_back(i);
+
+	//종이에 적힌 수가 N일 때 이길 수 있는지 저장
+	int psize = prime.size();
+	for (int i = 0; i <= MAXN; i++) {
+		if (canWin[i])
+			continue;
+		for (int j = 0; j < psize; j++)
+		{
+			if (i + prime[j] > MAXN)
+				break;
+			canWin[i + prime[j]] = 1;
+		}
+	}
+
+	//승리하는 경우의 수 부분합 계산
+	for (int N = 2; N <= MAXN; ++N)
+		sum[N] = sum[N - 1] + !canWin[N];
+
 
 	int T;
 	cin >> T;
 	while (T--) {
 		int A, k;
 		cin >> A >> k;
-		getX(A, k);
+
+		int bestX = 0;
+		int bestwin = 0;
+
+		for (int X = A + 1 - k; X >= 2; --X) {
+			int win = sum[X + k - 1] - sum[X - 1];
+			if (bestwin <= win) {
+				bestwin = win;
+				bestX = X;
+			}
+		}
+		cout << bestwin << " " << bestX << "\n";
 	}
 
 	return 0;
