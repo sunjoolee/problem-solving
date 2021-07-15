124405681baf028f5227d24e0238af046d658ab1
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-15 10:44:36 +0900

    BOJ 1520 내리막길

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -5,29 +5,31 @@
 #include <algorithm>
 using namespace std;
 
-const int MAXN = 500;
+const int MAXSIZE = 500;
 
-int n;
-int cache[MAXN + 1][MAXN + 1];
-int mp[MAXN + 1][MAXN + 1];
+int n,m;
+//cache[r][c]: (r,c)의 좌표에서 시작하여 (n,m)까지 갈 수 있는 내리막길 수
+int cache[MAXSIZE + 1][MAXSIZE + 1];
+int mp[MAXSIZE + 1][MAXSIZE + 1];
 
 int dirR[4] = { -1,0,0,1 };
 int dirC[4] = { 0,-1,1,0 };
 
 int dp(int r, int c) {
+	if (r == n && c == m) return 1;
+
 	int& ret = cache[r][c];
 	if (ret != -1) return ret;
 
-	ret = 1;
+	ret = 0;
 	for (int i = 0; i < 4; i++) {
 		int nextR = r + dirR[i];
 		int nextC = c + dirC[i];
 
 		if (nextR < 1 || nextR > n) continue;
-		if (nextC < 1 || nextC > n) continue;
-		if (mp[r][c] < mp[nextR][nextC])
-			ret = max(ret,1 + dp(nextR, nextC));
-		
+		if (nextC < 1 || nextC > m) continue;
+		if (mp[r][c] > mp[nextR][nextC])
+			ret += dp(nextR, nextC);
 	}
 	return ret;
 }
@@ -39,20 +41,11 @@ int main() {
 
 	memset(cache, -1, sizeof(cache));
 
-	cin >> n;
+	cin >> n >> m;
 	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= n; ++j)
+		for (int j = 1; j <= m; ++j)
 			cin >> mp[i][j];
 
-	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= n; ++j)
-			dp(i,j);
-
-	int ans = 0;
-	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= n; ++j)
-			ans = max(ans, cache[i][j]);
-
-	cout << ans;
+	cout << dp(1, 1);
 	return 0;
 }
