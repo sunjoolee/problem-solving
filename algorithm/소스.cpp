2ef2749af45f675aac73c8b1496cdcba9f715ffd
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-15 10:34:50 +0900

    feat : Feature dp function
    This problem was not a shortest path problem, but a longest path problem.
    So the solution to this problem is to use DFS and DP algorithm.

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -8,43 +8,28 @@ using namespace std;
 const int MAXN = 500;
 
 int n;
+int cache[MAXN + 1][MAXN + 1];
 int mp[MAXN + 1][MAXN + 1];
-int isVisited[MAXN + 1][MAXN + 1];
-//pair<pair<r, c>, dist>
-queue <pair<pair<int, int>, int>> q;
 
 int dirR[4] = { -1,0,0,1 };
 int dirC[4] = { 0,-1,1,0 };
 
-int bfs(int startR, int startC) {
-	q.push(make_pair(make_pair(startR, startC), 1));
+int dp(int r, int c) {
+	int& ret = cache[r][c];
+	if (ret != -1) return ret;
 
-	int maxDist = 0;
-	while (!q.empty()) {
-		pair<pair<int, int>, int> cur = q.front();
-		q.pop();
+	ret = 1;
+	for (int i = 0; i < 4; i++) {
+		int nextR = r + dirR[i];
+		int nextC = c + dirC[i];
 
-		int curR = cur.first.first;
-		int curC = cur.first.second;
-		int curDist = cur.second;
-
-		if (isVisited[curR][curC])continue;
-		isVisited[curR][curC] = true;
-
-		maxDist = max(maxDist, curDist);
-
-		for (int i = 0; i < 4; i++) {
-			int nextR = curR + dirR[i];
-			int nextC = curC + dirC[i];
-
-			if (nextR < 1 || nextR > n) continue;
-			if (nextC < 1 || nextC > n) continue;
-			if (mp[curR][curC] >= mp[nextR][nextC]) continue;
-
-			q.push(make_pair(make_pair(nextR, nextC), curDist + 1));
-		}
+		if (nextR < 1 || nextR > n) continue;
+		if (nextC < 1 || nextC > n) continue;
+		if (mp[r][c] < mp[nextR][nextC])
+			ret = max(ret,1 + dp(nextR, nextC));
+		
 	}
-	return maxDist;
+	return ret;
 }
 
 int main() {
@@ -52,17 +37,21 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	memset(cache, -1, sizeof(cache));
+
 	cin >> n;
 	for (int i = 1; i <= n; ++i)
 		for (int j = 1; j <= n; ++j)
 			cin >> mp[i][j];
 
+	for (int i = 1; i <= n; ++i)
+		for (int j = 1; j <= n; ++j)
+			dp(i,j);
+
 	int ans = 0;
 	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= n; ++j) {
-			memset(isVisited, 0, sizeof(isVisited));
-			ans = max(ans, bfs(i, j));
-		}
+		for (int j = 1; j <= n; ++j)
+			ans = max(ans, cache[i][j]);
 
 	cout << ans;
 	return 0;
