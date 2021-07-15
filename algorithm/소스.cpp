5764e7ad4c0dc0c0c57633c90c77d6e86bcc21d9
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-15 10:53:28 +0900

    BOJ 1890 점프

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -5,32 +5,27 @@
 #include <algorithm>
 using namespace std;
 
-const int MAXSIZE = 500;
+typedef unsigned long long ull;
+const int MAXSIZE = 100;
 
-int n,m;
-//cache[r][c]: (r,c)의 좌표에서 시작하여 (n,m)까지 갈 수 있는 내리막길 수
-int cache[MAXSIZE + 1][MAXSIZE + 1];
+int n;
+//cache[r][c]: (r,c)의 좌표에서 시작하여 (n,n)까지 갈 수 있는 경우의 수
+ull cache[MAXSIZE + 1][MAXSIZE + 1];
 int mp[MAXSIZE + 1][MAXSIZE + 1];
 
-int dirR[4] = { -1,0,0,1 };
-int dirC[4] = { 0,-1,1,0 };
+ull dp(int r, int c) {
+	if (r == n && c == n) return 1;
 
-int dp(int r, int c) {
-	if (r == n && c == m) return 1;
-
-	int& ret = cache[r][c];
+	ull& ret = cache[r][c];
 	if (ret != -1) return ret;
 
 	ret = 0;
-	for (int i = 0; i < 4; i++) {
-		int nextR = r + dirR[i];
-		int nextC = c + dirC[i];
-
-		if (nextR < 1 || nextR > n) continue;
-		if (nextC < 1 || nextC > m) continue;
-		if (mp[r][c] > mp[nextR][nextC])
-			ret += dp(nextR, nextC);
-	}
+	//오른쪽으로 점프
+	if (c + mp[r][c] <= n)
+		ret += dp(r, c + mp[r][c]);
+	//아래로 점프
+	if (r + mp[r][c] <= n)
+		ret += dp(r + mp[r][c],c);
 	return ret;
 }
 
@@ -41,9 +36,9 @@ int main() {
 
 	memset(cache, -1, sizeof(cache));
 
-	cin >> n >> m;
+	cin >> n ;
 	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= m; ++j)
+		for (int j = 1; j <= n; ++j)
 			cin >> mp[i][j];
 
 	cout << dp(1, 1);
