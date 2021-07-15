23004c3356822a059a50b4e6fb35edc06c17f5ac
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-16 07:31:51 +0900

    BOJ 2352 반도체 설계
    A simple LIS length proble but exceeds time limit with  DP solution.
    Needed to use a binary search approach (lower_bound) to reduce time to O(nlogn).

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,61 +1,34 @@
 #include <iostream>
-#include <cstring>
+#include <limits.h>
 #include <algorithm>
+#include <vector>
 using namespace std;
-
-const int MAXN = 300;
-const int MAXM = 20;
-
-int n, m;
-
-//[company][money]
-int profit[MAXM + 1][MAXN + 1];
-int cache[MAXM + 1][MAXN + 1];
-int bestChoice[MAXM + 1][MAXN + 1] = { 0 };
-
-int getMaxProfit(int company, int money) {
-	if (company > m || money == 0) return 0;
-
-	int& ret = cache[company][money];
-	if (ret != -1) return ret;
-
-	int choice = 0;
-	ret = getMaxProfit(company + 1, money);
-
-	for (int i = 1; i <= money; ++i) {
-		int cand = profit[company][i] + getMaxProfit(company + 1, money - i);
-		if (ret < cand) {
-			ret = cand;
-			choice = i;
-		}
-	}
-
-	bestChoice[company][money] = choice;
-	return ret;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	memset(cache, -1, sizeof(cache));
+	int n;
+	cin >> n;
 
-	cin >> n >> m;
-	for (int i = 1; i <= n; ++i) {
-		int money;
-		cin >> money;
-		for (int j = 1; j <= m; ++j)
-			cin >> profit[j][i];
+	int lis = 0;
+	vector <int> vt;
+	vt.push_back(INT_MIN);
+
+	for (int i = 0; i < n; i++) {
+		int A;
+		cin >> A;
+		if (vt.back() < A) {
+			vt.push_back(A);
+			lis++;
+		}
+		else {
+			auto it = lower_bound(vt.begin(), vt.end(), A);
+			*it = A;
+		}
 	}
 
-	cout << getMaxProfit(1, n) << "\n";
-
-	int sumOfChoice = 0;
-	for (int i = 1; i <= m; ++i) {
-		int choice = bestChoice[i][n - sumOfChoice];
-		cout << choice << " ";
-		sumOfChoice += choice;
-	}
+	cout << lis;
 	return 0;
 }
