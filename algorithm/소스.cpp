60449432bc464073f37f43afd0cf5f726ef9dc33
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-20 10:48:38 +0900

    BOJ 5397 키로거

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,34 +1,62 @@
 #include <iostream>
-#include <limits.h>
-#include <algorithm>
-#include <vector>
+#include <string>
+#include <deque>
 using namespace std;
+
+deque <char> leftDQ;
+deque <char> rightDQ;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	cin >> n;
+	int T;
+	cin >> T;
+	while (T--) {
 
-	int lis = 0;
-	vector <int> vt;
-	vt.push_back(INT_MIN);
+		string input;
+		cin >> input;
 
-	for (int i = 0; i < n; i++) {
-		int A;
-		cin >> A;
-		if (vt.back() < A) {
-			vt.push_back(A);
-			lis++;
+		for (int i = 0; input[i] != NULL; ++i) {
+			char x = input[i];
+
+			//커서 왼쪽으로
+			if (x == '<') {
+				if (leftDQ.empty()) continue;
+
+				char tmp = leftDQ.back();
+				leftDQ.pop_back();
+				rightDQ.push_front(tmp);
+			}
+			//커서 오른쪽으로
+			else if (x == '>') {
+				if (rightDQ.empty()) continue;
+
+				char tmp = rightDQ.front();
+				rightDQ.pop_front();
+				leftDQ.push_back(tmp);
+			}
+			//커서 왼쪽 문자 삭제
+			else if (x == '-') {
+				if (leftDQ.empty()) continue;
+				leftDQ.pop_back();
+			}
+			//커서 왼쪽 문자 추가
+			else {
+				leftDQ.push_back(x);
+			}
 		}
-		else {
-			auto it = lower_bound(vt.begin(), vt.end(), A);
-			*it = A;
-		}
+
+		for (auto it = leftDQ.begin(); it != leftDQ.end(); ++it)
+			cout << *it;
+		for (auto it = rightDQ.begin(); it != rightDQ.end(); ++it)
+			cout << *it;
+		cout << endl;
+
+		leftDQ.clear();
+		rightDQ.clear();
 	}
 
-	cout << lis;
 	return 0;
 }
