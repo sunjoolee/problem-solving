4d82e522da532a1f0dd951ccb90383b162420b33
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-20 14:39:07 +0900

    BOJ 3078 좋은 친구
    Solution w/o using sliding window technique.
    Exeeded time limit,

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,82 +1,7 @@
 #include <iostream>
-#include <queue>
-#include <utility>
+#include <vector>
+#include <string>
 using namespace std;
-
-const int MAXN = 100;
-
-int n;
-int time= 0;
-
-//빈자리 0, 뱀 1, 사과 2
-int board[MAXN+2][MAXN+2] = { 0 };
-
-//뱀이 차지하고 있는 좌표 
-queue <pair<int, int>> snake; 
-
-//<게임 시작 시간으로부터 x초, c방향으로 회전>
-queue <pair<int, char>> moves;
-
-//이동 방향
-int Rdir[4] = {0, 1, 0, -1};
-int Cdir[4] = {1, 0, -1, 0};
-
-int game() {
-	//<r, c>: 현재 뱀의 머리의 좌표
-	int r = 1;
-	int c = 1;
-	int dir = 0;
-	
-	while (true) {
-		time++;
-		r += Rdir[dir];
-		c += Cdir[dir];
-
-		if (r < 1 || r > n) break;
-		if (c < 1 || c > n) break;
-
-		if (board[r][c] == 1) 
-			break;
-		
-		else if (board[r][c] == 2) {
-			//머리 늘리기
-			board[r][c] = 1;
-			snake.push(make_pair(r, c));
-		}
-
-		else{
-			//머리 늘리기
-			board[r][c] = 1;
-			snake.push(make_pair(r, c));
-			//꼬리 줄이기
-			pair<int, int> tail = snake.front();
-			board[tail.first][tail.second] = 0;
-			snake.pop();
-		}
-
-		
-		if (!moves.empty()) {
-			pair<int, char> nextMove = moves.front();
-			
-			if (nextMove.first == time) {
-				//반시계방향으로 회전
-				if (nextMove.second == 'L') {
-					if (dir == 0) dir = 3;
-					else --dir;
-				}
-				//시계방향으로 회전
-				else {
-					if (dir == 3) dir = 0;
-					else ++dir;
-				}
-				moves.pop();
-			}
-		}
-
-	}
-
-	return time;
-}
 
 
 int main() {
@@ -84,30 +9,26 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> n;
 
-	//사과
-	int k;
-	cin >> k;
-	for (int i = 0; i < k; ++i) {
-		int r, c;
-		cin >> r >> c;
-		board[r][c] = 2;
+	int n, k;
+	cin >> n >> k;
+
+	//이름 길이
+	vector <int> name;
+
+	string input;
+	for (int i = 0; i < n; ++i) {
+		cin >> input;
+		name.push_back(input.size());
 	}
-
-	//이동 경로 
-	int m;
-	cin >> m;
-	for (int i = 0; i < m; ++i) {
-		int x; 
-		char c;
-		cin >> x >> c;
-		moves.push(make_pair(x,c));
-	}
-
-	//게임 시작
-	board[1][1] = 1;
-	snake.push(make_pair(1,1));
-	cout << game();
+	
+	//좋은 친구 쌍
+	int cnt = 0;
+	for (int i = 0; i < n; ++i)
+		for (int j = 1; j <= k && i + j < n; j++)
+			if (name[i] == name[i + j])
+				cnt++;
+	
+	cout << cnt;
 	return 0;
 }
