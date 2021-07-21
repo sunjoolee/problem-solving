f96e7e3c206d1bca979276b272a44b1f4b718f7c
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-21 10:08:58 +0900

    BOJ 1181 단어 정렬

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,83 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <queue>
+#include <string>
 #include <algorithm>
 using namespace std;
 
-
-const int MAXN = 500;
-int n;
-
-//건물을 짓는데 걸리는 시간
-int buildTime[MAXN + 1] = { 0 };
-//건물이 완성된 시간
-int finishTime[MAXN];
-
-int inDegree[MAXN+1] = { 0 };
-//vector<int> child[i]: 건물i를 선행으로 하는 건물들의 집합
-vector<int> child[MAXN+1];
-//vector<int> parent[i]: 건물i가 선행해야 하는 건물들의 집합
-vector<int> parent[MAXN + 1];
-
-void topologySort() {
-	int res = 0;
-
-	queue<int> q;
-
-	for (int i = 1; i <= n; ++i) {
-		if (inDegree[i] == 0) q.push(i);
-		finishTime[i] = buildTime[i];
-	}
-
-	for (int i = 1; i <= n; ++i) {
-		//n개의 정점을 방문하기 전에 큐가 비어버리면 사이클이 발생한 것
-		if (q.empty()) return;
-
-		int parentnode = q.front();
-		q.pop();
-
-		for (int i = 0; i < child[parentnode].size(); ++i) {
-			int childnode = child[parentnode][i];
-			
-			if (--inDegree[childnode] == 0) {
-				q.push(childnode);
-
-				int maxTime = 0;
-				for (auto it = parent[childnode].begin(); it != parent[childnode].end(); ++it)
-					maxTime = max(maxTime, finishTime[*it]);
-				finishTime[childnode] = maxTime + buildTime[childnode];
-			}
-		}
-	}
-
-	return;
+bool compare(string a, string b) {
+	//길이 짧은 순으로 정렬
+	if (a.size() < b.size()) return true;
+	//길이 같다면 사전 순으로 정렬
+	else if (a.size() == b.size())
+		return a < b;
+	else return false;
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	string words[20001];
+	
+	int n;
 	cin >> n;
-	for (int i = 1; i <= n; ++i) {
-		cin >> buildTime[i];
+	for (int i = 0; i < n; ++i)
+		cin >> words[i];
 
-		while (true) {
-			int parentnode;
-			cin >> parentnode;
-			if (parentnode == -1) break;
+	sort(words, words + n, compare);
 
-			child[parentnode].push_back(i);
-			parent[i].push_back(parentnode);
-			inDegree[i]++;
+	for (int i = 0; i < n; ++i) {
+		cout << words[i]<<"\n";
+		//중복된 단어 제외 출력
+		while (i < n) {
+			if (words[i] == words[i + 1]) ++i;
+			else break;
 		}
 	}
-
-	topologySort();
-
-	for (int i = 1; i <= n; ++i)
-		cout << finishTime[i] << "\n";
 
 	return 0;
 }
