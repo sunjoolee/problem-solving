53448bd72266091d14954eec3c01fbc308b24b83
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-24 09:55:07 +0900

    백준 4095 최대 정사각형

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,73 +1,34 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n, m;
-string S, P;
-
-vector<int> getPartialMatch() {
-
-	vector<int> pi(m, 0);
-
-	//begin = 0이면 자기 자신을 찾아버리므로 begin = 1부터 시작
-	int begin = 1;
-	int matched = 0;
-
-	while (begin + matched < m) {
-		if (S[begin + matched] >= '0' && S[begin + matched] <= '9') {
-			++begin;
-		}
-		else if (S[begin + matched] == P[matched]) {
-			++matched;
-			pi[begin + matched - 1] = matched;
-		}
-		else {
-			if (matched == 0) ++begin;
-			else {
-				begin += matched - pi[matched - 1];
-				matched = pi[matched - 1];
-			}
-		}
-	}
-	return pi;
-}
-
-int found() {
-	vector<int> pi = getPartialMatch();
-
-	int begin = 0, matched = 0;
-	while (begin <= n - m) {
-		if (S[begin + matched] >= '0' && S[begin + matched] <= '9') {
-			++begin;
-		}
-		else if (matched < m && S[begin + matched] == P[matched]) {
-			++matched;
-			if (matched == m) return 1;
-		}
-		else {
-			if (matched == 0) ++begin;
-			else {
-				begin += matched - pi[matched - 1];
-				matched = pi[matched - 1];
-			}
-		}
-	}
-	return 0;
-}
-
+int dp[1002][1002] = { 0 };
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> S >> P;
-	n = S.size();
-	m = P.size();
+	int n, m;
+	while (true) {
+		cin >> n >> m;
+		if (n == 0 && m == 0)
+			break;
 
-	cout << found();
+		for (int i = 1; i <= n; ++i)
+			for (int j = 1; j <= m; ++j)
+				cin >> dp[i][j];
+
+		int res = 0;
+		for (int i = 1; i <= n; ++i)
+			for (int j = 1; j <= m; ++j) {
+				if (dp[i][j] == 0) continue;
+
+				dp[i][j] = min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j-1]) + 1;
+				res = max(res, dp[i][j]);
+			}
+		cout << res << "\n";
+	}
 
 	return 0;
 }
