33fde9136912ea7c2751b7010064bee8c3d70b4d
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-25 10:32:14 +0900

    백준 7562 나이트의 이동

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,50 +1,47 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <utility>
-#include <cstring>
 #include <algorithm>
 using namespace std;
 
-const int MAXX = 50;
+const int MAXL = 300;
 
-int w, h;
-int mp[MAXX + 1][MAXX + 1] = { 0 };
-int visited[MAXX + 1][MAXX + 1] = { 0 };
+int L;
+int visited[MAXL][MAXL];
 
-int dirr[8] = { -1,-1,-1,0,0,1,1,1 };
-int dirc[8] = { -1,0,1,-1,1,-1,0,1 };
+int rdir[8] = { -2,-2,-1,-1,1,1,2,2 };
+int cdir[8] = { -1,1,-2,2,-2,2,-1,1 };
 
-//mp[r][c]와 이어진 섬을 방문한다
-void bfs(int r, int c) {
-	queue<pair<int, int>> q;
-	q.push(make_pair(r, c));
+int bfs(int startr, int startc, int finishr, int finishc) {
+	//<<r, c>, move>
+	queue<pair<pair<int,int>, int>> que;
 
-	while (!q.empty()) {
-		pair<int, int> cur = q.front();
-		int curr = cur.first;
-		int curc = cur.second;
-		q.pop();
+	que.push(make_pair(make_pair(startr, startc), 0));
+	
+	while (!que.empty()) {
+		pair<pair<int, int>, int> cur = que.front();
+		int curr = cur.first.first;
+		int curc = cur.first.second;
+		int move = cur.second;
+		que.pop();
 
-		//방문 확인
+		if (curr == finishr && curc == finishc)
+			return move;
+
 		if (visited[curr][curc]) continue;
 		visited[curr][curc] = 1;
 		
 		for (int i = 0; i < 8; i++) {
-			int nextr = curr + dirr[i];
-			int nextc = curc + dirc[i];
-			
-			//범위 확인
-			if (nextr < 0 || nextr >= h) continue;
-			if (nextc < 0 || nextc >= w) continue;
+			int nextr = curr + rdir[i];
+			int nextc = curc + cdir[i];
 
-			//현재 섬과 이어진 섬 방문
-			if (mp[nextr][nextc] == 1) 
-				q.push(make_pair(nextr, nextc));
+			if (nextr < 0 || nextr >= L) continue;
+			if (nextc < 0 || nextc >= L) continue;
+
+			que.push(make_pair(make_pair(nextr, nextc), move + 1));
 		}
 	}
-
-	return;
+	
 }
 
 int main() {
@@ -52,25 +49,18 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	while (true) {
+	int t;
+	cin >> t;
+	while (t--) {
 		memset(visited, 0, sizeof(visited));
 
-		cin >> w >> h;
-		if (w == 0 && h == 0) break;
+		cin >> L;
 
-		for (int i = 0; i < h; i++)
-			for (int j = 0; j < w; j++)
-				cin >> mp[i][j];
+		int startr, startc, finishr, finishc;
+		cin >> startr >> startc >> finishr >> finishc;
 
-		int cnt = 0;
-		for (int r = 0; r < h; ++r)
-			for (int c = 0; c < w; ++c)
-				if (mp[r][c] == 1 && visited[r][c] == 0) {
-					bfs(r, c);
-					++cnt;
-				}
-		cout << cnt << "\n";
+		cout << bfs(startr, startc, finishr, finishc) << "\n";
 	}
-	
+
 	return 0;
 }
