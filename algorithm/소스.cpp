903d7de4648258c474a87887ba289dc8b76a049e
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-25 10:08:36 +0900

    백준 2963 섬의 개수

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,51 +1,76 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <cstring>
+#include <algorithm>
 using namespace std;
+
+const int MAXX = 50;
+
+int w, h;
+int mp[MAXX + 1][MAXX + 1] = { 0 };
+int visited[MAXX + 1][MAXX + 1] = { 0 };
+
+int dirr[8] = { -1,-1,-1,0,0,1,1,1 };
+int dirc[8] = { -1,0,1,-1,1,-1,0,1 };
+
+//mp[r][c]와 이어진 섬을 방문한다
+void bfs(int r, int c) {
+	queue<pair<int, int>> q;
+	q.push(make_pair(r, c));
+
+	while (!q.empty()) {
+		pair<int, int> cur = q.front();
+		int curr = cur.first;
+		int curc = cur.second;
+		q.pop();
+
+		//방문 확인
+		if (visited[curr][curc]) continue;
+		visited[curr][curc] = 1;
+		
+		for (int i = 0; i < 8; i++) {
+			int nextr = curr + dirr[i];
+			int nextc = curc + dirc[i];
+			
+			//범위 확인
+			if (nextr < 0 || nextr >= h) continue;
+			if (nextc < 0 || nextc >= w) continue;
+
+			//현재 섬과 이어진 섬 방문
+			if (mp[nextr][nextc] == 1) 
+				q.push(make_pair(nextr, nextc));
+		}
+	}
+
+	return;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	string input;
-	int flag = 0;
-
 	while (true) {
-		cin >> input;
-		if (cin.eof()) break;
+		memset(visited, 0, sizeof(visited));
 
-		int found;
+		cin >> w >> h;
+		if (w == 0 && h == 0) break;
 
-		if (flag == 0) {
-			found = input.find('U');
-			if (found != -1) {
-				input = input.substr(found, -1);
-				++flag;
-			}
-		}
-		if (flag == 1) {
-			found = input.find('C');
-			if (found != -1) {
-				input = input.substr(found, -1);
-				++flag;
-			}
-		}
-		if (flag == 2) {
-			found = input.find('P');
-			if (found != -1) {
-				input = input.substr(found, -1);
-				++flag;
-			}
-		}
-		if (flag == 3) {
-			found = input.find('C');
-			if (found != -1) {
-				cout << "I love UCPC";
-				return 0;
-			}
-		}
+		for (int i = 0; i < h; i++)
+			for (int j = 0; j < w; j++)
+				cin >> mp[i][j];
+
+		int cnt = 0;
+		for (int r = 0; r < h; ++r)
+			for (int c = 0; c < w; ++c)
+				if (mp[r][c] == 1 && visited[r][c] == 0) {
+					bfs(r, c);
+					++cnt;
+				}
+		cout << cnt << "\n";
 	}
-
-	cout << "I hate UCPC";
+	
 	return 0;
 }
