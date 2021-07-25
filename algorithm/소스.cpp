98737f5964a1a55d8e63ce2e5c389a69c1546f9e
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-25 11:16:50 +0900

    백준 2178 미로 탐색

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <queue>
 #include <utility>
+#include <string>
 #include <algorithm>
 using namespace std;
 
-const int MAXL = 300;
+const int MAXX = 100;
 
-int L;
-int visited[MAXL][MAXL];
+int n,m;
 
-int rdir[8] = { -2,-2,-1,-1,1,1,2,2 };
-int cdir[8] = { -1,1,-2,2,-2,2,-1,1 };
+//이동할 수 없는 칸 0
+//이동할 수 있는 칸 1
+//이미 방문한 칸 2
+string mp[MAXX+1];
 
-int bfs(int startr, int startc, int finishr, int finishc) {
+int rdir[4] = {-1,0,0,1};
+int cdir[4] = { 0,-1,1,0};
+
+int bfs() {
 	//<<r, c>, move>
 	queue<pair<pair<int,int>, int>> que;
 
-	que.push(make_pair(make_pair(startr, startc), 0));
+	que.push(make_pair(make_pair(0,0),1));
 	
 	while (!que.empty()) {
 		pair<pair<int, int>, int> cur = que.front();
@@ -25,20 +30,23 @@ int bfs(int startr, int startc, int finishr, int finishc) {
 		int move = cur.second;
 		que.pop();
 
-		if (curr == finishr && curc == finishc)
+		if (curr == (n-1) && curc == (m-1))
 			return move;
 
-		if (visited[curr][curc]) continue;
-		visited[curr][curc] = 1;
+		if (mp[curr][curc] != '1') 
+			continue;
+		//방문 표시
+		mp[curr][curc] = '2';
 		
-		for (int i = 0; i < 8; i++) {
+		for (int i = 0; i < 4; i++) {
 			int nextr = curr + rdir[i];
 			int nextc = curc + cdir[i];
 
-			if (nextr < 0 || nextr >= L) continue;
-			if (nextc < 0 || nextc >= L) continue;
+			if (nextr < 0 || nextr >= n) continue;
+			if (nextc < 0 || nextc >= m) continue;
 
-			que.push(make_pair(make_pair(nextr, nextc), move + 1));
+			if (mp[nextr][nextc] == '1')
+				que.push(make_pair(make_pair(nextr, nextc), move + 1));
 		}
 	}
 	
@@ -48,19 +56,11 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+		cin >> mp[i];
 
-	int t;
-	cin >> t;
-	while (t--) {
-		memset(visited, 0, sizeof(visited));
-
-		cin >> L;
-
-		int startr, startc, finishr, finishc;
-		cin >> startr >> startc >> finishr >> finishc;
-
-		cout << bfs(startr, startc, finishr, finishc) << "\n";
-	}
-
+	cout << bfs();
 	return 0;
 }
