970063545d597e7a296c827db21e08bcff6c3ed5
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-27 14:57:23 +0900

    백준 15903 카드 합체 놀이

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,64 +1,43 @@
 #include <iostream>
-#include <vector>
-#include <math.h>
+#include <queue>
 #include <algorithm>
 using namespace std;
+
+typedef long long ll;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	cin >> n;
+	int n, m;
+	cin >> n >> m;
 
-	//코딩력
-	vector<int> X;
-	vector<int> tournament;
+	//작은 원소부터 출력되는 우선순위 큐
+	priority_queue<ll, vector<ll>, greater<ll>> card;
 
 	for (int i = 0; i < n; ++i) {
-		int input;
+		long long input;
 		cin >> input;
-		X.push_back(input);
-		tournament.push_back(input);
-
+		card.push(input);
 	}
 
-	//코딩력 오름차순 정렬
-	sort(X.begin(), X.end());
+	for (int i = 0; i < m; ++i) {
+		long long a = card.top();
+		card.pop();
+		long long b = card.top();
+		card.pop();
 
-	//지루함의 합
+		card.push(a + b);
+		card.push(a + b);
+	}
+	 
 	long long sum = 0;
-
-	for (int i = 0; i < n - 1; ++i) {
-		//가장 못하는 사람의 코딩력
-		int worst = X[i];
-
-		for (auto it = tournament.begin(); it != tournament.end(); ++it) {
-			if (*it == worst) {
-				//토너먼트의 맨 왼쪽
-				if (it == tournament.begin()) {
-					int right = *(it + 1);
-					sum += abs(worst - right);;
-				}
-				//토너먼트의 맨 왼쪽
-				else if (it == tournament.end() - 1) {
-					int left = *(it - 1);
-					sum += abs(worst - left);
-				}
-				//토너먼트의 중간
-				else {
-					int right = *(it + 1);
-					int left = *(it - 1);
-					sum += min(abs(worst - right), abs(worst - left));
-				}
-
-				tournament.erase(it);
-				break;
-			}
-		}
+	while(!card.empty()) {
+		sum += card.top();
+		card.pop();
 	}
-
 	cout << sum;
+
 	return 0;
 }
