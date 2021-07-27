34d879c9902c4a3b4e7f1aadc63d523a7a614322
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-27 16:49:00 +0900

    백준 2437 저울

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,43 +1,32 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-typedef long long ll;
+typedef int unsigned long long ull;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n, m;
-	cin >> n >> m;
+	ull weight[1001];
 
-	//작은 원소부터 출력되는 우선순위 큐
-	priority_queue<ll, vector<ll>, greater<ll>> card;
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> weight[i];
 
+	sort(weight, weight + n);
+
+	ull psum = 0;
 	for (int i = 0; i < n; ++i) {
-		long long input;
-		cin >> input;
-		card.push(input);
+		if (weight[i] > psum + 1ull) {
+			cout << psum + 1ull;
+			return 0;
+		}
+		psum += weight[i];
 	}
-
-	for (int i = 0; i < m; ++i) {
-		long long a = card.top();
-		card.pop();
-		long long b = card.top();
-		card.pop();
-
-		card.push(a + b);
-		card.push(a + b);
-	}
-	 
-	long long sum = 0;
-	while(!card.empty()) {
-		sum += card.top();
-		card.pop();
-	}
-	cout << sum;
 
 	return 0;
 }
