022d6e23ccaa15c0fa904a4f10eb7dd242475323
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-27 13:45:20 +0900

    백준 15321 이상한 토너먼트

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -10,44 +10,51 @@ int main() {
 	cout.tie(NULL);
 
 	int n;
+	cin >> n;
+
+	//코딩력
+	vector<int> X;
 	vector<int> tournament;
 
-	cin >> n;
 	for (int i = 0; i < n; ++i) {
 		int input;
 		cin >> input;
+		X.push_back(input);
 		tournament.push_back(input);
+
 	}
 
-	int sum = 0;
-	for (int worst = n; worst > 1; worst--) {
+	//코딩력 오름차순 정렬
+	sort(X.begin(), X.end());
+
+	//지루함의 합
+	long long sum = 0;
+
+	for (int i = 0; i < n - 1; ++i) {
+		//가장 못하는 사람의 코딩력
+		int worst = X[i];
+
 		for (auto it = tournament.begin(); it != tournament.end(); ++it) {
 			if (*it == worst) {
 				//토너먼트의 맨 왼쪽
 				if (it == tournament.begin()) {
 					int right = *(it + 1);
-
-					sum += abs(worst - right);
-					tournament.erase(it);
-					break;
+					sum += abs(worst - right);;
 				}
-				//토너먼트의 맨 오른쪽
+				//토너먼트의 맨 왼쪽
 				else if (it == tournament.end() - 1) {
 					int left = *(it - 1);
-
 					sum += abs(worst - left);
-					tournament.erase(it);
-					break;
 				}
 				//토너먼트의 중간
 				else {
 					int right = *(it + 1);
 					int left = *(it - 1);
-
 					sum += min(abs(worst - right), abs(worst - left));
-					tournament.erase(it);
-					break;
 				}
+
+				tournament.erase(it);
+				break;
 			}
 		}
 	}
