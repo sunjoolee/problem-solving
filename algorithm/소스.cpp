a6433e93b8ba3fc9c4fd1d2fcba983a8e06d0d83
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-07-28 14:26:03 +0900

    백준 16570 앞뒤가 맞는 수열

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -4,56 +4,67 @@
 #include <algorithm>
 using namespace std;
 
-typedef unsigned long long ull;
+typedef long long ll;
 
+int n;
 
-int main(){
+//<접두사도 되고 접미사도 되는 문자열의 최대 길이, 나타나는 횟수>
+pair<int, int> getPartialMatch(const vector<ll>& N) {
+	vector<int> pi(n, 0);
+
+	//최대 길이, 나타나는 횟수
+	int maxpi = 0;
+	int cnt = 0;
+
+	int begin = 1;
+	int matched = 0;
+	while (begin + matched < n) {
+		if (N[matched] == N[begin + matched]) {
+			++matched;
+			pi[begin + matched - 1] = matched;
+			
+			//최대 길이, 나타나는 횟수 갱신
+			if (matched > maxpi) {
+				maxpi = matched;
+				cnt = 1;
+			}
+			else if (matched == maxpi) {
+				++cnt;
+			}
+		}
+		else {
+			if (matched == 0) ++begin;
+			else {
+				begin += matched - pi[matched - 1];
+				matched = pi[matched - 1];
+			}
+		}
+	}
+	return make_pair(maxpi, cnt);
+}
+
+int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
 	cin >> n;
 
-	vector<string> coin;
-
+	vector<ll> num(n, 0);
 	for (int i = 0; i < n; ++i) {
-		string input;
+		ll input;
 		cin >> input;
-		coin.push_back(input);
-	}
-	
-	int minT = 401;
-
-	//모든 행에 대해서 뒤집을지 말지 결정
-	for (ull k = 0; k < (1 << n); ++k) {
-		//전체 동전 뒷면의 개수;
-		int totalT = 0;
-
-		for (int j = 0; j < n; ++j) {
-
-			//현재 열에서 동전 뒷면의 개수
-			int colT = 0;
-			for (int i = 0; i < n; ++i) {
-				char now;
-
-				//i번 째 행을 뒤집기로 결정된 경우
-				if ((1 << i) & k) {
-					if (coin[i][j] == 'T') now = 'H';
-					if (coin[i][j] == 'H') now = 'T';
-				}
-				else now = coin[i][j];
-
-				if (now == 'T') ++colT;
-			}
-			
-			//열을 뒤집는 경우와 뒤집지 않는 경우 중 동전 뒷면의 개수 적게 만드는 경우를 선택
-			totalT += min(n - colT, colT);
-		}
-
-		minT = min(minT, totalT);
+		num[n - 1 - i] = input;
 	}
 
-	cout << minT;
+	pair<ll, ll> res = getPartialMatch(num);
+
+	if (res.first == 0) {
+		cout << -1;
+		return 0;
+	}
+	else
+		cout << res.first << " " << res.second;
+
 	return 0;
 }
