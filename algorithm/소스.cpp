22038e083c8bcb545c544167acb2db3e291429ac
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-03 07:53:44 +0900

    백준 1068 트리

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -4,77 +4,69 @@
 #include <algorithm>
 using namespace std;
 
-
-//adj[i] : 노드i와 인접한 노드 집합
-vector<int> adj[200000];
-
-//visited[i] : 노드 i가 bfs에 의해 방문된 횟수
-//(adj[i].size() / 2)번 이상 방문되면 루머를 믿기 시작한다
-int visited[200000] = { 0 };
-
-//belive[i]: 노드 i가 루머를 믿기 시작한 시간
-int believe[200000];
-
-//<현재 노드 위치, 현재 시간>
-//루머를 믿기 시작하면 노드 큐에 저장
-queue<pair<int, int>> q;
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	for (int i = 0; i < 200000; ++i)
-		believe[i] = -1;
-
 	int n;
 	cin >> n;
 
+	//루트 -1, 삭제 -2
+	int parent[50];
+
 	for (int i = 0; i < n; ++i) {
-		while (true) {
-			int input;
-			cin >> input;
-			if (input == 0) break;
-
-			adj[i].push_back(input - 1);
-		}
+		int p;
+		cin >> p;
+		parent[i] = p;
 	}
 
+	//삭제될 노드의 번호
+	int x;
+	cin >> x;
 
-	//최초 유포자
-	int m;
-	cin >> m;
-	for (int i = 0; i < m; ++i) {
-		int input;
-		cin >> input;
-		q.push(make_pair(input - 1, 0));
+	//루트가 삭제된 경우
+	if (parent[x] == -1) {
+		cout << 0;
+		return 0;
 	}
+
+	//노드 삭제 & 이 노드를 부모로 하는 노드를 삭제하기 위해 큐에 저장
+	queue<int> q;
+	q.push(x);
 
 	while (!q.empty()) {
-		pair<int, int> cur = q.front();
-		int curnode = cur.first;
-		int curtime = cur.second;
+		int p = q.front();
+		//노드 삭제
+		parent[p] = -2;
 		q.pop();
 
-		//자신이 루머를 믿기 시작한 시간 갱신
-		if (believe[curnode] != -1) continue;
-		believe[curnode] = curtime;
-
-		//자신의 주변 노드에 루머 퍼트리기
-		for (int i = 0; i < adj[curnode].size(); ++i) {
-			int adjnode = adj[curnode][i];
-
-			if (believe[adjnode] == -1) {
-				visited[adjnode]++;
-				if (visited[adjnode] >= ((double)adj[adjnode].size() / 2)) {
-					q.push(make_pair(adjnode, curtime + 1));
-				}
+		for (int i = 0; i < n; ++i) {
+			//이 노드를 부모로 하는 노드 큐에 저장
+			if (parent[i] == p) {
+				q.push(i);
 			}
 		}
 	}
-	
-	for (int i = 0; i < n; ++i)
-		cout << believe[i] << " ";
 
+	//리프 노드의 개수 확인
+	int leaf = 0;
+	for (int i = 0; i < n; ++i) {
+		//삭제된 노드라면 건너뛴다
+		if (parent[i] == -2) continue;
+
+		bool isLeaf = true;
+		for (int j = 0; j < n; ++j) {
+			//자신을 부모로 갖는 노드가 있다면 리프 노드가 될 수 없다
+			if (parent[j] == i) {
+				isLeaf = false;
+				break;
+			}
+		}
+
+		if (isLeaf) leaf++;
+	}
+
+	cout << leaf;
 	return 0;
 }
