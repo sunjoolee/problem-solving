37ebf12a41ecac61393e71e6c22d1efa80198fd8
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-03 13:51:18 +0900

    백준 2233 사과나무

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,41 +1,78 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
 using namespace std;
-
-
-int n;
-
-//현재까지 만든 string의 길이 len
-void good(string str, int len) {
-	//좋은 수열인지 판단
-	if (str.size() > 1) {
-		for (int s = 1; s <= str.size() / 2; ++s)
-			if (str.substr(str.size() - s, s) == str.substr(str.size() - 2 * s, s))
-				return;
-	}
-	
-	//base case
-	if (len == n) {
-		cout << str;
-		exit(0);
-	}
-
-	//숫자 후보 1,2,3
-	good(str + '1', len + 1);
-	good(str + '2', len + 1);
-	good(str + '3', len + 1);
-
-	return;
-}
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	
-	cin >> n;
 
-	good("", 0);
+	int n;
+	cin >> n;
+	string input;
+	cin >> input;
+
+	//지금까지 나온 사과의 번호
+	int appleNo = 0;
+	//지금까지 나온 사과를 이진수와 대응시키기 위해 사용하는 스택
+	stack<int> apple;
+
+	//appleTree[i]: 이진수의 i번째 자리와 대응되는 사과의 번호
+	vector<int> appleTree;
+
+	for (int i = 0; i < 2 * n; ++i) {
+		if (input[i] == '0') {
+			//새로운 사과 방문 -> 스택에 저장
+			apple.push(appleNo);
+			appleTree.push_back(appleNo);
+
+			appleNo++;
+		}
+
+		if (input[i] == '1') {
+			//기존의 사과에서 리턴 -> 스택에서 pop
+			appleTree.push_back(apple.top());
+			apple.pop();
+		}
+	}
+
+	//applePair[i]: <사과 i를 방문할 때의 이진수에서의 0의 위치,사과 i에서 리턴될 때의 이진수에서의 1의 위치> 
+	vector<pair<int, int>> applePair(n, make_pair(0,0));
+	
+	for (int i = 0; i < 2 * n; ++i) {
+		int curapple = appleTree[i];
+		//curapple 사과를 방문할 때의 이진수에서의 0의 위치
+		if (input[i] == '0')
+			applePair[curapple].first = i;
+		//curapple 사과에서 리턴할 때의 이진수에서의 1의 위치
+		if (input[i] == '1')
+			applePair[curapple].second = i;
+	}
+
+	//썩은 사과
+	int x, y;
+	cin >> x >> y;
+	
+	//썩은 사과가 이진수에서 처음으로 나타난 위치 
+	int badappleStart = min(applePair[appleTree[x-1]].first, applePair[appleTree[y-1]].first);
+	//썩은 사과가 이진수에서 마지막으로 나타난 위치
+	int badappleEnd = max(applePair[appleTree[x-1]].second, applePair[appleTree[y-1]].second);
+
+	int ansi = 0;
+	int ansj = 2*n-1;
+
+	for (int i = 0; i < n; ++i) {
+		if ((applePair[i].first <= badappleStart) && (applePair[i].second >= badappleEnd)) {
+			if (applePair[i].first >= ansi) {
+				ansi = applePair[i].first;
+				ansj = applePair[i].second;
+			}
+		}
+	}
+
+	cout << ansi+1 << " " << ansj+1;
 	return 0;
 }
