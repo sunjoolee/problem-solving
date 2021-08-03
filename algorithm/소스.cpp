c280ebd073fb0e2858e58e14a2355c8668bb9670
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-03 09:33:37 +0900

    백준 1189 컴백홈

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,33 +1,61 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-typedef unsigned long long ull;
+int R, C, k;
+int cnt = 0;
+
+string mp[6];
+int visited[6][6] = { 0 };
+
+int dirr[4] = { 0,0,1,-1 };
+int dirc[4] = { 1,-1,0,0 };
+
+
+
+//현재 위치 (r, c)이고 이동해온 거리 p인 경우
+//거리가 k인 가짓수 반환
+void path(int r, int c, int p) {
+	//base case
+	//오른쪽 위 도착
+	if (r == 0 && c == C-1) {
+		if (p == k) cnt++;
+		return;
+	}
+
+	visited[r][c] = 1;
+	for (int i = 0; i < 4; ++i) {
+		int nextr = r + dirr[i];
+		int nextc = c + dirc[i];
+
+		if (nextr >= R || nextr < 0) continue;
+		if (nextc >= C || nextc < 0) continue;
+		if (visited[nextr][nextc] == 1) continue;
+		if (mp[nextr][nextc] == 'T') continue;
+
+		path(nextr, nextc, p + 1);
+	}
+	visited[r][c] = 0;
+
+	return;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int H;
-	cin >> H;
+	cin >> R >> C >> k;
 
-	//dp[h]: 높이가 h일 때 필요한 차의 개수
-	ull dp[61];
-	
-	dp[0] = 1;
-	dp[1] = 1;
-	
-	//현재 높이가 h일 때
-	//높이가 0 일 때 필요한 차의 개수의 합 + ... + 높이가 h-2 일 때 필요한 차의 개수의 합
-	ull psum = 1;
+	for (int i = 0; i < R; ++i)
+		cin >> mp[i];
 
-	for (int h = 2; h <= H; ++h) {
-		dp[h] = 1 + (2 * psum);
-		psum += dp[h - 1];
-	}
-
-	cout << dp[H];
-
+	//경로 계산
+	//왼쪽 아래에서 시작
+	path(R-1, 0, 1);
+	//경로 길이 k인 개수 출력
+	cout << cnt++;
 	return 0;
 }
