0bfc8b3ddb36a94b5ebc0d278d7b901b8bc150cd
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-04 10:12:53 +0900

    백준 10845 큐

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
+#include <queue>
 #include <string>
-#include <vector>
-#include <utility>
 #include <algorithm>
 using namespace std;
-
-bool compare(pair<pair<int, int>, string> a, pair<pair<int, int>, string> b) {
-	//나이순 정렬
-	if (a.first.first < b.first.first) return true;
-	//나이 같다면 가입한 순서대로 정렬
-	else if (a.first.first == b.first.first)
-		return (a.first.second < b.first.second);
-	else return false;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
+	queue<int> q;
+
 	int n;
 	cin >> n;
+	while (n--) {
+		string input;
+		cin >> input;
 
-	//<<나이, 가입 순서>, 이름>
-	vector<pair<pair<int, int>, string>> v;
-	for (int i = 0; i < n; ++i) {
-		int age;
-		string name;
-		cin >> age >> name;
-		v.push_back(make_pair(make_pair(age, i), name));
+		if (input == "push") {
+			int x;
+			cin >> x;
+			q.push(x);
+		}
+		else if (input == "pop"){
+			if (q.empty()) cout << -1<<"\n";
+			else {
+				cout << q.front() << "\n";
+				q.pop();
+			}
+		}
+		else if (input == "size") {
+			cout << q.size() << "\n";
+		}
+		else if (input == "empty") {
+			cout << q.empty() << "\n";
+		}
+		else if (input == "front") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.front() << "\n";
+		}
+		else if (input == "back") {
+			if (q.empty()) cout << -1 << "\n";
+			else cout << q.back() << "\n";
+		}
 	}
 
-	sort(v.begin(), v.end(), compare);
-	
-	for (int i = 0; i < n; ++i) {
-		cout << v[i].first.first << " "<< v[i].second<<"\n";
-	}
 	return 0;
 }
