e8afe918b23a2d6c980425879a271f5cb3f596e0
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-04 09:22:54 +0900

    백준 2798 블랙잭

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-//항상 a > b
-int GCD(int a, int b) {
-	if (a % b == 0) return b;
-	else return GCD(b, a % b);
-}
+vector<int> card;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -17,14 +13,52 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 
-	if (n < m) {
-		int tmp = m;
-		m = n;
-		n = tmp;
+	for (int i = 0; i < n; ++i) {
+		int input;
+		cin >> input;
+		card.push_back(input);
 	}
 
-	int gcd = GCD(n, m);
-	int lcm = n * m / gcd;
-	cout << gcd << "\n" << lcm;
+	sort(card.begin(), card.end());
+
+	//i: 첫번째 수의 인덱스
+	//j: 두번째 수의 인덱스
+	//k: 세번째 수의 인덱스
+	
+	int best = 0;
+	for (int i = 0; i < n; ++i) {
+		//투포인터 알고리즘
+		int j = 0;
+		int k = n - 1;
+
+		while (j < k) {
+			//i,j,k 서로 다른 카드를 가리키도록 함
+			if (j == i) {
+				j++;
+				continue;
+			}
+			if (k == i) {
+				k--;
+				continue;
+			}
+
+			//i,j,k가 가리키는 세 카드의 합
+			int sum = card[i] + card[j] + card[k];
+
+			if (sum > m) {
+				k--;
+			}
+			if(sum == m){
+				cout << m;
+				return 0;
+			}
+			if (sum < m) {
+				best = max(best, sum);
+				j++;
+			}
+		}
+	}
+
+	cout << best;
 	return 0;
 }
