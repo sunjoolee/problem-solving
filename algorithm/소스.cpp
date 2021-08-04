be391ead397f863e2027146e5d6259bb7739fc85
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-05 08:45:38 +0900

    백준 2108 통계학

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,64 +1,75 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <map>
+#include <cmath>
 #include <algorithm>
 using namespace std;
+
+//n개의 숫자 배열
+vector<int> num;
+
+//<숫자, 빈도수>
+map<int, int> freq;
+
+bool compare(pair<int, int>& a, const pair<int, int>& b) {
+	//빈도수 내림차순으로 정렬
+	if (a.second > b.second) return true;
+
+	//빈도수 같으면 수의 크기 오름차순으로 정렬 
+	if (a.second == b.second) 
+		return a.first < b.first;
+
+	return false;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int t;
-	cin >> t;
+	int n;
+	cin >> n;
 
-	while (t--) {
-		int n, m;
-		cin >> n >> m;
 
-		//모든 문서의 중요도 저장
-		vector<int> importance;
-		//프린터큐 <문서 번호, 문서 중요도>
-		queue<pair<int, int>> printor;
+	int sum = 0;
+	for (int i = 0; i < n; ++i) {
+		int input;
+		cin >> input;
+		num.push_back(input);
 
-		for (int i = 0; i < n; ++i) {
-			int input;
-			cin >> input;
+		//input이 나온 빈도수 계산
+		freq[input]++;
 
-			printor.push(make_pair(i, input));
-			importance.push_back(input);
-		}
-
-		//모든 문서의 중요도 정렬
-		sort(importance.begin(), importance.end());
-
-		//문서가 프린트된 순서
-		int seq = 1;
-		while (true) {
-			pair<int, int> document = printor.front();
-
-			//현재 문서의 중요도가 
-			//나머지 모든 문서의 중요도 중 가장 큰 값과 같은 경우
-			if (document.second == importance.back()) {
-				//궁금했던 문서인 경우 
-				if (document.first == m) {
-					//프린트된 순서 출력 후 종료
-					cout << seq<<"\n";
-					break;
-				}
-				//문서 프린트 & 문서의 중요도 벡터에서 제거
-				printor.pop();
-				importance.pop_back();
-				seq++;
-			}
-			//현재 문서의 중요도가 
-			//나머지 모든 문서의 중요도 중 가장 큰 값보다 작은 경우
-			else {
-				//현재 문서 프린터 큐의 뒤로 이동
-				printor.push(document);
-				printor.pop();
-			}
-		}
+		//숫자 배열의 합 계산
+		sum += input;
 	}
+
+	sort(num.begin(), num.end());
+
+	//1. 산술평균 출력
+	//floor(실수 + 0.5): 실수 반올림
+	cout << floor(((double)sum / (double)n) + 0.5) << "\n";
+
+	//2. 중앙값 출력
+	cout << num[n / 2] << "\n";
+
+	//3. 최빈값 출력
+	vector<pair<int, int>> v(freq.begin(), freq.end());
+
+	//빈도수가 큰 수가 앞에 오도록,
+	//같은 빈도수라면 크기가 제일 작은 수가 앞에 오도록 정렬
+	sort(v.begin(), v.end(), compare);
+	
+	//최빈값이 한 개인 경우
+	if (v.size() == 1 || v[0].second > v[1].second)
+		cout << v[0].first << "\n";
+	//최빈값이 여러 개인 경우 두 번째로 작은 값 출력
+	else if(v[0].second == v[1].second)
+		cout << v[1].first << "\n";
+
+	//4. 범위 출력
+	cout << num[n - 1] - num[0] << "\n";
+
+
 	return 0;
 }
