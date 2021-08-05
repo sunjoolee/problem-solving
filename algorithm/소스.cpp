ebe5cafbedf6037f9f9f39ef3b18c9e35c4d8319
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-06 08:19:52 +0900

    백준 11203 Numbers On a Tree

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,75 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <map>
 #include <cmath>
-#include <algorithm>
+#include <string>
 using namespace std;
-
-//n개의 숫자 배열
-vector<int> num;
-
-//<숫자, 빈도수>
-map<int, int> freq;
-
-bool compare(pair<int, int>& a, const pair<int, int>& b) {
-	//빈도수 내림차순으로 정렬
-	if (a.second > b.second) return true;
-
-	//빈도수 같으면 수의 크기 오름차순으로 정렬 
-	if (a.second == b.second) 
-		return a.first < b.first;
-
-	return false;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	cin >> n;
+	int height;
+	cin >> height;
 
+	int numofnodes = pow(2, height + 1) - 1;
 
-	int sum = 0;
-	for (int i = 0; i < n; ++i) {
-		int input;
-		cin >> input;
-		num.push_back(input);
+	string path;
+	cin >> path;
 
-		//input이 나온 빈도수 계산
-		freq[input]++;
-
-		//숫자 배열의 합 계산
-		sum += input;
+	//경로가 주어지지 않은 경우
+	if (cin.eof()) {
+		//루트의 노드 번호 출력 후 종료
+		cout << numofnodes;
+		return 0;
 	}
 
-	sort(num.begin(), num.end());
-
-	//1. 산술평균 출력
-	//floor(실수 + 0.5): 실수 반올림
-	cout << floor(((double)sum / (double)n) + 0.5) << "\n";
-
-	//2. 중앙값 출력
-	cout << num[n / 2] << "\n";
-
-	//3. 최빈값 출력
-	vector<pair<int, int>> v(freq.begin(), freq.end());
-
-	//빈도수가 큰 수가 앞에 오도록,
-	//같은 빈도수라면 크기가 제일 작은 수가 앞에 오도록 정렬
-	sort(v.begin(), v.end(), compare);
 	
-	//최빈값이 한 개인 경우
-	if (v.size() == 1 || v[0].second > v[1].second)
-		cout << v[0].first << "\n";
-	//최빈값이 여러 개인 경우 두 번째로 작은 값 출력
-	else if(v[0].second == v[1].second)
-		cout << v[1].first << "\n";
+	int i = 1;
+	for (int j = 0; j < path.size(); ++j) {
+		//LeftChild로 이동
+		if (path[j] == 'L') 
+			i = 2 * i;
+		//RightChild로 이동
+		else if (path[j] == 'R') 
+			i = 2 * i + 1;
+	}
 
-	//4. 범위 출력
-	cout << num[n - 1] - num[0] << "\n";
-
+	//경로의 도착 지점의 노드 번호 출력
+	cout << numofnodes - i + 1;
 
 	return 0;
 }
