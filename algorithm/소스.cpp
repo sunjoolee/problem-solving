cb47226936e087f7e2484a07d38e230ee9aa2dc9
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-06 13:33:59 +0900

    백준 15900 나무 탈출

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,35 +1,55 @@
 #include <iostream>
-#include <set>
-#include <string>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
-set<string> S;
+//트리의 리프 노드들의 깊이의 합
+int sum = 0;
+
+vector<vector<int>> edge (500001);
+int visited[500001] = { 0 };
+
+void dfs(int node, int depth) {
+
+	//리프노드 만나면 종료
+	//간선의 수 1개인 노드 = 리프 노드
+	if (node != 1 && edge[node].size() == 1) {
+		sum += depth;
+		return;
+	}
+	
+	visited[node] = 1;
+
+	for(int i = 0; i<edge[node].size(); ++i){
+		if (!visited[edge[node][i]])
+			dfs(edge[node][i], depth + 1);
+	}
+
+
+	return;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n, m;
-	cin >> n >> m;
+	int n;
+	cin >> n;
 
-	for (int i = 0; i < n; ++i) {
-		string input;
-		cin >> input;
+	for (int i = 1; i < n; ++i) {
+		int a, b;
+		cin >> a >> b;
 
-		S.insert(input);
+		edge[a].push_back(b);
+		edge[b].push_back(a);
 	}
 
-	int cnt = 0;
-	for (int i = 0; i < m; ++i) {
-		string input;
-		cin >> input;
+	//루트 노드부터 DFS
+	dfs(1, 0);
+	
+	//sum 홀수인 경우 성원이 승리
+	if (sum % 2) cout << "Yes";
+	else cout << "No";
 
-		if(S.find(input) != S.end())
-			cnt++;
-	}
-
-	cout << cnt;
 	return 0;
 }
