d9737273f73cdf50f5f8532a0b2328115c44369a
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-06 11:39:45 +0900

    백준 13116 30번

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -6,34 +6,18 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n;
-	cin >> n;
+	int T;
+	cin >> T;
+	while (T--) {
+		int A, B;
+		cin >> A >> B;
 
-	int root;
-	int parent[21] = { 0 };
-	for (int i = 1; i <= n; ++i) {
-		int p;
-		cin >> p;
-		parent[i] = p;
-
-		if (p == -1) root = i;
-	}
-
-	for (int i = 1; i <= n; ++i) {
-		int node = i;
-		int height = 0;
-
-		if (node == root) {
-			cout << height << "\n";
-			continue;
+		while (A != B) {
+			if (A > B) A /= 2;
+			else if (A < B) B /= 2;
 		}
 
-		while (node != root) {
-			node = parent[node];
-			height++;
-		}
-		cout << height << "\n";
+		cout << 10 * A << "\n";
 	}
-
 	return 0;
 }
