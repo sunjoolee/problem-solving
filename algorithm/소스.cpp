ee20178e9946933cb24930d2203d17fdac2f00ce
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-10 09:09:36 +0900

    백준 2343 기타 레슨

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,56 +1,60 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <vector>
+#include <math.h>
 #include <algorithm>
 using namespace std;
+
+//최적화 문제: M개의 블루레이로 모든 레슨을 녹화할 수 있는 블루레이의 크기 중 최솟값을 구해라
+//결정 문제:  크기 x 혹은 x이하인 블루레이 M개로 모든 레슨을 녹화할 수 있는가?
+
+int n, m;
+vector<int> lesson;
+
+bool decision(double x) {
+	//모든 레슨을 녹화하기 위해 필요한 블루레이의 개수
+	int cnt = 1;
+
+	//하나의 블루레이에 들어갈 레슨의 길이의 합
+	int sum = 0;
+	for (int i = 0; i < n; ++i) {
+		if (sum + lesson[i] > x) {
+			cnt++;
+			sum = lesson[i];
+		}
+		else sum += lesson[i];
+	}
+
+	return cnt <= m;
+}
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	while (true) {
-		string input;
-		getline(cin, input);
+	cin >> n >> m;
 
-		//종료 조건
-		if (input == ".") break;
+	//가장 길이가 긴 레슨의 길이
+	int longestlesson = 0;
+	for (int i = 0; i < n; ++i) {
+		int input;
+		cin >> input;
+		lesson.push_back(input);
 
-		stack<char> st;
-		bool balance = true;
-		
-		//문자열 검사
-		for (int i = 0; i < input.size(); ++i) {
-			char curchar = input[i];
-			
-			//여는 괄호인 경우 push
-			if (curchar == '(' || curchar == '[')
-				st.push(curchar);
-
-			//닫는 괄호인 경우 짝 맞으면 pop
-			else if (curchar == ')') {
-				if (!st.empty() && st.top() == '(') st.pop();
-				else {
-					balance = false;
-					break;
-				}
-			}
-			else if (curchar == ']') {
-				if (!st.empty() && st.top() == '[') st.pop();
-				else {
-					balance = false;
-					break;
-				}
-			}
-		}
-		//문자열 검사 종료 후 스택 비어있지 않은 경우
-		//문자열 균형을 이루지 않는다
-		if (!st.empty()) 
-			balance = false;
-
-		if (balance) cout << "yes\n";
-		else cout << "no\n";
-
+		longestlesson = max(longestlesson, input);
 	}
+	
+	double hi = 10000;
+	double lo = longestlesson;
+	for (int it = 0; it < 100; ++it) {
+		double mid = (hi + lo) / 2;
+
+		if (decision(mid)) hi = mid;
+		else lo = mid;
+	}
+
+	cout << lo;
+
 	return 0;
 }
