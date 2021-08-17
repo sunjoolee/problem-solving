cb3da2e4818111f8e487539f99961715bcd86618
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-17 15:36:38 +0900

    백준 1655 가운데를 말해요

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 #include <algorithm>
 using namespace std;
 
-typedef unsigned long long int ull;
-
 int n;
-priority_queue<ull, vector<ull>,  greater<ull>> minheap;
-ull cnt = 0;
+priority_queue<int, vector<int>, less<int>> maxheap;
+priority_queue<int, vector<int>, greater<int>> minheap;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -19,23 +16,27 @@ int main() {
 	for (int i = 0; i < n; ++i) {
 		int input;
 		cin >> input;
-		minheap.push(input);
+
+		//규칙 1
+		if (maxheap.size() == minheap.size()) maxheap.push(input);
+		else minheap.push(input);
+
+		//규칙 2
+		if ((!minheap.empty()) && (maxheap.top() > minheap.top())) {
+			int mx = maxheap.top();
+			int mn = minheap.top();
+
+			maxheap.pop();
+			minheap.pop();
+
+			maxheap.push(mn);
+			minheap.push(mx);
+		}
+
+		//중간값 출력
+		if (maxheap.size() % 2 == 1) cout << maxheap.top() << "\n";
+		else cout << min(maxheap.top(), minheap.top()) << "\n";
 	}
 
-	//가장 작은 두 카드 묶음 선택하여 합치기
-	while (true) {
-		if (minheap.size() == 1) 
-			break;
-		
-		ull a = minheap.top();
-		minheap.pop();
-		ull b = minheap.top();
-		minheap.pop();
-
-		cnt += (a + b);
-		minheap.push(a + b);
-	}
-
-	cout << cnt;
 	return 0;
 }
