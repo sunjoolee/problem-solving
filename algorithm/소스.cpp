d9ffce0e18c07d2be904e53a3aef89aa120a76a3
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-17 09:00:54 +0900

    백준 11279 최대 힙

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <set>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
-int n, m;
-//set: 균형 이진 트리 구조
-set <int> card;
+int n;
+priority_queue<int> maxheap;
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -16,18 +15,20 @@ int main() {
 	for (int i = 0; i < n; ++i) {
 		int input;
 		cin >> input;
-		card.insert(input);
+
+		//삽입
+		if (input > 0)
+			maxheap.push(input);
+
+		//최대 원소 출력 & 제거
+		else {
+			if (maxheap.empty())
+				cout << 0 <<"\n";
+			else {
+				cout << maxheap.top() << "\n";
+				maxheap.pop();
+			}
+		}
 	}
-
-	cin >> m;
-	for (int i = 0; i < m; ++i) {
-		int input;
-		cin >> input;
-
-		if (card.find(input) == card.end())
-			cout << "0 ";
-		else cout << "1 ";
-	}
-
 	return 0;
 }
