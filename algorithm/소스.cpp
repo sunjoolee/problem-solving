88c3721890449d406e6d85b7f965a329062dffad
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-18 19:08:01 +0900

    백준 15732 도토리 숨기기

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,68 +3,46 @@
 #include <algorithm>
 using namespace std;
 
-typedef unsigned long long ull;
-const ull MAX_TIME = (ull)2000000000 * (ull)30;
+const int MAX_BOX = 1000000;
 
-int N, M;
-int durtime[10001];
+int N, K, D;
+
+//<<시작 상자, 끝 상자>, 상자 간격>
+vector<pair<pair<int, int>, int>> rule;
 
 int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> M;
+	cin >> N >> K >> D;
 
-	for (int i = 0; i < M; i++)
-		cin >> durtime[i];
-
-	//사람 수가 놀이기구 수보다 작거나 같은 경우
-	//놀이기구 번호가 작은 것 부터 하나씩 차례대로 탑승한다
-	if (N <= M){
-		cout << N;
-		return 0;
+	for (int i = 0; i < K; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		rule.push_back(make_pair(make_pair(a, b), c));
 	}
 
-	//N명의 사람이 모두 놀이기구를 탑승하게 되는 최소 시간 (이분 탐색)
-	ull minTime = MAX_TIME;
-
-	ull low = 0;
-	ull high = MAX_TIME;
+	int low = 0;
+	int hi= MAX_BOX + 1;
 	for (int it = 0; it < 100; ++it) {
-		ull mid = (low + high) / 2;
+		int mid = (low + hi) / 2;
 
-		//결정 문제: x분동안 N명의 사람이 놀이기구에 탑승할 수 있는가?
-		//0초에 모든 놀이기구에 한 사람씩 탑승하므로 cnt = M부터 시작
-		ull cnt = M;
-		for (int i = 0; i < M; i++)
-			cnt += mid / durtime[i];
+		//결정 문제: 마지막 상자의 번호가 x일 때 D개의 도토리를 모두 넣을 수 있는가?
+		long long cnt = 0;
+		for (int i = 0; i < K; i++) {
+			int startBox = rule[i].first.first;
+			int endBox = rule[i].first.second;
+			int gap = rule[i].second;
 
-		if (cnt >= N) {
-			minTime =min(minTime, mid);
-			high = mid;
+			if (startBox > mid) continue;
+			else cnt += ((min(endBox, mid) - startBox) / gap ) + 1;
 		}
+
+		if (cnt >= D) hi = mid;
 		else low = mid;
 	}
 
-	//마지막 사람이 타는 놀이기구의 번호 구하기!
-	
-	//minTime - 1 시간까지 놀이기구에 탑승하게 되는 사람 수
-	ull cnt = M;
-	for (int i = 0; i < M; i++) 
-		cnt += (minTime - 1) / durtime[i];
-	
-
-	// minTime 시간에 놀이기구에 탑승하게 되는 사람 수
-	for (int i = 0; i < M; i++){
-		if (minTime % durtime[i] == 0)
-			++cnt;
-
-		if (cnt == N){
-			cout << i + 1;
-			return 0;
-		}
-	}
-
+	cout << hi;
 	return 0;
 }
