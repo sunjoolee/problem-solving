d151de9b9e9a2dd80ec2955a614e8cad4f515004
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-18 14:55:44 +0900

    백준 3020 개똥벌레

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
-#include <string>
-#include <cmath>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-typedef long long ll;
-
-//큰 수 덧셈 알고리즘
-string addString(string a, string b) {
-	ll carry = 0;
-	string result = "";
-
-	while (!a.empty() || !b.empty() || carry) {
-		if (!a.empty()) {
-			carry += a.back() - '0';
-			a.pop_back();
-		}
-
-		if (!b.empty()) {
-			carry += b.back() - '0';
-			b.pop_back();
-		}
-
-		result += ((carry % 10) + '0');
-		carry /= 10;
-	}
-
-	reverse(result.begin(), result.end());
-	return result;
-}
-
+int N, H;
+     
+vector<int> bottom; //석순
+vector<int> top;    //종유석
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	string a, b;
-	cin >> a >> b;
+	cin >> N >> H;
 
-	cout << addString(a, b);
+	for (int i = 0; i < N; ++i) {
+		int input;
+		cin >> input;
 
+		if (i % 2 == 0) bottom.push_back(input);
+		else top.push_back(input);
+	}
+
+	sort(bottom.begin(), bottom.end());
+	sort(top.begin(), top.end());
+
+	int minObstacle = N;
+	int cnt = 0;
+
+	//개똥벌레가 날아가는 구간의 높이 h
+	for (double h = 0.5; h < H; ++h) {
+		int obstacle = 0;
+		
+		//만나게 되는 석순의 개수
+		obstacle += bottom.size() - (lower_bound(bottom.begin(), bottom.end(), h) - bottom.begin());
+		
+		//만나게되는 종유석의 개수
+		obstacle += top.size() - (lower_bound(top.begin(), top.end(), H - h) - top.begin());
+
+		if (minObstacle > obstacle) {
+			minObstacle = obstacle;
+			cnt = 1;
+		}
+		else if (minObstacle == obstacle) {
+			++cnt;
+		}
+	}
+
+	cout << minObstacle << " " << cnt;
 	return 0;
-
 }
