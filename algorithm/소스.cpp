8381e71a778a112b46096ce28f29957edd648b00
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-18 18:14:07 +0900

    백준 1561 놀이공원

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,51 +3,68 @@
 #include <algorithm>
 using namespace std;
 
-int N, H;
-     
-vector<int> bottom; //석순
-vector<int> top;    //종유석
+typedef unsigned long long ull;
+const ull MAX_TIME = (ull)2000000000 * (ull)30;
 
-int main() {
+int N, M;
+int durtime[10001];
+
+int main(void){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N >> H;
+	cin >> N >> M;
 
-	for (int i = 0; i < N; ++i) {
-		int input;
-		cin >> input;
+	for (int i = 0; i < M; i++)
+		cin >> durtime[i];
 
-		if (i % 2 == 0) bottom.push_back(input);
-		else top.push_back(input);
+	//사람 수가 놀이기구 수보다 작거나 같은 경우
+	//놀이기구 번호가 작은 것 부터 하나씩 차례대로 탑승한다
+	if (N <= M){
+		cout << N;
+		return 0;
 	}
 
-	sort(bottom.begin(), bottom.end());
-	sort(top.begin(), top.end());
+	//N명의 사람이 모두 놀이기구를 탑승하게 되는 최소 시간 (이분 탐색)
+	ull minTime = MAX_TIME;
 
-	int minObstacle = N;
-	int cnt = 0;
+	ull low = 0;
+	ull high = MAX_TIME;
+	for (int it = 0; it < 100; ++it) {
+		ull mid = (low + high) / 2;
 
-	//개똥벌레가 날아가는 구간의 높이 h
-	for (double h = 0.5; h < H; ++h) {
-		int obstacle = 0;
-		
-		//만나게 되는 석순의 개수
-		obstacle += bottom.size() - (lower_bound(bottom.begin(), bottom.end(), h) - bottom.begin());
-		
-		//만나게되는 종유석의 개수
-		obstacle += top.size() - (lower_bound(top.begin(), top.end(), H - h) - top.begin());
+		//결정 문제: x분동안 N명의 사람이 놀이기구에 탑승할 수 있는가?
+		//0초에 모든 놀이기구에 한 사람씩 탑승하므로 cnt = M부터 시작
+		ull cnt = M;
+		for (int i = 0; i < M; i++)
+			cnt += mid / durtime[i];
 
-		if (minObstacle > obstacle) {
-			minObstacle = obstacle;
-			cnt = 1;
+		if (cnt >= N) {
+			minTime =min(minTime, mid);
+			high = mid;
 		}
-		else if (minObstacle == obstacle) {
+		else low = mid;
+	}
+
+	//마지막 사람이 타는 놀이기구의 번호 구하기!
+	
+	//minTime - 1 시간까지 놀이기구에 탑승하게 되는 사람 수
+	ull cnt = M;
+	for (int i = 0; i < M; i++) 
+		cnt += (minTime - 1) / durtime[i];
+	
+
+	// minTime 시간에 놀이기구에 탑승하게 되는 사람 수
+	for (int i = 0; i < M; i++){
+		if (minTime % durtime[i] == 0)
 			++cnt;
+
+		if (cnt == N){
+			cout << i + 1;
+			return 0;
 		}
 	}
 
-	cout << minObstacle << " " << cnt;
 	return 0;
 }
