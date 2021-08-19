6bb8f90d1936ba1ac9aa8e7651aa476d14cfc533
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-19 20:47:08 +0900

    백준 2292 벌집

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,33 +1,42 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 #include <algorithm>
 using namespace std;
-
-typedef unsigned long long ull;
-
-const int r = 31;
-const ull M = 1234567891;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	
+	int room;
+	cin >> room;
 
-	int L;
-	string str;
-	cin >> L >> str;
+	//1 ~ room번방까지 지나야하는 방의 개수 = room번방이 포함된 육각형의 번호
 
-	ull H = 0;
-	ull R = 1;
-	for (int i = 0; i < L; ++i) {
-		ull h = ((str[i] - 'a' + 1) * R) % M;
-
-		H = (H + h) % M;
-
-		R = (R * r) % M;
+	//1 ~ 1번방까지 이동하는 경우
+	if (room == 1) {
+		cout << 1;
+		return 0;
 	}
 
-	cout << H;
+	//육각형의 번호
+	int hex = 2;
+	//육각형에 포함된 방의 시작 번호
+	int start = 2;
+	//육각형에 포함된 방의 개수
+	int num = 6;
+
+	while (true) {
+		//현재 육각형에 방이 포함된 경우
+		if (start <= room && room <= start + num - 1) {
+			cout << hex;
+			return 0;
+		}
+		//현재 방이 포함되지 않았다면 다음 육각형으로 이동해야
+		++hex;
+		start += num;
+		num += 6;
+	}
+
 	return 0;
 }
