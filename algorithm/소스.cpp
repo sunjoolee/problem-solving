fd95d477774d9376e6592bf058d812335b1a5f29
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-19 20:11:00 +0900

    백준 15829 Hashing

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
+#include <string>
+#include <cmath>
+#include <algorithm>
 using namespace std;
+
+typedef unsigned long long ull;
+
+const int r = 31;
+const ull M = 1234567891;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	//arr[k][n]: k층 n호에 살고 있는 사람의 수
-	int arr[15][15] = { 0 };
+	int L;
+	string str;
+	cin >> L >> str;
 
-	//0층 i호에는 i명의 사람이 살고있다
-	for (int room = 0; room < 15; ++room)
-		arr[0][room] = room;
+	ull H = 0;
+	ull R = 1;
+	for (int i = 0; i < L; ++i) {
+		ull h = ((str[i] - 'a' + 1) * R) % M;
 
-	//1층 ~ 14층의 1 ~ 14호에 살고 있는 사람 수 계산
-	for (int floor = 1; floor < 15; ++floor) {
-		//1호에는 항상 1명의 사람이 살고 있다
-		arr[floor][0] = arr[floor][1] = 1;
+		H = (H + h) % M;
 
-		for (int room = 2; room < 15; ++room)
-			arr[floor][room] = arr[floor][room-1]+ arr[floor-1][room];
-	}
-	
-	int T;
-	cin >> T;
-	while (T--) {
-		int k, n;
-		cin >> k >> n;
-		cout << arr[k][n] << "\n";
+		R = (R * r) % M;
 	}
 
+	cout << H;
 	return 0;
 }
