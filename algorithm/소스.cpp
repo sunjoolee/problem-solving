88f443298bf88c8f751350faba243ea32582d4b0
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-19 19:20:14 +0900

    백준 2869 달팽이는 올라가고 싶다

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,66 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cmath>
 using namespace std;
 
-typedef long long ll;
-
-int N;
-vector<int> A;
-
-//A에서 이분 탐색으로 val값을 찾기 위해 필요한 비교 횟수 반환
-ll binary_search(int value, int left, int right) {
-	int mid = (left + right) / 2;
-
-	if (A[mid] == value)
-		return 0;
-
-	else if (value < A[mid])
-		return 1LL + binary_search(value, left, mid - 1);
-	else
-		return 1LL + binary_search(value, mid + 1, right);
-}
-
-
-//A에서 이분 탐색으로 val값을 찾기 위해 필요한 비교 횟수 반환
-ll ternary_search(int value, int left, int right) {
-	int left_third = left + ((right - left) / 3);
-	int right_third = right - ((right - left) / 3);
-
-	if (A[left_third] == value)
-		return 0;
-	else if (A[right_third] == value)
-		return 1LL;
-	else if (value < A[left_third])
-		return 2LL + ternary_search(value, left, left_third - 1);
-	else if (value < A[right_third])
-		return 2LL + ternary_search(value, left_third + 1, right_third - 1);
-	else
-		return 2LL + ternary_search(value, right_third + 1, right);
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
+	int A, B, V;
+	cin >> A >> B >> V;
 
-	for (int i = 0; i < N; ++i)
-		A.push_back(i);
-
-	int res1, res2, res3;
-	res1 = res2 = res3 = 0; 
-	for (int i = 0; i < N; ++i) {
-		ll B, T;
-		B = binary_search(A[i], 0, N - 1);
-		T = ternary_search(A[i], 0, N - 1);
-
-		if (B < T) ++res1;
-		else if (B == T) ++res2;
-		else if (B > T) ++res3;
-	}
+	//달팽이의 위치에서 정상까지 남은 높이가 A 이하일 때 
+	//다음날 정상에 도착할 수 있다 
 	
-	cout << res1 << "\n" << res2 << "\n" << res3;
+	// x번째 날이 종료된 후 달팽이 위치 = (A - B) * x	
+	// V - A <= (A - B) * x
+	// (V - A) / (A - B) <= x
+
+	cout << (int)ceil((double)(V - A) / (A - B)) + 1;
 	return 0;
 }
