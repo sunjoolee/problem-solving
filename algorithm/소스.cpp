698aea2270a5dea724dd3820f66a8444ce20d9bd
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-19 19:33:56 +0900

    백준 2775 부녀회장이 될테야

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int A, B, V;
-	cin >> A >> B >> V;
+	//arr[k][n]: k층 n호에 살고 있는 사람의 수
+	int arr[15][15] = { 0 };
 
-	//달팽이의 위치에서 정상까지 남은 높이가 A 이하일 때 
-	//다음날 정상에 도착할 수 있다 
+	//0층 i호에는 i명의 사람이 살고있다
+	for (int room = 0; room < 15; ++room)
+		arr[0][room] = room;
+
+	//1층 ~ 14층의 1 ~ 14호에 살고 있는 사람 수 계산
+	for (int floor = 1; floor < 15; ++floor) {
+		//1호에는 항상 1명의 사람이 살고 있다
+		arr[floor][0] = arr[floor][1] = 1;
+
+		for (int room = 2; room < 15; ++room)
+			arr[floor][room] = arr[floor][room-1]+ arr[floor-1][room];
+	}
 	
-	// x번째 날이 종료된 후 달팽이 위치 = (A - B) * x	
-	// V - A <= (A - B) * x
-	// (V - A) / (A - B) <= x
+	int T;
+	cin >> T;
+	while (T--) {
+		int k, n;
+		cin >> k >> n;
+		cout << arr[k][n] << "\n";
+	}
 
-	cout << (int)ceil((double)(V - A) / (A - B)) + 1;
 	return 0;
 }
