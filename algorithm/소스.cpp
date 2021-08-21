c1f73dd1eedc2b39cd5b6bd70fc92130f98185e6
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-22 08:54:49 +0900

    백준 1781 컵라면

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,42 +1,54 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
+
+typedef unsigned long long ull;
+
+int N;
+//<숙제 데드라인, 숙제 컵라면>
+vector<pair<int, int>> hw;
+
+//총 얻을 수 있는 컵라면의 수
+ull cnt = 0;
+priority_queue<int> maxheap;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	
-	int room;
-	cin >> room;
 
-	//1 ~ room번방까지 지나야하는 방의 개수 = room번방이 포함된 육각형의 번호
+	cin >> N;
 
-	//1 ~ 1번방까지 이동하는 경우
-	if (room == 1) {
-		cout << 1;
-		return 0;
+	for (int i = 0; i < N; ++i) {
+		int a, b;
+		cin >> a >> b;
+		hw.push_back(make_pair(a, b));
 	}
 
-	//육각형의 번호
-	int hex = 2;
-	//육각형에 포함된 방의 시작 번호
-	int start = 2;
-	//육각형에 포함된 방의 개수
-	int num = 6;
+	sort(hw.begin(), hw.end());
 
-	while (true) {
-		//현재 육각형에 방이 포함된 경우
-		if (start <= room && room <= start + num - 1) {
-			cout << hex;
-			return 0;
+	//deadline N부터 0까지 1씩 감소시키며 숙제 선택
+	for (int deadline = N; deadline > 0; --deadline) {
+
+		//hw 데드라인 오름차순으로 정렬됨 -> 역방향으로 순회
+		for (int i = hw.size()-1; i >= 0; --i) {
+			//데드라인이 deadline인 hw의 컵라면 수 maxheap에 추가
+			if (hw[i].first == deadline) {
+				maxheap.push(hw[i].second);
+				hw.pop_back();
+			}
+			else break;
 		}
-		//현재 방이 포함되지 않았다면 다음 육각형으로 이동해야
-		++hex;
-		start += num;
-		num += 6;
-	}
 
+		if (!maxheap.empty()) {
+			cnt += maxheap.top();
+			maxheap.pop();
+		}
+		
+	}
+	
+	cout << cnt;
 	return 0;
 }
