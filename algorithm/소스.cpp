4fb8bf1bb486a3aec2624363642df12af881966e
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-22 11:19:08 +0900

    백준 1826 연료 채우기

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,44 +1,67 @@
 #include <iostream>
-#include <string>
-#include <set>
 #include <vector>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
-int n, m;
-set<string> name;
-vector<string> ans;
+int n, l, p;
+//<방문 지점 위치, 방문 지점에서 얻을 수 있는 연료>
+vector<pair<int, int>> stop;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> n >> m;
+	cin >> n;
 
-	//듣도 못한 사람 명단 set 삽입
 	for (int i = 0; i < n; ++i) {
-		string input;
-		cin >> input;
-		name.insert(input);
+		int a, b;
+		cin >> a >> b;
+		stop.push_back(make_pair(a, b));
 	}
+	cin >> l >> p;
 
-	//보도 못한 사람 명단 set 삽입
-	for (int i = 0; i < m; ++i) {
-		string input; 
-		cin >> input;
-			
-		//중복된 문자열인 경우 set 삽입 실패 = 듣도 보도 못한 사람
-		auto it = name.insert(input);
-		if (!it.second) ans.push_back(input);
+	//출발 지점도 방문 지점에 포함
+	stop.push_back(make_pair(0, 0));
+	//마을도 방문 지점에 포함
+	stop.push_back(make_pair(l, 0));
+	//지점 위치 좌표 오름차순 정렬
+	sort(stop.begin(), stop.end());
+
+	//방문 주유소 수
+	int cnt = 0;
+	//현재 지점 & 지나쳐온 지점들에서 얻을 수 있는 연료 저장
+	priority_queue<int> pastStop;
+
+	int cur = 0;
+	while(cur < stop.size()){
+		//현재 위치 = 마을 위치가 되면 종료
+		if (stop[cur].first == l) {
+			cout << cnt;
+			return 0;
+		}
+
+		pastStop.push(stop[cur].second);
+		while (true) {
+			//다음 지점으로 이동 가능한 경우
+			if (stop[cur].first + p >= stop[cur+1].first) {
+				p -= (stop[cur + 1].first - stop[cur].first);
+				++cur;
+				break;
+			}
+			//다음 지점으로 이동 불가능한 경우
+			else {
+				if (pastStop.empty()) {
+					cout << -1;
+					return 0;
+				}
+				++cnt;
+				p += pastStop.top();
+				pastStop.pop();
+			}
+		}
 	}
-
-	//듣도 보도 못한 사람 명단 사전순 정렬
-	sort(ans.begin(), ans.end());
-
-	cout << ans.size() << "\n";
-	for (int i = 0; i < ans.size(); ++i)
-		cout << ans[i] << "\n";
 
 	return 0;
 }
