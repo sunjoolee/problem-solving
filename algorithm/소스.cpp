5686d848b8a9c357fa62a79d5efb7d6e29e3ec67
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-22 10:09:20 +0900

    백준 1764 듣보잡

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,54 +1,44 @@
 #include <iostream>
+#include <string>
+#include <set>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
-typedef unsigned long long ull;
-
-int N;
-//<숙제 데드라인, 숙제 컵라면>
-vector<pair<int, int>> hw;
-
-//총 얻을 수 있는 컵라면의 수
-ull cnt = 0;
-priority_queue<int> maxheap;
+int n, m;
+set<string> name;
+vector<string> ans;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> N;
+	cin >> n >> m;
 
-	for (int i = 0; i < N; ++i) {
-		int a, b;
-		cin >> a >> b;
-		hw.push_back(make_pair(a, b));
+	//듣도 못한 사람 명단 set 삽입
+	for (int i = 0; i < n; ++i) {
+		string input;
+		cin >> input;
+		name.insert(input);
 	}
 
-	sort(hw.begin(), hw.end());
-
-	//deadline N부터 0까지 1씩 감소시키며 숙제 선택
-	for (int deadline = N; deadline > 0; --deadline) {
-
-		//hw 데드라인 오름차순으로 정렬됨 -> 역방향으로 순회
-		for (int i = hw.size()-1; i >= 0; --i) {
-			//데드라인이 deadline인 hw의 컵라면 수 maxheap에 추가
-			if (hw[i].first == deadline) {
-				maxheap.push(hw[i].second);
-				hw.pop_back();
-			}
-			else break;
-		}
-
-		if (!maxheap.empty()) {
-			cnt += maxheap.top();
-			maxheap.pop();
-		}
-		
+	//보도 못한 사람 명단 set 삽입
+	for (int i = 0; i < m; ++i) {
+		string input; 
+		cin >> input;
+			
+		//중복된 문자열인 경우 set 삽입 실패 = 듣도 보도 못한 사람
+		auto it = name.insert(input);
+		if (!it.second) ans.push_back(input);
 	}
-	
-	cout << cnt;
+
+	//듣도 보도 못한 사람 명단 사전순 정렬
+	sort(ans.begin(), ans.end());
+
+	cout << ans.size() << "\n";
+	for (int i = 0; i < ans.size(); ++i)
+		cout << ans[i] << "\n";
+
 	return 0;
 }
