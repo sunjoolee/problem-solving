f02f3d0625127c13ea899b5449005d805a7df63c
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-25 15:15:47 +0900

    백준 14651 걷다보니 신천역 삼(Large)

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+typedef long long ll;
+const int MOD = 1000000009;
+
 int n;
-int cnt = 0;
+ll cache[33334];
 
 //digit-1번째 자리의 수까지의 합이 digitsum일 때, digit번째 수를 고른다
-void dp(int digit, int digitsum) {
+ll dp(int digit, int digitsum) {
 	//N번째 수까지 모두 완성한 경우
 	if (digit > n) {
-		if (digitsum == 0) ++cnt;
-		return;
+		if (digitsum == 0) return 1;
+		else return 0;
 	}
 
-	//digit번째 자리의 수 0
-	dp(digit + 1, digitsum);
-	//digit번째 자리의 수 1
-	dp(digit + 1, (digitsum + 1 )% 3);
-	//digit번째 자리의 수 2
-	dp(digit + 1, (digitsum + 2) % 3);
+	ll& ret = cache[digit];
+	if (ret != -1) return ret;
 
-	return;
+	ret = dp(digit + 1, digitsum);
+	ret %= MOD;
+	ret += dp(digit + 1, (digitsum + 1 )% 3);
+	ret %= MOD;
+	ret += dp(digit + 1, (digitsum + 2) % 3);
+	ret %= MOD;
+
+	return ret;
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
+	
+	memset(cache, -1, sizeof(cache));
 
 	cin >> n;
 
-	//첫번째 자리의 수 1
-	dp(2, 1);
-	//첫번째 자리의 수 2
-	dp(2, 2);
-
-	cout << cnt;
+	ll res = dp(2, 1) + dp(2, 2);
+	cout << res % MOD;
 	return 0;
 }
