33a5a306fc52c60cbcb0e79194dcf2318661d79f
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-25 21:45:56 +0900

    백준 1275 커피숍2
    Solution using Fenwick Tree

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,83 +1,36 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
 #include <algorithm>
 using namespace std;
 
 typedef long long ll;
 
-//N: 수의 개수
-//Q: 턴의 수
 int N, Q;
-vector<ll> arr;
 
-struct SegmentTree {
+struct FenwickTree {
 	vector<ll> tree;
 
-	//구간의 합 계산
-	ll merge(ll left, ll right) {
-		return left + right;
+	FenwickTree(int size) : tree(size + 1) {}
+
+	ll sum(int pos) {
+		ll ret = 0LL;
+		while (pos > 0) {
+			ret += tree[pos];
+
+			//0이 아닌 마지막 비트만큼 빼면서 구간들의 값 변경
+			pos -= (pos & -pos);
+		}
+		return ret;
 	}
 
-	ll buildRecursive(int node, int nodeLeft, int nodeRight) {
-		if (nodeLeft == nodeRight)
-			return tree[node] = arr[nodeLeft];
-
-		int mid = (nodeLeft + nodeRight) / 2;
-		ll leftVal = buildRecursive(node * 2, nodeLeft, mid);
-		ll rightVal = buildRecursive(node * 2 + 1, mid + 1, nodeRight);
-
-		return tree[node] = merge(leftVal, rightVal);
+	void add(int pos, ll val) {
+		while (pos < tree.size()) {
+			tree[pos] += val;
+			//0이 아닌 마지막 비트만큼 더하면서 구간들의 값 변경
+			pos += (pos & -pos);
+		}
 	}
-
-	void build() {
-		tree.resize(N * 4);
-		buildRecursive(1, 0, N - 1);
-	}
-
-	ll updateRecursive(int index, ll newVal, int node, int nodeLeft, int nodeRight) {
-		if (index < nodeLeft || nodeRight < index)
-			return tree[node];
-
-		if (nodeLeft == nodeRight)
-			return tree[node] = newVal;
-
-		int mid = (nodeLeft + nodeRight) / 2;
-		ll leftVal = updateRecursive(index, newVal, node * 2, nodeLeft, mid);
-		ll rightVal = updateRecursive(index, newVal, node * 2 + 1, mid + 1, nodeRight);
-
-		return tree[node] = merge(leftVal, rightVal);
-	}
-
-	void update(int index, ll newVal) {
-		updateRecursive(index, newVal, 1, 0, N - 1);
-	}
-
-
-	ll queryRecursive(int left, int right, int node, int nodeLeft, int nodeRight) {
-		//쿼리의 구간에 포함되지 않는 구간인 경우 default 값 반환 
-		//default 값은 merge 연산에 따라 다르다
-		if (right < nodeLeft || nodeRight < left)
-			return 0LL;
-
-		//쿼리의 구간에 포함되는 구간인 경우
-		if (left <= nodeLeft && nodeRight <= right)
-			return tree[node];
-
-		//쿼리의 구간에 부분적으로 포함되는 구간인 경우
-		int mid = (nodeLeft + nodeRight) / 2;
-		ll leftVal = queryRecursive(left, right, node * 2, nodeLeft, mid);
-		ll rightVal = queryRecursive(left, right, node * 2 + 1, mid + 1, nodeRight);
-
-		return merge(leftVal, rightVal);
-	}
-
-	ll query(int left, int right) {
-		return queryRecursive(left, right, 1, 0, N - 1);
-	}
-
 };
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -86,25 +39,26 @@ int main() {
 
 	cin >> N >> Q;
 
-	for (int i = 0; i < N; ++i) {
+	FenwickTree fenwickTree(N);
+
+	for (int i = 1; i <= N; ++i) {
 		ll input;
 		cin >> input;
-		arr.push_back(input);
+		fenwickTree.add(i, input);
 	}
 
-	SegmentTree segmentTree;
-	segmentTree.build();
-
-	for (int i = 0; i < Q; ++i) {
-		int x, y;
+	while(Q--) {
+		int x, y; 
 		cin >> x >> y;
 		
-		if(x > y) cout << segmentTree.query(y - 1, x - 1) << "\n";
-		else cout << segmentTree.query(x-1, y-1) << "\n";
+		if(x > y) cout << fenwickTree.sum(x) - fenwickTree.sum(y - 1) << "\n";
+		else cout << fenwickTree.sum(y) - fenwickTree.sum(x - 1) << "\n";
 
 		int a; ll b;
 		cin >> a >> b;
-		segmentTree.update(a-1, b);
+
+		b = b - (fenwickTree.sum(a) - fenwickTree.sum(a - 1));
+		fenwickTree.add(a, b);
 	}
 
 	return 0;
