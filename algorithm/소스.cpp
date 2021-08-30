0d5b73d50d1180fc5bc666f119c11c7aa47a2117
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-30 13:59:52 +0900

    백준 4195 친구 네트워크

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,76 +1,70 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <string>
 #include <algorithm>
 using namespace std;
 
-//최적화된 상호 배타적 집합의 구현
-//parent[i].first = i의 부모 노드의 번호
-//parent[i].second = i가 루트 노드일 경우, 사이클인 경우 false, 사이클이 아닌 경우 true 저장
-vector<pair<int, bool>> parent;
-vector<int> rank_;
 
-int find(int u) {
-	if (u == parent[u].first) return u;
-	return parent[u].first = find(parent[u].first);
-}
+struct OptimizedDisjointSet {
+	vector<int> parent, rank, size;
 
-void merge(int u, int v) {
-	u = find(u); v = find(v);
-	if (u == v) { 
-		//u와 v가 이미 같은 연결 요소에 포함되어 있는 경우 = 사이클
-		parent[u].second = false;
-		parent[v].second = false;
-		return; 
+	OptimizedDisjointSet(int n) : parent(n), rank(n, 1), size(n, 1) {
+		for (int i = 0; i < n; ++i)
+			parent[i] = i;
 	}
 
-	//연결 요소 + 사이클 = 사이클
-	if (parent[u].second == false) parent[v].second = false;
-	if (parent[v].second == false) parent[u].second = false;
+	int find(int u) {
+		if (u == parent[u]) return u;
+		return parent[u] = find(parent[u]);
+	}
 
-	if (rank_[u] > rank_[v]) swap(u, v);
-	parent[u].first = v;
-	if (rank_[u] == rank_[v]) ++rank_[v];
-}
+	//u가 포함된 상호 배타적 집합 + v가 포함된 상호 배타적 집합
+	//의 결과인 상호 배타적 집합의 크기 반환
+	int merge(int u, int v) {
+		u = find(u); v = find(v);
+		if (u == v) return size[u];
 
+		if (rank[u] > rank[v]) swap(u, v);
+		parent[u] = v;
+		if (rank[u] == rank[v]) ++rank[v];
+
+		size[v] += size[u];
+		return size[v];
+	}
+};
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int caseNo = 1;
-	while (true) {
-		int n, m;
-		cin >> n >> m;
-		if (n == 0 && m == 0) break;
+	int t;
+	cin >> t;
+	while (t--) {
+		//친구 관계의 수
+		int f;
+		cin >> f;
 
-		//초기화
-		parent.clear();
-		rank_.clear();
-		for (int i = 0; i < n; ++i) {
-			parent.push_back(make_pair(i, true));
-			rank_.push_back(1);
+		map <string, int> name2node;
+		OptimizedDisjointSet ods(2 * f);
+
+		int nodeNo = 0;
+
+		for (int i = 0; i < f; ++i) {
+			string name1, name2;
+			cin >> name1 >> name2;
+
+			auto it = name2node.find(name1);
+			if (it == name2node.end()) 
+				name2node[name1] = nodeNo++;
+
+			it = name2node.find(name2);
+			if (it == name2node.end()) 
+				name2node[name2] = nodeNo++;
+
+			cout << ods.merge(name2node[name1], name2node[name2]) << "\n";
 		}
-
-		for (int i = 0; i < m; ++i) {
-			int a, b;
-			cin >> a >> b;
-			merge(a - 1, b - 1);
-		}
-
-		//사이클이 아닌 연결 요소의 개수
-		int cnt = 0;
-		for (int node = 0; node < n; ++node) {
-			if (node == parent[node].first && parent[node].second) ++cnt;
-		}
-
-		cout << "Case "<<caseNo<<": ";
-		if (cnt == 0) cout << "No trees.\n";
-		else if (cnt == 1) cout << "There is one tree.\n";
-		else cout << "A forest of " << cnt << " trees.\n";
-
-		++caseNo;
 	}
-
 	return 0;
 }
