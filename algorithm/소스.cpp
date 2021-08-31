b1464e2e353e046311198342c374bd8df9bb5b09
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-08-31 15:57:36 +0900

    백준 1708 블록 껍질

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 typedef long long ll;
 typedef pair<ll, ll> dot;
-typedef vector<dot> polygon;
 
+int N;
 vector<dot> dots;
 
 int ccw(dot a, dot b) {
@@ -44,7 +44,7 @@ bool cmp(dot& a, dot& b) {
 	//외적 결과가 0일 때(평행) 
 	//Y좌표가 낮으면 (같다면 X좌표가 낮으면) true 반환
 	if (a0.second != b0.second)
-		return a0.second < b0.second ;
+		return a0.second < b0.second;
 	return a0.first < b0.first;
 }
 
@@ -72,11 +72,11 @@ void quickSortByAngle(int first, int last) {
 	return;
 }
 
-polygon convexHull() {
+void convexHull() {
 	//1. 기준점 찾기
 	//가장 Y좌표가 낮은 (같다면 X좌표가 낮은)점을 기준으로 한다
 	int minX = 1000000000, minY = 1000000000, minIdx = 0;
-	for (int i = 0; i < dots.size(); i++) {
+	for (int i = 0; i < N; i++) {
 		if (minY > dots[i].second || (minY == dots[i].second && minX > dots[i].first)) {
 			minX = dots[i].first;
 			minY = dots[i].second;
@@ -87,17 +87,17 @@ polygon convexHull() {
 	swap(dots[minIdx], dots[0]);
 
 	//2. 모든 노드를 기준점에 대해 반시계 방향으로 정렬
-	quickSortByAngle(1, dots.size() - 1);
+	quickSortByAngle(1, N - 1);
 
 	//3. 기준점부터 시작해서, 순서대로 모든 노드에 대해서 좌회전 여부를 검사
 	// 좌회전에 해당하는 선분만 찾은 것 = Convex Hull
-	vector<int> stack(dots.size());
+	vector<int> stack(N);
 	int idx = -1;
 	stack[++idx] = 0;
 	stack[++idx] = 1;
 
 	int next = 2;
-	while (next < dots.size()) {
+	while (next < N) {
 		while ((idx + 1) >= 2) {
 			int second = stack[idx--];
 			int first = stack[idx];
@@ -110,61 +110,22 @@ polygon convexHull() {
 		stack[++idx] = next++;
 	}
 
-	//4. 볼록 껍질 반환
-	polygon hull;
-	for (int i = 0; i < idx + 1; ++i)
-		hull.push_back(dots[stack[i]]);
-	
-	return hull;
-}
+	//convex hull에 포함된 점의 개수
+	cout << idx + 1 << "\n";
 
-bool isInside(dot q, const polygon& p) {
-	int crosses = 0;
-	for (int i = 0; i < p.size(); ++i) {
-		int j = (i + 1) % p.size();
-		if (p[i].second > q.second != p[j].second > q.second) {
-			double atX = (p[j].first - p[i].first)*(q.second - p[i].second) / (p[j].second - p[i].second) + p[i].first;
-			if (q.first < atX) {
-				++crosses;
-			}
-		}
-	}
-	return crosses % 2 > 0;
+	return;
 }
-
 
 int main(void) {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int N;
 	cin >> N;
-
-	dot cow;
-	cin >> cow.first >> cow.second;
-
 	for (int i = 0; i < N; i++) {
 		ll x, y;
 		cin >> x >> y;
 		dots.push_back(make_pair(x, y));
 	}
-	
-	int cnt = 0;
-	while (true) {
-		//더이상 볼록 껍질을 만들 수 없으면 종료
-		if (dots.size() < 3) break;
-
-		polygon hull = convexHull();
-		//볼록 껍질 내부에 소가 있는지 확인
-		if (isInside(cow, hull)) {
-			++cnt;
-			//dots에서 hull에 포함된 점 제거
-			for (int i = 0; i < hull.size(); ++i)
-				dots.erase(find(dots.begin(), dots.end(), hull[i]));
-		}
-		else break;
-	}
-
-	cout << cnt;
+	convexHull();
 	return 0;
 }
