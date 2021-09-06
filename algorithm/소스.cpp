be81bb83f133007a6037bb270d2b5a81eee6b797
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-09-06 22:19:48 +0900

    백준 9240 로버트 후드

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,76 +1,191 @@
 #include <iostream>
+#include <math.h>
 #include <vector>
 #include <utility>
 #include <algorithm>
 using namespace std;
 
-typedef long long ll;
-typedef pair<ll, ll> dot;
+//죄표의 최댓값
+const double MAXX = 1001;
+
+typedef pair<double, double> dot;
 typedef vector<dot> polygon;
 
-int ccw(dot p, dot a, dot b) {
+double ccw(dot a, dot b) {
+	return a.first * b.second - a.second * b.first;
+}
+
+double ccw(dot p, dot a, dot b) {
 	a.first -= p.first; a.second -= p.second;
 	b.first -= p.first; b.second -= p.second;
-	
-	ll cross = a.first * b.second - a.second * b.first;
-
-	if (cross > 0LL) return 1;
-	else if (cross < 0LL) return -1;
-	else return 0;
+	return ccw(a, b);
 }
 
-bool isInside(dot q, polygon& p){
+bool leftTurn(dot p, dot a, dot b) {
+	return ccw(p, a, b) > 0;
+}
+
+double norm(dot a) {
+	return hypot(a.first, a.second);
+}
+
+double dot_product(dot a, dot rhs) {
+	return a.first * rhs.first + a.second * rhs.second;
+}
+
+dot normalize(dot a) {
+	return { a.first / norm(a), a.second / norm(a) };
+}
+
+
+//1. 그라함 스캔
+
+//퀵 소트를 위한 비교 함수
+bool cmp(vector<dot>& dots, dot& a, dot& b) {
+	double cross = ccw(dots[0], a, b);
+	
+	//세 점이 한 직선 위에 있지 않은 경우 반시계 방향으로 정렬
+	if (cross != 0)
+		return (cross > 0);
+
+	//세 점이 한 직선 위에 있는 경우 p[0]와 가까운 점이 앞에 오도록 정렬
+	if (norm({dots[0].first - a.first, dots[0].second - a.second }) < norm({dots[0].first - b.first, dots[0].second - b.second}))
+		return true;
+	return false;
+}
+
+//모든 노드를 기준점에 대해 반시계 방향으로 정렬(퀵 소트)
+void quickSortByAngle(vector<dot>& dots, int first, int last) {
+	if (first >= last) return;
+
+	int pivot = first;
+	int i = first + 1;
+	int j = last;
+
+	while (i <= j) {
+		while (i <= last && cmp(dots, dots[i], dots[pivot])) i++;
+		while (j > first && !cmp(dots, dots[j], dots[pivot])) j--;
+
+		if (i >= j) break;
+
+		swap(dots[i], dots[j]);
+	}
+	swap(dots[pivot], dots[j]);
+
+	quickSortByAngle(dots, first, j - 1);
+	quickSortByAngle(dots, j + 1, last);
+	return;
+}
+
+polygon convexHull(vector<dot>& dots) {
+	double minX = MAXX, minY = MAXX;
+	int minIdx = 0;
+
+	//y좌표가 가장 작은 점 (y좌표가 같다면 x좌표가 가장 작은 점) 
+	for (int i = 0; i < dots.size(); i++) {
+		if (minY > dots[i].second || (minY == dots[i].second && minX > dots[i].first)) {
+			minX = dots[i].first;
+			minY = dots[i].second;
+			minIdx = i;
+		}
+	}
+	swap(dots[minIdx], dots[0]);
+
+	quickSortByAngle(dots, 1, dots.size() - 1);
+
+	vector<int> stack(dots.size());
+	int idx = -1;
+	stack[++idx] = 0;
+	stack[++idx] = 1;
+
+	int next = 2;
+	while (next < dots.size()) {
+		while ((idx + 1) >= 2) {
+			int second = stack[idx--];
+			int first = stack[idx];
+
+			if (leftTurn(dots[first], dots[second], dots[next])) {
+				stack[++idx] = second;
+				break;
+			}
+		}
+		stack[++idx] = next++;
+	}
+
+	//볼록 껍질에 포함되게 되는 점의 개수 = idx + 1
+	polygon hull;
+	for (int i = 0; i < idx + 1; ++i)
+		hull.push_back(dots[stack[i]]);
+
+	return hull;
+}
+
+//2. 회전하는 캘리퍼스 
+
+//시계 반대 방향으로 주어진 볼록 다각형 p
+double diameter(const polygon& p) {
 	int n = p.size();
 
-	//1. 점 q가 p내부에 있기 위한 첫번째 조건
-	if (ccw(p[0], p[1], q) < 0) return false;
-	if (ccw(p[0], p[n-1], q) > 0) return false;
+	int left = min_element(p.begin(), p.end()) - p.begin();
+	int right = max_element(p.begin(), p.end()) - p.begin();
 
-	//2. 점 q가 p의 내부 중 어느 구간에 있는지 이분 탐색
-	int lo = 1, hi = n - 1;
-	while (lo + 1 < hi) {
-		int mid = (lo + hi) / 2;
+	dot calipersA = { 0, 1 };
+	double ret = norm({ p[right].first - p[left].first, p[right].second - p[left].second });
 
-		if (ccw(p[0],p[mid], q) > 0) 
-			lo = mid;
-		else 
-			hi = mid;
+	vector<dot> toNext(n);
+	for (int i = 0; i < n; ++i)
+		toNext[i] = normalize({ p[(i + 1) % n].first - p[i].first, p[(i + 1) % n].second - p[i].second });
+
+	int a = left, b = right;
+	while (a != right || b != left) {
+		double cosThetaA = dot_product(calipersA, toNext[a]);
+		double cosThetaB = dot_product({ -calipersA.first, -calipersA.second }, toNext[b]);
+
+		if (cosThetaA > cosThetaB) {
+			calipersA = toNext[a];
+			a = (a + 1) % n;
+		}
+		else {
+			calipersA = { -toNext[b].first, -toNext[b].second };
+			b = (b + 1) % n;
+		}
+		ret = max(ret, norm({ p[a].first - p[b].first , p[a].second - p[b].second }));
 	}
 
-	//3. 점 q가 p내부에 있기 위한 마지막 조건
-	return ccw(p[lo], q, p[hi]) < 0;
+	return ret;
 }
 
-int main() {
-	ios_base::sync_with_stdio(false);
+int main(void) {
 	cin.tie(NULL);
-	cout.tie(NULL);
+	ios::sync_with_stdio(false);
 
-	int n, m, k;
-	cin >> n >> m >> k;
+	int C;
+	cin >> C;
 
-	//다각형의 점 반시계 방향으로 주어진다
-	polygon a, b;
-	ll x, y;
-	for (int i = 0; i < n; ++i) {
+	vector<dot> dots;
+
+	for (int i = 0; i < C; ++i) {
+		double x, y;
 		cin >> x >> y;
-		a.push_back({ x, y });
-	}
-	for (int i = 0; i < m; ++i) {
-		cin >> x >> y;
-		b.push_back({ x, y });
+		dots.push_back({ x,y });
 	}
 
-	int wrong = 0;
-	for (int i = 0; i < k; ++i) {
-		cin >> x >> y;
-		dot q = { x, y };
+	//소수점 아래 7자리까지 출력
+	cout << fixed;
+	cout.precision(7);
 
-		if (!isInside(q, a) || isInside(q, b)) ++wrong;
+	if (C < 3) {
+		double ret = 0.0;
+		for (int i = 0; i < C; ++i) {
+			for (int j = 0; j < C; ++j) {
+				ret = max(ret, norm({ dots[i].first - dots[j].first, dots[i].second - dots[j].second }));
+			}
+		}
+		cout << ret;
+		return 0;
 	}
 
-	if (wrong == 0) cout << "YES";
-	else cout << wrong;
-
+	polygon hull = convexHull(dots);
+	cout << diameter(hull);
 	return 0;
 }
