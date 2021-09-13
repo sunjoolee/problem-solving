194a231ee7aa799fc19973de4483f7ec29effd9d
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-09-13 15:38:17 +0900

    백준 5052 전화번호 목록

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,26 +1,47 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <cstring>
+#include <string>
 #include <algorithm>
 using namespace std;
 
-struct OptimizedDisjointSet {
-	vector<int> parent, rank;
+//주어지는 문자열은 0 ~ 9 숫자로 이루어짐 
+const int ALPHABETS = 10;
+int toNumber(char ch) { return ch - '0'; }
 
-	OptimizedDisjointSet(int n) : parent(n), rank(n, 1) {
-		for (int i = 0; i < n; ++i)
-			parent[i] = i;
-	}
-	int find(int u) {
-		if (u == parent[u]) return u;
-		return parent[u] = find(parent[u]);
-	}
-	void merge(int u, int v) {
-		u = find(u); v = find(v);
-		if (u == v) return;
+struct TrieNode {
+	TrieNode* children[ALPHABETS];
+	int terminal;
 
-		if (rank[u] > rank[v]) swap(u, v);
-		parent[u] = v;
-		if (rank[u] == rank[v]) ++rank[v];
+	TrieNode() : terminal(false) {
+		memset(children, 0, sizeof(children));
+	}
+	~TrieNode(){
+		for (int i = 0; i < ALPHABETS; ++i) {
+			if (children[i]) delete children[i];
+		}
+	}
+
+	void insert(const char* key) {
+		if (*key == 0) terminal = true;
+		else {
+			int next = toNumber(*key);
+			if (children[next] == NULL)
+				children[next] = new TrieNode();
+
+			children[next]->insert(key + 1);
+		}
+	}
+
+	TrieNode* find(const char* key) {
+		if (*key == 0) return this;
+		
+		int next = toNumber(*key);
+		if (children[next] == NULL)
+			return NULL;
+
+		return children[next]->find(key + 1);
 	}
 };
 
@@ -29,35 +50,40 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n, m;
-	cin >> n >> m;
+	int T;
+	cin >> T;
+	while(T--){
+		int N;
+		cin >> N;
 
-	OptimizedDisjointSet ods(n);
+		vector<string> inputs;
+		TrieNode* root = new TrieNode();
 
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			int input;
+		for (int i = 0; i < N; ++i) {
+			string input;
 			cin >> input;
-
-			if (input == 1) 
-				ods.merge(i, j);
+			
+			inputs.push_back(input);
+			root->insert(input.c_str());
 		}
-	}
 
-	bool flag = true;
-	int start, dest;
-
-	cin >> start;
-	for(int i = 1; i<m; ++i){
-		cin >> dest;
-		if (ods.find(start - 1) != ods.find(dest - 1)) {
-			flag = false;
-			break;
+		bool flag = true;
+		for (int i = 0; i < N; ++i) {
+			TrieNode* ret = root->find(inputs[i].c_str());
+			
+			if (ret != NULL) {
+				for (int next = 0; next < 10; next++) {
+					if (ret->children[next]) {
+						flag = false;
+						break;
+					}
+				}
+				if (!flag) break;
+			}
 		}
-		start = dest;
-	}
 
-	if (flag) cout << "YES";
-	else cout << "NO";
+		if (flag)cout << "YES\n";
+		else cout << "NO\n";
+	}
 	return 0;
 }
