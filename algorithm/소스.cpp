f3585bc0d425476d28f03d36e0d8ca3a96a5dd44
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-09-13 18:27:05 +0900

    백준 14725 개미굴

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,73 +1,86 @@
 #include <iostream>
 #include <vector>
-#include <cstring>
+#include <queue>
 #include <string>
 #include <algorithm>
 using namespace std;
 
-//주어지는 문자열은 알파벳 소문자로만 이루어짐
-const int ALPHABETS = 26;
-int toNumber(char ch) { return ch - 'a'; }
-
 struct TrieNode {
-	TrieNode* children[ALPHABETS];
-	int terminal;
+	string food;
+	vector<TrieNode*> children;
 
-	TrieNode() : terminal(false) {
-		memset(children, 0, sizeof(children));
-	}
-	~TrieNode() {
-		for (int i = 0; i < ALPHABETS; ++i) {
-			if (children[i]) delete children[i];
-		}
-	}
+	void insert(const vector<string>& key, int index) {
+		if (index == key.size()) return;
 
-	void insert(const char* key) {
-		if (*key == 0) terminal = true;
 		else {
-			int next = toNumber(*key);
-			if (children[next] == NULL)
-				children[next] = new TrieNode();
+			string nextFood = key[index];
 
-			children[next]->insert(key + 1);
+			for (int i = 0; i < children.size(); ++i) {
+				if (children[i]->food == nextFood) {
+					children[i]->insert(key, index + 1);
+					return;
+				}
+			}
+
+			TrieNode* t = new TrieNode();
+			t->food = nextFood;
+			this->children.push_back(t);
+
+			t->insert(key, index + 1);
+			return;
 		}
 	}
 
-	TrieNode* find(const char* key) {
-		if (*key == 0) return this;
-
-		int next = toNumber(*key);
-		if (children[next] == NULL)
-			return NULL;
-
-		return children[next]->find(key + 1);
-	}
 };
 
+bool cmp(TrieNode* t1, TrieNode* t2) {
+	if (t1->food < t2->food) return true;
+	else return false;
+}
+
+void dfs(TrieNode* root, int dep) {
+
+	if (dep > 0) {
+		for (int i = 1; i < dep; ++i) {
+			cout << "--";
+		}
+		cout << root->food << "\n";
+	}
+	
+	sort(root->children.begin(), root->children.end(), cmp);
+
+	for (int i = 0; i < root->children.size(); ++i) {
+		TrieNode* child = root->children[i];
+		dfs(child, dep+1);
+	}
+	return;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int N, M;
-	cin >> N >> M;
+	int N;
+	cin >> N;
 
-	string input;
 	TrieNode* root = new TrieNode();
+	root->food = "";
+
 	for (int i = 0; i < N; ++i) {
-		cin >> input;
-		root->insert(input.c_str());
+		int K;
+		cin >> K;
+
+		vector<string> inputFood;
+		for (int j = 0; j < K; ++j) {
+			string input;
+			cin >> input;
+			inputFood.push_back(input);
+		}
+		root->insert(inputFood, 0);
 	}
 
-	int cnt = 0;
-	for (int i = 0; i < M; ++i) {
-		cin >> input;
-		TrieNode* ret = root->find(input.c_str());
+	dfs(root, 0);
 
-		if (ret != NULL && ret->terminal) ++cnt;
-	}
-
-	cout << cnt;
 	return 0;
 }
