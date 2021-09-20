3335ae90ddab6e863f7bd0b4e97e1199713cb043
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-09-20 17:00:34 +0900

    백준 11724 연결 요소의 개수

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,27 +3,27 @@
 #include <algorithm>
 using namespace std;
 
-int V, E;
+int N, M;
 
-vector<vector<int>> adj(3001);
+vector<vector<int>> adj(1001);
 vector<bool> visited;
 
 void dfs(int here) {
 	visited[here] = true;
 
-	for (int i = 0; i < adj[here].size(); ++i) { 
+	for (int i = 0; i < adj[here].size(); ++i) {
 		int there = adj[here][i];
-		if (!visited[there]) 
+		if (!visited[there])
 			dfs(there);
 	}
 }
 
 int dfsAll() {
-	visited = vector<bool>(V, false);
+	visited = vector<bool>(N, false);
 
 	//모든 정점이 방문될 때까지 dfs를 호출해야하는 횟수
 	int cnt = 0;
-	for (int i = 0; i < V; ++i) {
+	for (int i = 0; i < N; ++i) {
 		if (!visited[i]) {
 			dfs(i);
 			++cnt;
@@ -37,9 +37,8 @@ int main() {
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	cin >> V >> E;
-
-	for (int i = 0; i < E; ++i) {
+	cin >> N >> M;
+	for (int i = 0; i < M; ++i) {
 		int a, b;
 		cin >> a >> b;
 
@@ -47,23 +46,6 @@ int main() {
 		adj[b - 1].push_back(a - 1);
 	}
 
-	//모든 간선 하나의 컴포넌트에 포함되어 있는가
-	int component = dfsAll();
-	if (component > 1) {
-		cout << "NO";
-		return 0;
-	}
-
-	//오일러 서킷/트레일 존재하는지 검사
-	//그래프의 모든 정점이 짝수점 -> 오일러 서킷 존재
-	//그래프의 두 정점 홀수점 & 나머지 모든 정점 짝수점 -> 오일러 트레일 존재
-	int cnt = 0;
-	for (int k = 0; k < V; ++k) {
-		if (adj[k].size() % 2) ++cnt;
-	}
-
-	if (cnt == 0 || cnt == 2) cout << "YES";
-	else cout << "NO";
-	
+	cout << dfsAll();
 	return 0;
 }
