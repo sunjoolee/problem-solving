35a6ffef7a7a2acb929991e6d0d3d1d7e9f0cf01
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-09-26 17:48:06 +0900

    백준 18250 철도 여행

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,99 +1,76 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
 using namespace std;
 
-//알파벳의 각 글자에 대한 인접 행렬 표현
-//adj[i][j]: 글자 i가 글자 j보다 먼저 와야함을 나타낸다
+//도시의 개수, 두 도시를 연결하는 철도의 개수
+int n, m;
+
+//그래프의 인접 리스트 표현
 vector<vector<int>> adj;
 
-void makeGraph(const vector<string>& words) {
-	//adj 초기화
-	adj = vector<vector<int>>(26, vector<int>(26, 0));
+//dfsAll을 통해 방문한 정점
+vector<int> visited;
 
-	for (int j = 1; j < words.size(); ++j) {
-		int i = j - 1;
-		int len = min(words[i].size(), words[j].size());
+//dfs를 통해 방문한 컴포넌트에 포함된 정점
+vector<int> component;
 
-		for (int k = 0; k < len; ++k) {
-			if (words[i][k] != words[j][k]) {
-				int a = words[i][k] - 'a';
-				int b = words[j][k] - 'a';
-				adj[a][b] = 1;
-				break;
-			}
-		}
-	}
-}
+//철도 여행 횟수
+int cnt = 0;
 
-//깊이 우선 탐색을 이용한 위상 정렬
-vector<int> seen, order;
-
-//깊이 우선 탐색을 진행하며 dfs종료 순서 기록
 void dfs(int here) {
-	seen[here] = 1;
-	for (int there = 0; there < 26; ++there) {
-		if (adj[here][there] && !seen[there])
+	visited[here] = 1;
+
+	for (int i = 0; i < adj[here].size(); ++i) {
+		int there = adj[here][i];
+		if (!visited[there])
 			dfs(there);
 	}
-	order.push_back(here);
+	component.push_back(here);
 }
 
-//adj에 주어진 그래프 위상 정렬한 결과 반환
-//그래프가 DAG가 아니라면 빈 벡터 반환
-vector<int> topologicalSort() {
-	int n = adj.size();
-	seen = vector<int>(n, 0);
-	order.clear();
+void dfsAll() {
+	visited = vector<int>(n, 0);
 
-	//dfsAll
 	for (int i = 0; i < n; ++i) {
-		if (!seen[i]) dfs(i);
-	}
-	//dfs 종료 역순으로 뒤집기
-	reverse(order.begin(), order.end());
+		if (!visited[i]) {
+			component.clear();
+			dfs(i);
 
-	//만약 정렬 결과에 역방향 간선이 있다면 DAG가 아니다
-	for (int i = 0; i < n; ++i) {
-		for (int j = i + 1; j < n; ++j) {
-			if (adj[order[j]][order[i]]) return vector<int>();
+			//철도가 연결되지 않은 도시인 경우 철도 여행 횟수에 포함되지 않는다
+			if (component.size() == 1 && adj[i].size() == 0) continue;
+
+			//방문한 컴포넌트의 모든 정점을 방문하기 위해 필요한 철도 여행의 수 계산
+			int oddNode = 0;
+			for (int i = 0; i < component.size(); ++i) {
+				int node = component[i];
+				if(adj[node].size() % 2) oddNode++;
+			}
+
+			if (oddNode == 0 || oddNode == 2) cnt += 1;
+			else cnt += (oddNode / 2);
 		}
 	}
-
-	return order;
 }
+
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int C;
-	cin >> C;
-	while (C--) {
-		int n;
-		cin >> n;
+	cin >> n >> m;
 
-		vector<string> words;
-		string input;
-		for (int i = 0; i < n; ++i) {
-			cin >> input;
-			words.push_back(input);
-		}
+	adj = vector<vector<int>>(n, vector<int>(0));
+	for (int i = 0; i < m; ++i) {
+		int u, v;
+		cin >> u >> v;
 
-		makeGraph(words);
-		vector<int> res = topologicalSort();
-
-		if (res.size() == 0) {
-			cout << "INVALID HYPOTHESIS\n";
-		}
-		else {
-			for (auto it = res.begin(); it < res.end(); ++it) 
-				cout << (char)(*it + 'a');
-			cout << "\n";
-		}
+		adj[u-1].push_back(v-1);
+		adj[v-1].push_back(u-1);
 	}
 
+	dfsAll();
+	cout << cnt;
 	return 0;
 }
