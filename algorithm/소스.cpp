07d42d08ff4a5ae9b821413f0a53a4ccf9e40ae3
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-10-06 04:01:27 +0900

    백준 1697 숨바꼭질

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,51 +1,59 @@
 #include <iostream>
-#include <string>
+#include <queue>
 #include <algorithm>
 using namespace std;
+
+int n, k;
+
+int bfs(int start) {
+	//<위치,시간>
+	queue<pair<int, int>> q;
+	int found[200001] = { 0 };
+
+	found[start] = 1;
+	q.push({ start, 0 });
+
+	while (!q.empty()) {
+		int here = q.front().first;
+		int time = q.front().second;
+		q.pop();
+
+		//현재 위치 방문하기
+		if (here == k) return time;
+
+		//뒤로 걷기
+		if (here - 1 > 0 && found[here - 1] == 0) {
+			found[here - 1] =  1;
+			q.push({ here - 1, time + 1});
+		}
+		//앞으로 걷기
+		if (here + 1 < 200001 && found[here + 1] == 0) {
+			found[here + 1] = 1;
+			q.push({ here + 1, time + 1 });
+		}
+		//순간이동 하기
+		if (here * 2 < 200001 && found[here * 2] == 0) {
+			found[here * 2] = 1;
+			q.push({ here * 2, time + 1 });
+		}
+	}
+
+	//oops
+	return -1;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int m;
-	unsigned int bitmask = 0;
+	cin >> n >> k;
 
-	cin >> m;
-	
-	string op;
-	int x;
-	for (int i = 0; i < m; ++i) {
-		cin >> op;
-		if (op == "all") {
-			bitmask = (1 << 20) - 1;
-			continue;
-		}
-		if (op == "empty") {
-			bitmask = 0;
-			continue;
-		}
-
-		cin >> x;
-		if (op == "add") {
-			bitmask |= (1 << (x - 1));
-			continue;
-		}
-		if (op == "remove") {
-			bitmask &= ~(1 << (x - 1));
-			continue;
-		}
-		if (op == "check") {
-			if (bitmask & (1 << (x - 1))) cout << "1\n";
-			else cout << "0\n";
-			continue;
-		}
-		if (op == "toggle") {
-			bitmask ^= (1 << (x - 1));
-			continue;
-		}
+	if (n == k) {
+		cout << 0;
+		return 0;
 	}
 
-
+	cout << bfs(n);
 	return 0;
 }
