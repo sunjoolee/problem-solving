199bf1eab78628a5e6339da09774f40d6d588767
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2021-10-31 18:16:56 +0900

    알고스팟 신호 라우팅 (ROUTING)

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,59 +1,75 @@
 #include <iostream>
+#include <vector>
 #include <queue>
+#include <limits.h>
 #include <algorithm>
 using namespace std;
 
-int n, k;
+const int MAX_V = 10000;
 
-int bfs(int start) {
-	//<위치,시간>
-	queue<pair<int, int>> q;
-	int found[200001] = { 0 };
+//정점의 개수
+int V, E;
 
-	found[start] = 1;
-	q.push({ start, 0 });
+//그래프의 인접 리스트 
+//adj[u]: <u와 간선으로 연결된 정점 v, 간선의 가중치 w> 저장 
+vector<pair<int, double>> adj[MAX_V];
 
-	while (!q.empty()) {
-		int here = q.front().first;
-		int time = q.front().second;
-		q.pop();
+//다익스트라의 최단 거리 알고리즘 
+double dijkstra(int start) {
+	vector<double> dist(V, -1);
+	dist[start] = 1;
 
-		//현재 위치 방문하기
-		if (here == k) return time;
+	//pq: <-정점까지의 거리, 정점의 번호> 저장
+	priority_queue<pair<double, int>> pq;
+	pq.push(make_pair(-1, start));
 
-		//뒤로 걷기
-		if (here - 1 > 0 && found[here - 1] == 0) {
-			found[here - 1] =  1;
-			q.push({ here - 1, time + 1});
-		}
-		//앞으로 걷기
-		if (here + 1 < 200001 && found[here + 1] == 0) {
-			found[here + 1] = 1;
-			q.push({ here + 1, time + 1 });
-		}
-		//순간이동 하기
-		if (here * 2 < 200001 && found[here * 2] == 0) {
-			found[here * 2] = 1;
-			q.push({ here * 2, time + 1 });
+	while (!pq.empty()) {
+		double cost = -pq.top().first;
+		int here = pq.top().second;
+		pq.pop();
+
+		//cost보다 짧은 경로가 이미 발견되었다면 무시
+		if (dist[here] != -1 && dist[here] < cost) continue;
+
+		for (int i = 0; i < adj[here].size(); ++i) {
+			int there = adj[here][i].first;
+			double nextDist = adj[here][i].second * cost;
+
+			if (dist[there] == -1 || dist[there] > nextDist) {
+				dist[there] = nextDist;
+				pq.push(make_pair(-nextDist, there));
+			}
 		}
 	}
-
-	//oops
-	return -1;
+	return dist[V-1];
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(NULL); 
 	cout.tie(NULL);
 
-	cin >> n >> k;
+	int c;
+	cin >> c;
+	while (c--) {
+		//초기화
+		for(int i = 0; i < MAX_V; ++i)
+			adj[i].clear();
 
-	if (n == k) {
-		cout << 0;
-		return 0;
+		cin >> V >> E;
+
+		for (int i = 0; i < E; ++i) {
+			int a, b; double c;
+			cin >> a >> b >> c;
+
+			adj[a].push_back(make_pair(b, c));
+			adj[b].push_back(make_pair(a, c));
+		}
+		
+		cout << fixed;
+		cout.precision(10);
+		cout << dijkstra(0) << "\n";
 	}
 
-	cout << bfs(n);
 	return 0;
 }
