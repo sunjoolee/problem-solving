66cc091e2fc055a775cdf5f9fbfbfc09c689738c
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-01-03 07:43:00 +0900

    백준 1865 웜홀

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -5,38 +5,16 @@ using namespace std;
 
 typedef long long ll;
 
-const int MAX_V = 500;
+const int MAX_V = 500 + 1;
 const ll INF = 987654321;
-const ll OCHA = 250000;
+const ll OCHA = 125751501; //501^3
 
 //정점의 개수
 int V;
-//간선의 개수
-int E;
 
 //그래프의 인접 리스트 <연결된 정점 번호, 간선의 가중치>
 vector<pair<int, ll>> adj[MAX_V];
 
-//0번 도시와 연결된 도시인지 체크
-vector<int> connected(MAX_V, 0);
-
-void bfs() {
-	queue<int> q;
-	q.push(0);
-
-	while (!q.empty()) {
-		int here = q.front();
-		q.pop();
-
-		if (connected[here]) break;
-		connected[here] = 1;
-
-		for (int i = 0; i < adj[here].size(); ++i) {
-			int there = adj[here][i].first;
-			if (!connected[there]) q.push(there);
-		}
-	}
-}
 
 //음수 사이클이 있을 경우 텅 빈 배열 반환
 //시작 정점 src
@@ -44,6 +22,7 @@ vector<ll> bellmanFord(int src) {
 	//시작점을 제외한 모든 정점까지의 최단 거리 상한 INF
 	vector<ll> upper(V, INF);
 	upper[src] = 0;
+
 	bool updated;
 
 	//V번 순회
@@ -58,9 +37,7 @@ vector<ll> bellmanFord(int src) {
 				if (upper[there] > upper[here] + cost) {
 					//성공
 					upper[there] = upper[here] + cost;
-
-					//0번 도시와 연결된 도시인 경우 완화 체크
-					if(connected[there]) updated = true;
+					updated = true;
 				}
 			}
 		}
@@ -77,27 +54,45 @@ int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> V >> E;
-
-	for (int i = 0; i < E; ++i) {
-		int A, B;
-		ll C;
-		cin >> A >> B >> C;
-
-		adj[A - 1].push_back({ B - 1, C });
-	}
-
-	//0번 도시와 연결된 도시 체크
-	bfs();
-
-	vector<ll> res = bellmanFord(0);
-	
-	if (res.empty()) cout << -1;
-	else {
-		for (int i = 1; i < res.size(); ++i) {
-			if (res[i] < INF - OCHA) cout << res[i] << "\n";
-			else cout << -1 << "\n";
+	int TC;
+	cin >> TC;
+	while (TC--) {
+		//그래프 초기화
+		for (int i = 0; i < MAX_V; ++i){
+			adj[i].clear();
 		}
+		
+		int M, W;
+
+		cin >> V >> M >> W;
+
+		//다리 입력 받기
+		for(int i = 0; i < M; ++i) {
+			int S, E;
+			ll T;
+			cin >> S >> E >> T;
+
+			adj[S].push_back({ E, T });
+			adj[E].push_back({ S, T });
+		}
+		//웜홀 입력 받기
+		for (int i = 0; i < W; ++i) {
+			int S, E;
+			ll T;
+			cin >> S >> E >> T;
+
+			adj[S].push_back({ E, -T });
+		}
+
+		//1번 도시와 연결된 가상의 시작 지점
+		V++;
+		adj[0].push_back({ 1, 0 });
+		adj[1].push_back({ 0, 0 });
+
+		vector<ll> res = bellmanFord(0);
+
+		if (res.empty()) cout << "YES\n";
+		else cout << "NO\n";
 	}
 
 	return 0;
