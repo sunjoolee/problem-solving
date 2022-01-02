2944416b89c7bbf880e8204efdb80b2e47650379
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-01-03 07:57:07 +0900

    백준 11404 플로이드

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -2,18 +2,20 @@
 #include  <algorithm>
 using namespace std;
 
-const int MAX_V = 500;
+typedef long long ll;
+const int MAX_V = 100;
+const ll INF = 987654321;
 
 //정점의 개수 
 int V;
 //그래프의 인접 행렬 표현
 //adj[u][v] = u에서 v로 가는 간선의 가중치
-int adj[MAX_V][MAX_V];
+ll adj[MAX_V][MAX_V];
 
 //플로이드-와샬 알고리즘
 void floyd() {
 	for (int i = 0; i < V; ++i) 
-		adj[i][i] = 0;
+		adj[i][i] = 0LL;
 
 	//i지점 -> j지점으로 이동할 때 거쳐가는 지점 K
 	for (int k = 0; k < V; ++k)
@@ -25,6 +27,33 @@ void floyd() {
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
+
+	//adj 초기화
+	for (int i = 0; i < MAX_V; ++i)
+		for (int j = 0; j < MAX_V; ++j)
+			adj[i][j] = INF;
+
+	//버스의 개수
+	int E;
+	cin >> V >> E;
+
+	for (int i = 0; i < E; ++i) {
+		int a, b;
+		ll c;
+		cin >> a >> b >> c;
+		
+		adj[a-1][b-1] = min(adj[a-1][b-1], c);
+	}
+
+	floyd();
+
+	for (int i = 0; i < V; ++i) {
+		for (int j = 0; j < V; ++j) {
+			if (adj[i][j] < INF) cout << adj[i][j] << " ";
+			else cout << "0 ";
+		}
+		cout << "\n";
+	}
 
 	return 0;
 }
