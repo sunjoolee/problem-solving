5e429c78deb41967ddf4ba538c59ef71200ef21a
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-01-10 12:47:20 +0900

    백준 4386 별자리 만들기

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include <algorithm>
 using namespace std;
 
@@ -16,7 +17,7 @@ struct DisjointSet {
 	}
 
 	//u가 속한 트리의 루트 번호 반환
-	int find(int u) const {
+	int find(int u) {
 		if (u == parent[u]) return u;
 		return parent[u] = find(parent[u]);
 	}
@@ -37,19 +38,18 @@ struct DisjointSet {
 //정점의 개수
 int V;
 //그래프의 인접 리스트 (연결된 정점 번호, 간선 가중치) 쌍 저장
-vector<pair<int, int>> adj[MAX_V];
+vector<pair<int, double>> adj[MAX_V];
 
-//주어진 그래프에 대해 최소 스패닝 트리에 포합된 간선의 목록을 selected에 저장하고, 가중치의 합을 반환한다.
-int kruskal(vector<pair<int, int>>& selected) {
-	int ret = 0;
-	selected.clear();
+//주어진 그래프에 대해 최소 스패닝 트리 가중치의 합을 반환한다.
+double kruskal() {
+	double ret = 0;
 
 	//<가중치, <u, v>>의 목록을 얻는다
-	vector<pair<int, pair<int, int>>> edges;
+	vector<pair<double, pair<int, int>>> edges;
 	for (int u = 0; u < V; ++u) {
 		for (int i = 0; i < adj[u].size(); ++i) {
 			int v = adj[u][i].first;
-			int cost = adj[u][i].second;
+			double cost = adj[u][i].second;
 			edges.push_back({ cost, {u, v} });
 		}
 	}
@@ -59,7 +59,7 @@ int kruskal(vector<pair<int, int>>& selected) {
 	//처음엔 모든 정점이 서로 분리되어 있다.
 	DisjointSet sets(V);
 	for (int i = 0; i < edges.size(); ++i) {
-		int cost = edges[i].first;
+		double cost = edges[i].first;
 		int u = edges[i].second.first;
 		int v = edges[i].second.second;
 
@@ -68,7 +68,6 @@ int kruskal(vector<pair<int, int>>& selected) {
 		if (sets.find(u) == sets.find(v)) continue;
 
 		sets.merge(u, v);
-		selected.push_back({ u, v });
 		ret += cost;
 	}
 
@@ -78,6 +77,30 @@ int kruskal(vector<pair<int, int>>& selected) {
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
+
+	//별의 개수
+	cin >> V;
+
+	//별의 좌표
+	vector<pair<double, double>> stars;
+	
+	for (int i = 0; i < V; ++i) {
+		double x, y;
+		cin >> x >> y;
+		stars.push_back({ x, y });
+	}
+
+	//그래프 생성
+	for (int i = 0; i < V; ++i) {
+		for (int j = 0; j < V; ++j) {
+			double dist  = sqrt(pow(stars[i].first - stars[j].first, 2) + pow(stars[i].second - stars[j].second, 2));
+
+			adj[i].push_back({ j, dist });
+			adj[j].push_back({ i, dist });
+		}
+	}
+
+	cout << kruskal();
 
 	return 0;
 }
