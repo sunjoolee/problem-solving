f635047f2dcf4330986a7deb5cf617622d0d10c8
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-01-17 00:06:48 +0900

    인접 리스트로 포드-풀커슨 알고리즘 구현하기
    유량 네트워크의 간선을 표현하는 구조체 구현

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,67 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
 using namespace std;
 
-//네트워크 유량 문제를 해결하는 포드-폴커슨 알고리즘의 구현
-
+//인접 리스트로 포드-풀커슨 알고리즘 구현하기
 const int MAX_V = 500;
-const int INF = 987654321;
-int V;
 
-//capacity[u][v] = u에서 v로 보낼 수 있는 용량
-//flow[u][v] = u에서 v로 흘러가는 유량 (반대 방향인 경우 음수)
-int capacity[MAX_V][MAX_V], flow[MAX_V][MAX_V];
-
-//flow[]를 계산하고 총 유량을 반환한다.
-int networkFlow(int source, int sink) {
-	
-	//flow를 0으로 초기화한다.
-	memset(flow, 0, sizeof(flow));
-	int totalFlow = 0;
-	
-	while (true) {
-		//BFS로 증가 경로를 찾는다
-		vector<int> parent(MAX_V, -1);
-		queue<int> q;
-
-		parent[source] = source;
-		q.push(source);
-
-		while (!q.empty() && parent[sink] == -1) {
-			int here = q.front();
-			q.pop();
-
-			for (int there = 0; there < V; ++there) {
-				//잔여 용량이 남아있는 간선 탐색
-				if (capacity[here][there] - flow[here][there] > 0 && parent[there] == -1) {
-					q.push(there);
-					parent[there] = here;
-				}
-			}
-		}
-
-		//증가 경로가 없는 경우 종료
-		if (parent[sink] == -1) break;
-		
-		//증가 경로를 통해 유량을 얼마나 보낼지 결정한다
-		int amount = INF;
-		for (int p = sink; p != source; p = parent[p]) {
-			int r = capacity[parent[p]][p] - flow[parent[p]][p];
-			amount = min(r, amount);
-		}
-		//증가 경로를 통해 유량을 보낸다
-		for (int p = sink; p != source; p = parent[p]) {
-			flow[parent[p]][p] += amount;
-			flow[p][parent[p]] -= amount;
-		}
-		totalFlow += amount;
+//간선의 정보를 나타내는 구조체
+struct Edge {
+	int target, capacity, flow;
+	//역방향 간선의 포인터
+	Edge* reverse;
+	//이 간선의 잔여 용량을 계산한다
+	int residualCapacity() const { return capacity - flow; }
+	//이 간선을 이용해 amount를 보낸다
+	void push(int amount) {
+		flow += amount;
+		reverse->flow -= amount;
 	}
+};
 
-	return totalFlow;
+//유량 네트워크의 인접 리스트
+vector<Edge*> adj[MAX_V];
+
+//u에서 v로 가는 간선을 추가한다
+
+void addEdge(int u, int v, int capacity) {
+	Edge* uv = new Edge();
+	Edge* vu = new Edge();
+
+	//u에서 v로 가는 간선을 초기화한다
+	uv->target = v;
+	uv->capacity = capacity;
+	uv->flow = 0;
+	uv->reverse = vu;
+
+	//v에서 u로 가는 간선을 초기화한다
+	//이 간선은 용량이 0인 유령 간선이다
+	vu->target = u;
+	vu->capacity = 0;
+	vu->flow = 0;
+	vu->reverse = uv;
+
+	adj[u].push_back(uv);
+	adj[v].push_back(vu);
 }
-
 
 int main() {
 	ios_base::sync_with_stdio(false);
