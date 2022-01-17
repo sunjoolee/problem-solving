8800ae04a71b11ef7d61108bab443b6e7befbe85
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-01-17 10:38:38 +0900

    이분 매칭을 해결하는 증가 경로 알고리즘 구현

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,47 +3,55 @@
 #include <algorithm>
 using namespace std;
 
-//인접 리스트로 포드-풀커슨 알고리즘 구현하기
-const int MAX_V = 500;
+//이분 매칭을 해결하는 증가 경로 알고리즘
 
-//간선의 정보를 나타내는 구조체
-struct Edge {
-	int target, capacity, flow;
-	//역방향 간선의 포인터
-	Edge* reverse;
-	//이 간선의 잔여 용량을 계산한다
-	int residualCapacity() const { return capacity - flow; }
-	//이 간선을 이용해 amount를 보낸다
-	void push(int amount) {
-		flow += amount;
-		reverse->flow -= amount;
+const int MAX_N = 500;
+const int MAX_M = 500;
+
+//A와 B의 정점의 개수
+int n, m;
+
+//adj[i][j] = Ai와 Bj가 연결되어 있는가?
+bool adj[MAX_N][MAX_M];
+
+//각 정점에 매칭된 상대 정점의 번호를 저장한다
+vector<int> aMatch, bMatch;
+
+//dis()의 방문 여부
+vector<bool> visited;
+
+//A의 정점인 a에서 B의 매칭되지 않은 정점으로 가는 경로를 찾는다
+bool dfs(int a) {
+	if (visited[a]) return false;
+	visited[a] = true;
+	for (int b = 0; b < m; ++b) {
+		if (adj[a][b]) {
+			//b가 매칭되어 있지 않다면 bMatch[b]에서부터 시작해 증가 경로를 찾는다
+			if (bMatch[b] == -1 || dfs(bMatch[b])) {
+				//증가 경로 발견 -> a와 b를 매치시킨다
+				aMatch[a] = b;
+				bMatch[b] = a;
+				return true;
+			}
+		}
 	}
-};
+	return false;
+}
 
-//유량 네트워크의 인접 리스트
-vector<Edge*> adj[MAX_V];
-
-//u에서 v로 가는 간선을 추가한다
-
-void addEdge(int u, int v, int capacity) {
-	Edge* uv = new Edge();
-	Edge* vu = new Edge();
-
-	//u에서 v로 가는 간선을 초기화한다
-	uv->target = v;
-	uv->capacity = capacity;
-	uv->flow = 0;
-	uv->reverse = vu;
-
-	//v에서 u로 가는 간선을 초기화한다
-	//이 간선은 용량이 0인 유령 간선이다
-	vu->target = u;
-	vu->capacity = 0;
-	vu->flow = 0;
-	vu->reverse = uv;
-
-	adj[u].push_back(uv);
-	adj[v].push_back(vu);
+//aMatch, bMatch 배열을 계산하고 최대 매칭의 크기를 반환한다
+int bipartiteMatch(){
+	//처음에는 어떤 정점도 연결되어 있지 않다
+	aMatch = vector<int>(n, -1);	
+	bMatch = vector<int>(m, -1);
+	
+	int size = 0;
+	for (int start = 0; start < n; ++start) {
+		visited = vector<bool>(n, false);
+		//깊이 우선 탐색을 통해 start에서 시작하는 증가 경로를 찾는다
+		if (dfs(start))
+			++size;
+	}
+	return size;
 }
 
 int main() {
