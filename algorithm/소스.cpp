6124fdb860e40bee0c080af75db5618f0729e975
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-02 11:21:07 +0900

    백준 11406 책 구매하기 2

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -11,58 +11,40 @@ const int MAX_V = 210;
 const int INF = 987654321;
 int V;
 
-//cost[u][v] = u에서 v로 보내는게 드는 비용
 //capacity[u][v] = u에서 v로 보낼 수 있는 용량
 //flow[u][v] = u에서 v로 흘러가는 유량 (반대 방향인 경우 음수)
-int cost[MAX_V][MAX_V], capacity[MAX_V][MAX_V], flow[MAX_V][MAX_V];
+int capacity[MAX_V][MAX_V], flow[MAX_V][MAX_V];
 
 
 int networkFlow(int source, int sink) {
-	
+
 	//flow를 0으로 초기화한다.
 	memset(flow, 0, sizeof(flow));
 	int totalFlow = 0;
 
 	//총 배송비
 	int totalCost = 0;
-	
-	
+
+
 	while (true) {
 
 		//BFS로 증가 경로를 찾는다
-		//dist[i][j] : i->j 경로의 최소 비용
-
 		vector<int> parent(MAX_V, -1);
-		vector<int> dist(MAX_V, INF);
-		vector<bool> inQ(MAX_V, false);
 		queue<int> q;
 
 		parent[source] = source;
-		dist[source] = 0;
-
 		q.push(source);
-		inQ[source] = true;
 
 		while (!q.empty() && parent[sink] == -1) {
 			int here = q.front();
 			q.pop();
-			inQ[here] = false;
 
 			for (int there = 0; there < V; ++there) {
 				//잔여 용량이 남아있는 간선 탐색
 				if (capacity[here][there] - flow[here][there] > 0 && parent[there] == -1) {
-					
-					//최소 비용 계산
-					if (dist[here] + cost[here][there] < dist[there]) {
-						dist[there] = dist[here] + cost[here][there];
+					parent[there] = here;
+					q.push(there);
 
-						parent[there] = here;
-
-						if (!inQ[there]) {
-							q.push(there);
-							inQ[there] = true;
-						}
-					}
 				}
 			}
 		}
@@ -70,7 +52,7 @@ int networkFlow(int source, int sink) {
 
 		//증가 경로가 없는 경우 종료
 		if (parent[sink] == -1) break;
-		
+
 		//증가 경로를 통해 유량을 얼마나 보낼지 결정한다
 		int amount = INF;
 		for (int p = sink; p != source; p = parent[p]) {
@@ -81,14 +63,11 @@ int networkFlow(int source, int sink) {
 		for (int p = sink; p != source; p = parent[p]) {
 			flow[parent[p]][p] += amount;
 			flow[p][parent[p]] -= amount;
-
-			//보내는 경로의 배송비 계산
-			totalCost += amount * cost[parent[p]][p];
 		}
 		totalFlow += amount;
 	}
 
-	return totalCost;
+	return totalFlow;
 }
 
 
@@ -110,17 +89,12 @@ int main() {
 	//점점 수 = 사람 수 + 서점 수 + source + sink
 	V = N + M + 2;
 
-	for (int i = 0; i < V; ++i) {
-		for (int j = 0; j < V; ++j) {
-			cost[i][j] = 0;
-			capacity[i][j] = INF;
-		}
-	}
+	memset(capacity, 0, sizeof(capacity));
 
 	//사람이 구매하고 싶은 책의 수
 	//= 사람 -> sink 간선의 용량
 	for (int i = 0; i < N; ++i) {
-		cin >> capacity[1 + M + i][M+N+1];
+		cin >> capacity[1 + M + i][M + N + 1];
 	}
 	//서점이 가지고 있는 책의 수
 	//= source -> 서점 간선의 용량
@@ -128,15 +102,15 @@ int main() {
 		cin >> capacity[0][1 + i];
 	}
 
-	//서점 i -> 사람 j 로 보내는 배송비
-	//= 서점 i -> 사람 j로 보내는 비용
+	//서점 i -> 사람 j 팔 수 있는 책의 수
+	//= 서점 i -> 사람 j 간선의 용량
 	for (int i = 0; i < M; ++i) {
 		for (int j = 0; j < N; ++j) {
-			cin >> cost[1+i][1+M+j];
+			cin >> capacity[1 + i][1 + M + j];
 		}
 	}
 
-	cout << networkFlow(0,M+N+1);
+	cout << networkFlow(0, M + N + 1);
 
 	return 0;
 }
