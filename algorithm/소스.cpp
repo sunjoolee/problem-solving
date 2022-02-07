1c3bd4497e67bd846895b857466cf0e3ddb89974
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-07 21:08:43 +0900

    프로그래머스 튜플

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,35 +1,53 @@
 #include <string>
 #include <vector>
-#include <stack>
-#include <queue>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
-int solution(vector<vector<int>> board, vector<int> moves) {
-	int answer = 0;
+const int MAX_SIZE = 100005;
 
-	vector<queue<int>> boardQ(30);
-	stack<int> st;
+//해당 숫자가 집합에 몇 번 포함되었는지 저장
+int arr[MAX_SIZE] = { 0 };
 
-	for (int i = 0; i < board.size(); ++i) {
-		for (int j = 0; j < board.size(); ++j) {
-			if (board[i][j] != 0) boardQ[j].push(board[i][j]);
+bool cmp(int i, int j) {
+	return j < i;
+}
+
+vector<int> solution(string s) {
+	//가장 최대 중복 횟수 = 튜플의 길이
+	int numMax = 0;
+
+	string num = "";
+	for (int i = 0; i < s.length(); ++i) {
+		char ch = s[i];
+
+		if (ch == '{' || ch == '"') continue;
+		else if (ch == ',' || ch == '}') {
+			if (num == "") continue;
+			
+			int pos = stoi(num);
+			arr[pos]++;
+			if (numMax < arr[pos]) numMax = arr[pos];
+			num = "";
 		}
+		else num += ch;
 	}
 
-	for (int m = 0; m < moves.size(); ++m) {
-		int pos = moves[m] - 1;
-		if (boardQ[pos].empty()) continue;
-
-		int num = boardQ[pos].front();
-		boardQ[pos].pop();
-
-		if (!st.empty() && st.top() == num) {
-			st.pop();
-			answer += 2;
-		}
-		else st.push(num);
+	vector<int> answer(numMax, 0);
+	for (int i = 0; i < MAX_SIZE; ++i) {
+		if (arr[i] == 0) continue;
+		answer[arr[i] - 1] = i;
 	}
+	reverse(answer.begin(), answer.end());
 
 	return answer;
 }
+
+int main() {
+	string input;
+	cin >> input;
+
+	solution(input);
+
+}
+
