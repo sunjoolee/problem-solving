bfd665e72e867970cbc332fe8236d7dbe3a6606d
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-08 12:02:33 +0900

    프로그래머스 호텔 방 배정
    효율성 탈락

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,93 +1,36 @@
-#include <iostream>
+#include <string>
 #include <vector>
-#include <cstring>
-#include <limits.h>
-#include <algorithm>
+#include <set>
+
 using namespace std;
 
-const int MAX_VAL = 2000000000;
+typedef long long ll;
 
-int N;
-vector<int> arr;
+set<ll> hotel;
 
-struct treeNode {
-	int maxVal;
-};
+vector<ll> solution(ll k, vector<ll> room_number) {
+	vector<ll> answer;
 
-struct SegmentTree {
-	vector<treeNode> tree;
-
-	treeNode merge(treeNode left, treeNode right) {
-		treeNode mergeNode;
-		mergeNode.maxVal = max(left.maxVal, right.maxVal);
-		return mergeNode;
+	for (ll i = 1LL; i <= k; ++i) {
+		hotel.insert(i);
 	}
 
-	treeNode buildRecursive(int node, int nodeLeft, int nodeRight) {
-		if (nodeLeft == nodeRight) {
-			treeNode mergeNode;
-			mergeNode.maxVal = arr[nodeLeft];
 
-			return tree[node] = mergeNode;
+	for (int i = 0; i < room_number.size(); ++i) {
+		ll room = room_number[i];
+
+		auto iter = hotel.find(room);
+		if (iter != hotel.end()) {
+			answer.push_back(room);
+			hotel.erase(iter);
+		}
+		else {
+			iter = hotel.upper_bound(room);
+			answer.push_back(*iter);
+			hotel.erase(iter);
 		}
 
-		int mid = (nodeLeft + nodeRight) / 2;
-		treeNode leftNode = buildRecursive(node * 2, nodeLeft, mid);
-		treeNode rightNode = buildRecursive(node * 2 + 1, mid + 1, nodeRight);
-
-		return tree[node] = merge(leftNode, rightNode);
 	}
-
-	void build() {
-		tree.resize(N * 4);
-		buildRecursive(1, 0, N - 1);
-	}
-
-	treeNode queryRecursive(int left, int right, int node, int nodeLeft, int nodeRight) {
-		//쿼리의 구간에 포함되지 않는 구간인 경우 default 값 반환 
-		//default 값은 merge 연산에 따라 다르다
-		if (right < nodeLeft || nodeRight < left) {
-			treeNode defaultNode;
-			defaultNode.maxVal = 0;
-
-			return defaultNode;
-		}
-
-		if (left <= nodeLeft && nodeRight <= right)
-			return tree[node];
-
-		int mid = (nodeLeft + nodeRight) / 2;
-		treeNode leftNode = queryRecursive(left, right, node * 2, nodeLeft, mid);
-		treeNode rightNode = queryRecursive(left, right, node * 2 + 1, mid + 1, nodeRight);
-
-		return merge(leftNode, rightNode);
-	}
-
-	treeNode query(int left, int right) {
-		return queryRecursive(left, right, 1, 0, N - 1);
-	}
-
-};
-
-
-int solution(vector<int> stones, int k) {
-
-	arr = stones;
-	N = stones.size();
-
-	SegmentTree segmentTree;
-	segmentTree.build();
-
-	int answer = 0;
-
-	int minVal = MAX_VAL;
-	for (int i = 0; i <= stones.size() - k; ++i) {		
-		treeNode res = segmentTree.query(i, i + k-1);
-		minVal = min(minVal, res.maxVal);
-	}
-	answer = minVal;
 
 	return answer;
 }
-
-
