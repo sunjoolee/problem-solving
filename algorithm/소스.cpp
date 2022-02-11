d1fc928c28931b2d3dc6955967ab207c46cc98dd
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-11 16:33:24 +0900

    프로그래머스 호텔 방 배정
    해결

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -8,55 +8,33 @@ using namespace std;
 
 typedef long long ll;
 
-//parent[i]: i번보다 번호가 크고, 배정 가능한 방 중 가장 번호가 작은 방의 번호
-//parent[i] = i일 경우 방 배정 가능 -> 방 배정 후 parent[i] = i+1
-unordered_map<ll, ll> parent;
+unordered_map<ll, ll> notEmptyRoom;
 
-
-//u가 속한 트리의 루트 번호 반환
-int find(int u) {
-	//부모가 자기 자신을 가리키는 경우 u는 루트 노드
-	if (u == parent[u]) return u;
+ll findEmptyRoom(ll u) {
+	if (notEmptyRoom.find(u) == notEmptyRoom.end()) return u;
 
 	//경로 압축(path compression) 최적화 구현
-	return parent[u] = find(parent[u]);
+	return notEmptyRoom[u] = findEmptyRoom(notEmptyRoom[u]);
 }
 
 vector<ll> solution(ll k, vector<ll> room_number) {
 	vector<ll> answer;
 
-	for (ll i = 0; i < k; ++i)
-		parent.insert({ i, i });
-	
 	for (int i = 0; i < room_number.size(); ++i) {
-		ll roomNum = room_number[i] - 1;
-		
-		if (parent[roomNum] == roomNum) {
-			answer.push_back(roomNum + 1);
-			parent[roomNum] = roomNum + 1;
+		ll roomNum = room_number[i];
+
+		if (notEmptyRoom.find(roomNum) == notEmptyRoom.end()) {
+			answer.push_back(roomNum);
+
+			notEmptyRoom[roomNum] = findEmptyRoom(roomNum + 1);
 		}
 		else {
-			ll availableNum = find(roomNum);
-			answer.push_back(availableNum + 1);
-			parent[availableNum] = availableNum + 1;
+			ll emptyRoomNum = findEmptyRoom(roomNum);
+			answer.push_back(emptyRoomNum);
+
+			notEmptyRoom[emptyRoomNum] = findEmptyRoom(emptyRoomNum + 1);
 		}
 	}
 
 	return answer;
 }
-
-int main() {
-	ll k;
-	vector<ll> room_number;
-	
-	cin >> k;
-	for (int i = 0; i < 6; ++i) {
-		ll n;
-		cin >> n;
-		room_number.push_back(n);
-	}
-
-	solution(k, room_number);
-	return 0;
-}
-
