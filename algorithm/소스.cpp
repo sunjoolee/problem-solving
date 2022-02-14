5545eb9296f8a003b53e91f34bb1d2cc3a5bb7f6
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-14 22:21:11 +0900

    프로그래머스 수식 최대화
    정확성: 86.7
    합계: 86.7 / 100.0

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,74 +1,97 @@
 #include <string>
 #include <vector>
+#include <list>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
 
 using namespace std;
 
+typedef long long ll;
 
-string solution(vector<int> numbers, string hand) {
+list<string> calcList(list <string> exp, string op) {
+	for (auto it = exp.begin(); it != exp.end(); ++it) {
+		if (*it == op) {
+			auto prevIt = --it;
+			++it;
+			auto nextIt = ++it;
+			--it;
 
-	string answer = "";
+			ll num1 = stoi(*prevIt);
+			ll num2 = stoi(*nextIt);
+			
+			exp.erase(prevIt);
+			exp.erase(nextIt);
 
-	//번호 간 거리 초기화 (* : 13, # : 14)
-	vector<vector<int>> dist(15, vector<int>(15, 0));
+			//연산 결과
+			ll num3;
+			if (op == "*") num3 = num1 * num2;
+			if (op == "+") num3 = num1 + num2;
+			if (op == "-") num3 = num1 - num2;
 
-	dist[1][2] = dist[3][2] = 1;
-	dist[1][5] = dist[3][5] = 2;
-	dist[1][8] = dist[3][8] = 3;
-	dist[1][0] = dist[3][0] = 4;
-
-	dist[4][2] = dist[6][2] = 2;
-	dist[4][5] = dist[6][5] = 1;
-	dist[4][8] = dist[6][8] = 2;
-	dist[4][0] = dist[6][0] = 3;
-
-	dist[7][2] = dist[9][2] = 3;
-	dist[7][5] = dist[9][5] = 2;
-	dist[7][8] = dist[9][8] = 1;
-	dist[7][0] = dist[9][0] = 2;
-
-	dist[13][2] = dist[14][2] = 4;
-	dist[13][5] = dist[14][5] = 3;
-	dist[13][8] = dist[14][8] = 2;
-	dist[13][0] = dist[14][0] = 1;
-
-	//center
-	dist[2][2] = dist[5][5] = dist[8][8] = dist[0][0] = 0;
-	dist[2][5] = dist[5][2] = dist[5][8] = dist[8][5] = dist[0][8] = dist[8][0] = 0;
-	dist[2][8] = dist[8][2] = dist[5][0] = dist[0][5] = 2;
-	dist[2][0] = dist[0][2] = 3;
-
-
-	//현재 엄지 손가락의 위치
-	int Lpos = 13; //*
-	int Rpos = 14; //#
-
-	for (int i = 0; i < numbers.size(); ++i) {
-		int num = numbers[i];
-		if (num == '*') num = 13;
-		if (num == '#') num = 14;
-
-		//flag = 0 왼손, flag = 1 오른손
-		int flag;
-
-		if (num == 1 || num == 4 || num == 7 || num == 13) flag = 0;
-		else if (num == 3 || num == 6 || num == 9 || num == 14) flag = 1;
-
-		else {
-			if (dist[Lpos][num] < dist[Rpos][num]) flag = 0;
-			else if (dist[Lpos][num] > dist[Rpos][num]) flag = 1;
-			else if (hand == "left") flag = 0;
-			else flag = 1;
-		}
-
-		//손가락 움직이기
-		if (flag){
-			Rpos = num;
-			answer += "R";
-		}
-		else {
-			Lpos = num;
-			answer += "L";
+			//연산자 위치 앞에 연산 결과 삽입
+			exp.insert(it, to_string(num3));
+			
+			//연산자 삭제
+			it = exp.erase(it);
+			--it;
 		}
 	}
+
+	return exp;
+}
+
+ll calcAnswer(list <string> exp, char op1, char op2, char op3) {
+
+	exp = calcList(exp, string(1, op1));
+	exp = calcList(exp, string(1, op2));
+	exp = calcList(exp, string(1, op3));
+	
+	if (exp.size() != 1) return 0;
+
+	return abs(stol(exp.front()));
+}
+
+ll solution(string expression) {
+	ll answer = 0;
+
+	list <string> exp;
+
+	//주어진 수식 list로 변환
+	string num = "";
+	for (int i = 0; i < expression.length(); ++i) {
+		char ch = expression[i];
+
+		if (ch == '+' || ch == '-' || ch == '*') {
+			if (num != "") {
+				exp.push_back(num);
+				num = "";
+			}
+
+			exp.push_back(string(1, ch));
+		}
+		else num += ch;
+	}
+	if (num != "") exp.push_back(num);
+
+	//각 우선순위에 따른 결괏값의 최댓값 구하기
+	// + > - > *
+	answer = max(answer, calcAnswer(exp, '+', '-', '*'));
+	answer = max(answer, calcAnswer(exp, '+', '*', '-'));
+	answer = max(answer, calcAnswer(exp, '-', '+', '*'));
+	answer = max(answer, calcAnswer(exp, '-', '*', '+'));
+	answer = max(answer, calcAnswer(exp, '*', '-', '+'));
+	answer = max(answer, calcAnswer(exp, '*', '+', '-'));
+
 	return answer;
+}
+
+int main() {
+
+	string expression;
+	cin >> expression;
+
+	solution(expression);
+
+	return 0;
 }
