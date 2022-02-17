277724143ec45ddfd45add753e038ca0915a7239
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-17 17:30:37 +0900

    프로그래머스 보석 쇼핑
    비트마스크로 구현 시도 중
    정확성 실패

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <set>
 #include <string>
 #include <algorithm>
 using namespace std;
 
-typedef long long ll;
+typedef long long ull;
+
 const int INF = 987654321;
 
 //N: 보석 진열대의 길이
@@ -14,17 +16,15 @@ int N;
 //보석 진열대
 vector<string> arr;
 
+//보석 숫자 매핑
+map<string, int> gemToInt;
+
 struct SegmentTree {
-	vector<set<string>> tree;
+	vector<ull> tree;
 
 	//보석 진열대 구간 내 보석의 종류
-	set <string> merge(set<string> left, set<string> right) {
-		set <string> result;
-
-		result.insert(left.begin(), left.end());
-		result.insert(right.begin(), right.end());
-
-		return result;
+	ull merge(ull left, ull right) {
+		return left | right;
 	}
 
 	//---------------------------------------
@@ -32,13 +32,11 @@ struct SegmentTree {
 	//node: Segment Tree의 노드의 번호
 	//nodeLeft ~ nodeRight: 해당 노드가 저장하고 있는 구간
 
-	set<string> buildRecursive(int node, int nodeLeft, int nodeRight) {
+	ull buildRecursive(int node, int nodeLeft, int nodeRight) {
 
 		//리프노드에 도달한 경우
 		if (nodeLeft == nodeRight) {
-			set<string> leaf;
-			leaf.insert(arr[nodeLeft]);
-			return tree[node] = leaf;
+			return tree[node] = (0LL | (1 << gemToInt[arr[nodeLeft]]));
 		}
 
 		//리프노드가 아닌 경우 구간 반으로 나누어 Recursive call
@@ -46,8 +44,8 @@ struct SegmentTree {
 		//노드의 구간의 왼쪽 반: 노드의 왼쪽 자식이 저장
 
 		int mid = (nodeLeft + nodeRight) / 2;
-		set<string> leftVal = buildRecursive(node * 2, nodeLeft, mid);
-		set<string> rightVal = buildRecursive(node * 2 + 1, mid + 1, nodeRight);
+		ull leftVal = buildRecursive(node * 2, nodeLeft, mid);
+		ull rightVal = buildRecursive(node * 2 + 1, mid + 1, nodeRight);
 
 		return tree[node] = merge(leftVal, rightVal);
 	}
@@ -64,13 +62,12 @@ struct SegmentTree {
 	//node: Segment Tree의 노드의 번호
 	//nodeLeft ~ nodeRight: 해당 노드가 저장하고 있는 구간
 
-	set<string> queryRecursive(int left, int right, int node, int nodeLeft, int nodeRight) {
+	ull queryRecursive(int left, int right, int node, int nodeLeft, int nodeRight) {
 
 		//쿼리의 구간에 포함되지 않는 구간인 경우 default 값 반환 
 		//default 값은 merge 연산에 따라 다르다
 		if (right < nodeLeft || nodeRight < left) {
-			set<string> emptySet;
-			return emptySet;
+			return 0LL;
 		}
 
 		//쿼리의 구간에 포함되는 구간인 경우
@@ -80,32 +77,42 @@ struct SegmentTree {
 		//쿼리의 구간에 부분적으로 포함되는 구간인 경우
 		//구간 반으로 나누어 Recursive call
 		int mid = (nodeLeft + nodeRight) / 2;
-		set<string> leftVal = queryRecursive(left, right, node * 2, nodeLeft, mid);
-		set<string> rightVal = queryRecursive(left, right, node * 2 + 1, mid + 1, nodeRight);
+		ull leftVal = queryRecursive(left, right, node * 2, nodeLeft, mid);
+		ull rightVal = queryRecursive(left, right, node * 2 + 1, mid + 1, nodeRight);
 
 		return merge(leftVal, rightVal);
 	}
 
-	set<string> query(int left, int right) {
+	ull query(int left, int right) {
 		// 루트 노드부터 제귀적으로 쿼리 구간의 합 계산
 		return queryRecursive(left, right, 1, 0, N - 1);
 	}
 
 };
 
-
 vector<int> solution(vector<string> gems) {
 	vector<int> answer;
 
+	//전역 변수 초기화
 	N = gems.size();
 	arr = gems;
 
+	//보석 종류 숫자 매핑
+	int gemId = 0;
+	for (int i = 0; i < gems.size(); ++i) {
+		if (gemToInt.find(gems[i]) == gemToInt.end()) {
+			gemToInt[gems[i]] = gemId;
+			gemId++;
+		}
+	}
+	//총 보석의 개수
+	int totalGems = gemId;
+
+	//구간 별 포함된 보석의 종류를 비트마스크로 저장한 세그먼트 트리
 	SegmentTree segmentTree;
 	segmentTree.build();
 
-	//총 보석의 개수 = 전체 구간의 쿼리 값 set<string> 의 크기
-	int totalGems = segmentTree.query(0, gems.size() - 1).size();
-
+	
 	//가능한 진열대 구간의 길이 이분 탐색
 	int lo = 0, hi = gems.size() + 1;
 
@@ -117,7 +124,10 @@ vector<int> solution(vector<string> gems) {
 
 		bool flag = false;
 		for (int start = 0; start + mid - 1 < gems.size(); ++start) {
-			if (segmentTree.query(start, start + mid - 1).size() == totalGems) {
+
+			ull bitmask = segmentTree.query(start, start + mid - 1);
+
+			if ( bitmask == ((1LL << totalGems) - 1)) {
 				flag = true;
 
 				if (minLen >= mid) {
@@ -138,4 +148,15 @@ vector<int> solution(vector<string> gems) {
 	return answer;
 }
 
+int main() {
 
+	vector<string> in;
+
+	for (int i = 0; i < 5; ++i) {
+		string s;
+		cin >> s;
+		in.push_back(s);
+	}
+
+	solution(in);
+}
