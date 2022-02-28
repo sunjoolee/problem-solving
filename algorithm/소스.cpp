d15ec75d8e60f6de92bb997afd151ebee4b23b91
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-28 20:17:43 +0900

    프로그래머스 비밀지도

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,93 +1,31 @@
 #include <string>
+#include <stack>
 #include <vector>
-#include <queue>
 #include <algorithm>
+
 using namespace std;
 
-const int INF = 987654321;
-
-//roads의 행은 [P, Q, S]로 이루어져 있습니다.
-//P에서 Q로 갈 수 있는 길이 있으며, 길을 따라 이동하는데 S만큼 시간이 걸립니다.
-vector<vector<int>> Groads;
-//함정 노드
-vector<int> Gtraps;
-
-//adj[u] = {v, cost} u -cost-> v
-vector<vector<pair<int, int>>> adj(1000, vector<pair<int, int>>());
-
-//reverseAdj[u] = {v, cost} u <-cost- v
-vector<vector<pair<int, int>>> reverseAdj(1000, vector<pair<int, int>>());
-
-vector<bool> isTrap(1000, false);
-
-
-//end의 최단 거리를 반환하는 bfs
-int bfs(int n, int startnode, int endnode) {
+vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
 	
-	vector<int> dist(n, INF);
-	vector<int> visited(n, 0);
+	vector<string> answer;
 	
-	//<node, 거리>
-	queue <pair<int, int>> q;
-	q.push({ startnode, 0 });
-
-	while (!q.empty()) {
-		int curnode = q.front().first;
-		int curcost = q.front().second;
-		q.pop();
-
-		if (!isTrap[curnode] && visited[curnode] == 1) continue;
-		visited[curnode] = 1;
-
-		if (isTrap[curnode]) {
-			vector<pair<int, int>> tmp(adj[curnode].begin(), adj[curnode].end());
-
-			adj[curnode].clear();
-			adj[curnode].assign(reverseAdj[curnode].begin(), reverseAdj[curnode].end());
-
-			reverseAdj[curnode].clear();
-			reverseAdj[curnode].assign(tmp.begin(), tmp.end());
-		}
-			
-		for (int i = 0; i < adj[curnode].size(); ++i) {
-			int nextnode = adj[curnode][i].first;
-			int nextcost = curcost + adj[curnode][i].second;
-
-			if (isTrap[nextnode]) {
-				q.push({ nextnode, nextcost });
-			}
-				
-			else if (visited[nextnode] == 0 || dist[nextnode] > nextcost){
-				dist[nextnode] = nextcost;
-				q.push({ nextnode, nextcost });
-			}	
-		}
+	for (int i = 0; i < n; ++i) {
 		
+		int num = arr1[i] | arr2[i];
+		
+		stack<char> st;
+		while (num > 0) {
+			if (num % 2) st.push('#');
+			else st.push(' ');
+			num /= 2;
+		}
+		while (st.size() > n) {
+			row += " ";
+		}
+
+		string row = "";
+		answer.push_back(row);
 	}
-	return dist[endnode];
-}
-
-int solution(int n, int start, int end, vector<vector<int>> roads, vector<int> traps) {
-
-	Groads = roads;
-	Gtraps = traps;
-
-	for (int i = 0; i < roads.size(); ++i) {
-		vector<int> edge = roads[i];
-
-		int u = edge[0] - 1; 
-		int v = edge[1] - 1; 
-		int cost = edge[2];
-
-		adj[u].push_back({ v, cost }); 
-		reverseAdj[v].push_back({ u, cost });
-	}
-
-	for (int i = 0; i < traps.size(); ++i) {
-		isTrap[traps[i] - 1] = true;
-	}
-
-	int answer = bfs(n, start - 1, end - 1);
 
 	return answer;
 }
