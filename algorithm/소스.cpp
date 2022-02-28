052b72c4dc64e23bb4f023691e3d95c2c0cb89f7
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-28 13:10:02 +0900

    프로그래머스 거리두기 확인하기

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,51 +1,68 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <map>
-#include <iostream>
+#include <utility>
 
 using namespace std;
 
-map <string, char> stringToChar;
+//global places
+vector<string> Gplaces;
 
-int solution(string s) {
-
-	stringToChar.insert({ "zero", '0' });
-	stringToChar.insert({ "one", '1' });
-	stringToChar.insert({ "two", '2' });
-	stringToChar.insert({ "three", '3' });
-	stringToChar.insert({ "four", '4' });
-	stringToChar.insert({ "five", '5' });
-	stringToChar.insert({ "six", '6' });
-	stringToChar.insert({ "seven", '7' });
-	stringToChar.insert({ "eight", '8' });
-	stringToChar.insert({ "nine", '9' });
-
-	int answer = 0;
-	string answerString = "";
-	
-	string in;
-	for (int i = 0; i < s.length(); ++i) {
-		char ch = s[i];
-
-		if (ch >= '0' && ch <= '9') {
-			answerString += ch;
-			continue;
-		}
-
-		in += ch;
-		if (stringToChar.find("in") != stringToChar.end()) {
-			answerString += stringToChar[in];
-			in = "";
-		}
-	}
-
-	answer = stoi(answerString);
-	return answer;
+bool inRange(int r, int w) {
+	if (r < 0 || r >= 5) return false;
+	if (w < 0 || w >= 5) return false;
+	return true;
 }
 
-int main() {
-	string s;
-	cin >> s;
-	solution(s);
+//인접한 상하좌우 
+bool check1(int r, int w) {
+	if (inRange(r + 1, w) && Gplaces[r + 1][w] == 'P') return false;
+	if (inRange(r - 1, w) && Gplaces[r - 1][w] == 'P') return false;
+	if (inRange(r, w + 1) && Gplaces[r][w + 1] == 'P') return false;
+	if (inRange(r, w - 1) && Gplaces[r][w - 1] == 'P') return false;
+	return true;
+}
+
+//먼 상하좌우 
+bool check2(int r, int w) {
+	if (inRange(r + 2, w) && Gplaces[r + 2][w] == 'P') return Gplaces[r + 1][w] == 'X';
+	if (inRange(r - 2, w) && Gplaces[r - 2][w] == 'P') return Gplaces[r - 1][w] == 'X';
+	if (inRange(r, w + 2) && Gplaces[r][w + 2] == 'P') return Gplaces[r][w + 1] == 'X';
+	if (inRange(r, w - 2) && Gplaces[r][w - 2] == 'P') return Gplaces[r][w - 1] == 'X';
+	return true;
+}
+
+//대각선 
+bool check3(int r, int w) {
+	if (inRange(r - 1, w - 1) && Gplaces[r - 1][w - 1] == 'P') return (Gplaces[r - 1][w] == 'X' && Gplaces[r][w - 1] == 'X');
+	if (inRange(r - 1, w + 1) && Gplaces[r - 1][w + 1] == 'P') return (Gplaces[r - 1][w] == 'X' && Gplaces[r][w + 1] == 'X');
+	if (inRange(r + 1, w - 1) && Gplaces[r + 1][w - 1] == 'P') return (Gplaces[r][w - 1] == 'X' && Gplaces[r + 1][w] == 'X');
+	if (inRange(r + 1, w + 1) && Gplaces[r + 1][w + 1] == 'P') return (Gplaces[r][w + 1] == 'X' && Gplaces[r + 1][w] == 'X');
+	return true;
+}
+
+vector<int> solution(vector<vector<string>> places) {
+	vector<int> answer;
+	
+	for (int i = 0; i < 5; ++i) {
+		
+		Gplaces = places[i];
+		bool flag = true;
+
+		for (int j = 0; j < 5; ++j) {
+			for (int k = 0; k < 5; ++k) {
+				if (Gplaces[j][k] == 'P') {
+					if (!check1(j, k)) flag = false;
+					else if (!check2(j, k)) flag = false;
+					else if (!check3(j, k)) flag = false;
+				}
+				if (!flag) break;
+			}
+			if (!flag) break;
+		}
+
+		if (flag) answer.push_back(1);
+		else answer.push_back(0);
+	}
+	return answer;
 }
