cef6752adad6425e965fc36697dcead10e84641e
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-28 21:58:26 +0900

    프로그래머스 캐시

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,65 +1,49 @@
 #include <string>
-#include <map>
+#include <vector>
 #include <algorithm>
+
+const int INF = 987654321;
 using namespace std;
 
-int solution(string str1, string str2) {
+int solution(int cacheSize, vector<string> cities) {
+	int answer = 0;
 
-	//대소문자 구분 X -> 모두 소문자로 변환
-	transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
-	transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+	vector<pair<string, int>> cache;
 
-	map<string, int> m1;
-	map<string, int> m2;
-
-	for (int i = 0; i < str1.size() - 1; ++i) {
-		if (str1[i] < 'a' || str1[i] > 'z') continue;
-		if (str1[i+1] < 'a' || str1[i+1] > 'z') continue;
-
-		string back = str1.substr(i, 2);
-		if (m1.find(back) == m1.end()) {
-			m1[back] = 1;
+	for (int i = 0; i < cities.size(); ++i) {
+		string city = cities[i];
+		transform(city.begin(), city.end(), city.begin(), ::tolower);	
+		bool hit = false;
+		for (int j = 0; j < cache.size(); ++j) {
+			if (cache[j].first == city) {
+				//hit
+				hit = true;
+				answer += 1;
+				//캐시 갱신
+				cache[j].second = i;
+				break;
+			}
 		}
-		else m1[back]++;
-	}
-	for (int i = 0; i < str2.size() - 1; ++i) {
-		if (str2[i] < 'a' || str2[i] > 'z') continue;
-		if (str2[i + 1] < 'a' || str2[i + 1] > 'z') continue;
+		if (hit) continue;
 
-		string back = str2.substr(i, 2);
-		if (m2.find(back) == m2.end()) {
-			m2[back] = 1;
-		}
-		else m2[back]++;
-	}
-
-	//교집합의 크기, 합집합의 크기 구하기
-	int gyo = 0;
-	int hap = 0;
-
-	for (auto it = m1.begin(); it != m1.end(); ++it) {
-		string str = it->first;
-		int val = it->second;
-
-		if (m2.find(str) == m2.end()) {
-			hap += val;
+		//miss
+		answer += 5;
+		//캐시에 추가
+		if (cache.size() < cacheSize) {
+			cache.push_back({ city, i });
 		}
 		else {
-			gyo += min(m2[str], val);
-			hap += max(m2[str], val);
-			m2.erase(str);
+			int minUsed = INF;
+			int minIndex;
+			for (int j = 0; j < cache.size(); ++j) {
+				if (cache[j].second < minUsed) {
+					minUsed = cache[j].second;
+					minIndex = j;
+				}
+			}
+			cache[minIndex] = { city, i };
 		}
 	}
-	//m1과 중복되지 않은 str만 남은 상태
-	for (auto it = m2.begin(); it != m2.end(); ++it) {
-		hap += it ->second;
-	}
 
-	int answer = 0;
-	answer = 65536 * gyo / hap;
 	return answer;
-}
-
-int main() {
-	solution("E=M*C^2", "e=m*c^2");
 }
