33e6a6c2c32c7c3006685fa497121752fa6b01b6
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-28 20:29:21 +0900

    프로그래머스 다크 게임

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,31 +1,53 @@
 #include <string>
-#include <stack>
-#include <vector>
-#include <algorithm>
+#include <math.h>
+#include <deque>
 
 using namespace std;
 
-vector<string> solution(int n, vector<int> arr1, vector<int> arr2) {
-	
-	vector<string> answer;
-	
-	for (int i = 0; i < n; ++i) {
-		
-		int num = arr1[i] | arr2[i];
-		
-		stack<char> st;
-		while (num > 0) {
-			if (num % 2) st.push('#');
-			else st.push(' ');
-			num /= 2;
-		}
-		while (st.size() > n) {
-			row += " ";
-		}
 
-		string row = "";
-		answer.push_back(row);
+int solution(string dartResult) {
+	
+	deque <int> score;
+
+	string num = "";
+	for (int i = 0; i < dartResult.length(); ++i) {
+		char ch = dartResult[i];
+		//숫자인 경우
+		if (ch >= '0' && ch <= '9') num += ch;
+		//보너스인 경우
+		else if (ch == 'S') {
+			score.push_back(stoi(num));
+			num = "";
+		}
+		else if (ch == 'D') {
+			score.push_back(stoi(num) * stoi(num));
+			num = "";
+		}
+		else if (ch == 'T') {
+			score.push_back(stoi(num) * stoi(num) * stoi(num));
+			num = "";
+		}
+		//옵션인 경우
+		else if (ch == '*') {
+			int sc1 = score.back();
+			score.pop_back();
+			int sc2 = score.back();
+			score.pop_back();
+
+			score.push_back(sc2 * 2);
+			score.push_back(sc1 * 2);
+		}
+		else if (ch == '#') {
+			int sc = score.back();
+			score.pop_back();
+			
+			score.push_back(-sc);
+		}
 	}
+
+	int answer = 0;
+	
+
 
 	return answer;
 }
