0048dc0c6d9d98449ff5b6cb560aad4fd029cd60
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-02-28 13:42:47 +0900

    프로그래머스 표 편집
    채점 결과
    정확성: 30.0
    효율성: 35.0
    합계: 65.0 / 100.0

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,68 +1,98 @@
 #include <string>
+#include <stack>
 #include <vector>
-#include <algorithm>
-#include <utility>
+#include <iostream>
 
 using namespace std;
 
-//global places
-vector<string> Gplaces;
+string solution(int n, int k, vector<string> cmd) {
+	string answer = "";
 
-bool inRange(int r, int w) {
-	if (r < 0 || r >= 5) return false;
-	if (w < 0 || w >= 5) return false;
-	return true;
-}
+	vector<char> table(n, 'O');
+	stack <int> deleted;
 
-//인접한 상하좌우 
-bool check1(int r, int w) {
-	if (inRange(r + 1, w) && Gplaces[r + 1][w] == 'P') return false;
-	if (inRange(r - 1, w) && Gplaces[r - 1][w] == 'P') return false;
-	if (inRange(r, w + 1) && Gplaces[r][w + 1] == 'P') return false;
-	if (inRange(r, w - 1) && Gplaces[r][w - 1] == 'P') return false;
-	return true;
-}
+	for (int i = 0; i < cmd.size(); ++i) {
+		string curCmd = cmd[i];
 
-//먼 상하좌우 
-bool check2(int r, int w) {
-	if (inRange(r + 2, w) && Gplaces[r + 2][w] == 'P') return Gplaces[r + 1][w] == 'X';
-	if (inRange(r - 2, w) && Gplaces[r - 2][w] == 'P') return Gplaces[r - 1][w] == 'X';
-	if (inRange(r, w + 2) && Gplaces[r][w + 2] == 'P') return Gplaces[r][w + 1] == 'X';
-	if (inRange(r, w - 2) && Gplaces[r][w - 2] == 'P') return Gplaces[r][w - 1] == 'X';
-	return true;
-}
-
-//대각선 
-bool check3(int r, int w) {
-	if (inRange(r - 1, w - 1) && Gplaces[r - 1][w - 1] == 'P') return (Gplaces[r - 1][w] == 'X' && Gplaces[r][w - 1] == 'X');
-	if (inRange(r - 1, w + 1) && Gplaces[r - 1][w + 1] == 'P') return (Gplaces[r - 1][w] == 'X' && Gplaces[r][w + 1] == 'X');
-	if (inRange(r + 1, w - 1) && Gplaces[r + 1][w - 1] == 'P') return (Gplaces[r][w - 1] == 'X' && Gplaces[r + 1][w] == 'X');
-	if (inRange(r + 1, w + 1) && Gplaces[r + 1][w + 1] == 'P') return (Gplaces[r][w + 1] == 'X' && Gplaces[r + 1][w] == 'X');
-	return true;
-}
-
-vector<int> solution(vector<vector<string>> places) {
-	vector<int> answer;
-	
-	for (int i = 0; i < 5; ++i) {
-		
-		Gplaces = places[i];
-		bool flag = true;
-
-		for (int j = 0; j < 5; ++j) {
-			for (int k = 0; k < 5; ++k) {
-				if (Gplaces[j][k] == 'P') {
-					if (!check1(j, k)) flag = false;
-					else if (!check2(j, k)) flag = false;
-					else if (!check3(j, k)) flag = false;
+		if (curCmd[0] == 'D') {
+			//move만큼 아래로 이동
+			int move = stoi(curCmd.substr(2));
+			while (move != 0) {
+				if (table[k + 1] == 'O') {
+					k++;
+					move--;
 				}
-				if (!flag) break;
+				else k++;
 			}
-			if (!flag) break;
 		}
+		else if (curCmd[0] == 'U') {
+			//move만큼 위로 이동
+			int move = stoi(curCmd.substr(2));
+			while (move != 0) {
+				if (table[k - 1] == 'O') {
+					k--;
+					move--;
+				}
+				else k--;
+			}
+		}
+		else if (curCmd[0] == 'C') {
+			//선택된 행 삭제
+			table[k] ='X';
+			deleted.push(k);
+			
+			int tmp = k;
+			bool move = false;
 
-		if (flag) answer.push_back(1);
-		else answer.push_back(0);
+			//한 칸 아래로 이동
+			while(!move) {
+				if (tmp + 1 == n) break;
+
+				if (table[tmp + 1] == 'O') {
+					k = tmp + 1;
+					move = true;
+				}
+				else tmp++;
+			}
+
+			//아래 칸이 없는 경우 한 칸 위로 이동
+			tmp = k;
+			while(!move) {
+				if (table[tmp - 1] == 'O') {
+					k = tmp - 1;
+					move = true;
+				}
+				else tmp--;
+			}
+		}
+		else if (curCmd[0] == 'Z') {
+			//가장 최근에 삭제된 행 복원
+			table[deleted.top()] = 'O';
+			deleted.pop();
+		}
+	}
+
+	for (int i = 0; i < n; ++i) {
+		answer += table[i];
 	}
 	return answer;
+}
+
+int main() {
+	int n, k;
+	vector<string> cmd;
+
+	n = 8;
+	k = 2;
+	cmd.push_back("D 2");
+	cmd.push_back("C");
+	cmd.push_back("U 3");
+	cmd.push_back("C");
+	cmd.push_back("D 4");
+	cmd.push_back("C");
+	cmd.push_back("U 2");
+	cmd.push_back("Z");
+	cmd.push_back("Z");
+
+	solution(n, k, cmd);
 }
