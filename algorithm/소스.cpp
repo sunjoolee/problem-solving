905880c5144064618ca63971e32401e27f216db2
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-12 20:05:36 +0900

    프로그래머스 프렌즈4블록

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,91 +1,112 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-typedef long long int ll;
+//높이 M, 폭 N
+int M, N;
 
+vector<vector<int>> intBoard;
+vector<vector<int>> eraseBoard;
 
-//로그 형식 2016-09-15 03:10:33.020 0.011s
-
-//시:분:초.xxx -> 초.xxx  * 1000
-ll timeToll(string time) {
-	//time 형식 03:10:33.020
-
-	ll res = 0;
-	res += 3600 * stoll(time.substr(0, 2));
-	res += 60 * stoll(time.substr(3, 2));
-	res += stoll(time.substr(6, 2));
-	res *= 1000;
-	res += stoll((time.substr(9)));
-
-	return res;
+bool inRange(int r, int w) {
+	if (r < 0 || r >= M) return false;
+	if (w < 0 || w >= N) return false;
+	return true;
 }
 
-//구간: time ~ time + 1000 - 1 
-bool inRange(ll time, ll start, ll end) {
-	ll timeStart = time;
-	ll timeEnd = time + 1000 - 1;
-
-	//구간의 앞에 걸쳐있는 경우
-	//start - 구간 시작 - end - 구간 끝
-	if (start <= timeStart && timeStart <= end && end <= timeEnd) return true;
-
-	//구간 안에 로그가 포함된 경우
-	//구간 시작 - start - end - 구간 끝
-	if (timeStart <= start && end <= timeEnd) return true;
-
-	//구간의 뒤에 걸쳐있는 경우
-	//구간 시작 - start - 구간 끝 - end
-	if (timeStart <= start && start <= timeEnd && timeEnd <= end) return true;
-
-	//로그 안에 구간이 포함된 경우
-	//start - 구간 시작 - 구간 끝 - end
-	if (start <= timeStart && timeEnd <= end) return true;
-
+//(r, w)(r, w+1)
+//(r+1, w)(r+1, w+1)
+// 2x2 정사각형의 좌표 intBoard에서 모두 같은지 확인
+bool findSquare(int r, int w) {
+	if (!inRange(r, w + 1) || !inRange(r + 1, w) || !inRange(r + 1, w + 1)) return false;
+	
+	int cur = intBoard[r][w];
+	if ((cur == intBoard[r][w + 1]) && (cur == intBoard[r + 1][w]) && (cur == intBoard[r + 1][w + 1])) return true;
 	else return false;
 }
 
-int solution(vector<string> lines) {
+
+//(r, w)(r, w+1)
+//(r+1, w)(r+1, w+1)
+// 2x2 정사각형의 좌표 eraseBoard에 체크하며, 새롭게 체크된 칸이 몇 개인지 반환
+int eraseSquare(int r, int w) {
+	int cnt = 0;
 	
-	vector<pair<ll, ll>> startEndPair;
+	if (eraseBoard[r][w] == 0) cnt++;
+	if (eraseBoard[r][w+1] == 0) cnt++;
+	if (eraseBoard[r+1][w] == 0) cnt++;
+	if (eraseBoard[r+1][w+1] == 0) cnt++;
 
-	for (int i = 0; i < lines.size(); ++i) {
-		//맨 앞 날짜 제거 & 맨 마지막 s 제거
-		string log = lines[i].substr(11, 18);
+	eraseBoard[r][w] = eraseBoard[r][w+1] = eraseBoard[r+1][w] = eraseBoard[r+1][w+1] = 1;
 
-		ll S = timeToll(log.substr(0, 12));
-		ll T = stod(log.substr(13)) * 1000;
+	return cnt;
+}
 
-		//로그 시작 시점이 전날로 넘어간 경우 00:00:00.000에 시작한 것으로 처리
-		startEndPair.push_back({ S - T + 1 >= 0 ? S - T + 1 : 0, S });
+//(r, w)이 삭제된 경우 호출 
+void moveBoardDown(int r, int w) {
+	for (int i = r; i > 0 ; --i) {
+		intBoard[i][w] = intBoard[i - 1][w];
+	}
+	intBoard[0][w] = -1;
+	return;
+}
+
+int solution(int m, int n, vector<string> board) {
+
+	//전역변수 초기화
+	M = m;
+	N = n;
+	for (int i = 0; i < M; ++i) {
+		vector<int> boardBuff;
+		vector<int> eraseBuff;
+		for (int j = 0; j < N; ++j) {
+			boardBuff.push_back(board[i][j]);
+			eraseBuff.push_back(0);
+		}
+		intBoard.push_back(boardBuff);
+		eraseBoard.push_back(eraseBuff);
 	}
 
-	sort(startEndPair.begin(), startEndPair.end());
 
 	int answer = 0;
 
-	for (int i = 0; i < startEndPair.size(); ++i) {
-		// time에서 시작하는 1초 구간에 포함되는 로그 세기
-		int cnt = 0;
-		ll time = startEndPair[i].first;
-		for (int j = 0; j < startEndPair.size(); ++j) {
-			if (inRange(time, startEndPair[j].first, startEndPair[j].second)) {
-				++cnt;
-			}
-		}
-		answer = max(answer, cnt);
+	while (true) {
+		int eraseCnt = 0;
+		//intBoard 순회하며 2x2 칸 찾기
+		for (int r = 0; r < M; ++r) {
+			for (int w = 0; w < N; ++w) {
+				// intBoard 빈칸인 경우 스킵
+				if (intBoard[r][w] == -1) continue;
 
-		cnt = 0;
-		time = startEndPair[i].second; 
-		for (int j = 0; j < startEndPair.size(); ++j) {
-			if (inRange(time, startEndPair[j].first, startEndPair[j].second)) {
-				++cnt;
+				// 2x2 칸이 모두 같은지 검사
+				// 모두 같다면 eraseBoard에 지워지는 칸 체크 & 새롭게 체크된 칸이 몇 개 인지 계산
+				if (findSquare(r, w)) eraseCnt += eraseSquare(r, w);
 			}
 		}
-		answer = max(answer, cnt);
+
+		//지워진 칸 없는 경우 게임 종료
+		if (eraseCnt == 0) break;
+		else answer += eraseCnt;
+
+		//intBoard 칸 위치 조정
+		for (int w = 0; w < N; ++w) {
+			for (int r = 0; r < M; ++r) {
+				if (eraseBoard[r][w] == 1) {
+					moveBoardDown(r, w);
+					eraseBoard[r][w] = 0;
+				}
+			}
+		}
 	}
 
 	return answer;
 }
 
+/*
+int main() {
+	vector<string> v = { "CCBDE", "AAADE", "AAABF", "CCBBF" };
+	solution(4, 5, v);
+}
+*/
