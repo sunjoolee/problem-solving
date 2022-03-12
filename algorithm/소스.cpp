996c95d529bc648f0ccc750f077275f73cfa0067
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-12 21:28:47 +0900

    프로그래머스 셔틀버스

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,112 +1,102 @@
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
-//높이 M, 폭 N
-int M, N;
+typedef long long int ll;
+const int MAXTIME = 25 * 60;
 
-vector<vector<int>> intBoard;
-vector<vector<int>> eraseBoard;
-
-bool inRange(int r, int w) {
-	if (r < 0 || r >= M) return false;
-	if (w < 0 || w >= N) return false;
-	return true;
+ll timeToll(string time) {
+	ll lltime = 0;
+	lltime += stoll(time.substr(0, 2)) * 60;
+	lltime += stoll(time.substr(3, 2));
+	return lltime;
 }
 
-//(r, w)(r, w+1)
-//(r+1, w)(r+1, w+1)
-// 2x2 정사각형의 좌표 intBoard에서 모두 같은지 확인
-bool findSquare(int r, int w) {
-	if (!inRange(r, w + 1) || !inRange(r + 1, w) || !inRange(r + 1, w + 1)) return false;
-	
-	int cur = intBoard[r][w];
-	if ((cur == intBoard[r][w + 1]) && (cur == intBoard[r + 1][w]) && (cur == intBoard[r + 1][w + 1])) return true;
-	else return false;
+string llTotime(ll lltime) {
+	ll hour = lltime / 60;
+	ll min = lltime % 60;
+
+	string time = ""; 
+
+	//HH:MM
+	if (hour < 10) time = "0" + to_string(hour);
+	else time = to_string(hour);
+	time += ":";
+	if (min < 10) time = "0" + to_string(min);
+	else time = to_string(min);
+
+	return time;
 }
 
+string solution(int n, int t, int m, vector<string> timetable) {
 
-//(r, w)(r, w+1)
-//(r+1, w)(r+1, w+1)
-// 2x2 정사각형의 좌표 eraseBoard에 체크하며, 새롭게 체크된 칸이 몇 개인지 반환
-int eraseSquare(int r, int w) {
-	int cnt = 0;
-	
-	if (eraseBoard[r][w] == 0) cnt++;
-	if (eraseBoard[r][w+1] == 0) cnt++;
-	if (eraseBoard[r+1][w] == 0) cnt++;
-	if (eraseBoard[r+1][w+1] == 0) cnt++;
+	vector<ll> crew;
 
-	eraseBoard[r][w] = eraseBoard[r][w+1] = eraseBoard[r+1][w] = eraseBoard[r+1][w+1] = 1;
-
-	return cnt;
-}
-
-//(r, w)이 삭제된 경우 호출 
-void moveBoardDown(int r, int w) {
-	for (int i = r; i > 0 ; --i) {
-		intBoard[i][w] = intBoard[i - 1][w];
+	for (int i = 0; i < timetable.size(); ++i) {
+		ll lltime = timeToll(timetable[i]);
+		crew.push_back(lltime);
 	}
-	intBoard[0][w] = -1;
-	return;
-}
+	sort(crew.begin(), crew.end());
 
-int solution(int m, int n, vector<string> board) {
+	//셔틀을 타고 사무실로 갈 수 있는 도착 시간 중 제일 늦은 시간
+	//= 마지막 셔틀을 탈 수 있기 위해 도착해야하는 시간
 
-	//전역변수 초기화
-	M = m;
-	N = n;
-	for (int i = 0; i < M; ++i) {
-		vector<int> boardBuff;
-		vector<int> eraseBuff;
-		for (int j = 0; j < N; ++j) {
-			boardBuff.push_back(board[i][j]);
-			eraseBuff.push_back(0);
+	ll busTime = timeToll("09:00");
+
+	//마지막 셔틀 전까지 태워보낼 수 있는 크루들 태워 보내기
+	while (n > 1) {
+		int seats = m;
+
+		for (int i = 0; i < crew.size(); ++i) {
+			ll crewTime = crew[i];
+			if (crewTime > busTime) break;
+
+			seats--;
+			crew[i] = MAXTIME;
+			if (seats == 0) break;
 		}
-		intBoard.push_back(boardBuff);
-		eraseBoard.push_back(eraseBuff);
+		sort(crew.begin(), crew.end());
+
+		busTime += t;
+		n--;
 	}
 
+	//마지막 셔틀을 기다리는 크루들의 <대기 시간, 명 수>
+	vector<pair<ll, int>> lastCrew;
 
-	int answer = 0;
+	for (int i = 0; i < crew.size(); ++i) {
+		ll crewTime = crew[i];
+		if (crewTime > busTime) break;
 
-	while (true) {
-		int eraseCnt = 0;
-		//intBoard 순회하며 2x2 칸 찾기
-		for (int r = 0; r < M; ++r) {
-			for (int w = 0; w < N; ++w) {
-				// intBoard 빈칸인 경우 스킵
-				if (intBoard[r][w] == -1) continue;
-
-				// 2x2 칸이 모두 같은지 검사
-				// 모두 같다면 eraseBoard에 지워지는 칸 체크 & 새롭게 체크된 칸이 몇 개 인지 계산
-				if (findSquare(r, w)) eraseCnt += eraseSquare(r, w);
+		bool inLastCrew = false;
+		for (int j = 0; j < lastCrew.size(); ++j) {
+			if (lastCrew[j].first == crewTime) {
+				lastCrew[j].second++;
+				inLastCrew = true;
+				break;
 			}
 		}
-
-		//지워진 칸 없는 경우 게임 종료
-		if (eraseCnt == 0) break;
-		else answer += eraseCnt;
-
-		//intBoard 칸 위치 조정
-		for (int w = 0; w < N; ++w) {
-			for (int r = 0; r < M; ++r) {
-				if (eraseBoard[r][w] == 1) {
-					moveBoardDown(r, w);
-					eraseBoard[r][w] = 0;
-				}
-			}
+		if (!inLastCrew) {
+			lastCrew.push_back({ crewTime, 1 });
 		}
 	}
 
+	string answer = "";
+	int seats = m;
+	for (int i = 0; i < lastCrew.size(); ++i) {
+		if (seats - lastCrew[i].second > 1) {
+			seats -= lastCrew[i].second;
+		}
+		else {
+			answer = llTotime(lastCrew[i].first - 1);
+			return answer;
+		}
+	}
+
+	answer = llTotime(busTime);
 	return answer;
-}
 
-/*
-int main() {
-	vector<string> v = { "CCBDE", "AAADE", "AAABF", "CCBBF" };
-	solution(4, 5, v);
 }
-*/
