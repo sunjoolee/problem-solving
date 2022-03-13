15b0034e02a5616144c14f25230635660ed4f0dd
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-13 23:49:47 +0900

    프로그래머스 자동완성
    아직도 시간초과
    well-known 알고리즘을 찾아봐야할 것 같다

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -4,38 +4,60 @@
 
 using namespace std;
 
-
 int solution(vector<string> words) {
-	int answer = 0;
-	for (int i = 0; i < words.size(); ++i) {
-		
-		//해당 단어를 검색하기 위해 사용해야하는 문자열
-		//searchWord 한 글자씩 append 해주며 구해준다
-		//필요는 없지만 알고리즘 논리 이해를 위해 계산해줌
-		string search = "";
 
+	int N = words.size();
+
+	int answer = 0;
+
+	//다른 단어와 공통된 접미사가 있는 경우, 접미사가 끝나는 지점의 인덱스 저장
+	vector<int> maxPrefix(N, -1);
+
+	for (int i = 0; i < words.size(); ++i) {
 		//검색 범위
 		//공통된 접두사를 가진 단어들 사이에서만 재검색되도록 검색 범위 계속 갱신해주기 
-		vector<string> searchRange(words.begin(), words.end());
+		//맨 처음에는 모든 단어가 검색 범위에 해당됨
+		vector<bool> searchRange(N, true);
+
+		//공통된 접미사를 갖는 단어만 검색 범위에 들도록 함 
+		if (maxPrefix[i] != -1) {
+			for (int j = 0; j < N; ++j) {
+				if (words[j].length() <= maxPrefix[i])
+					searchRange[j] = false;
+				else if (words[i].substr(0, maxPrefix[i] + 1) != words[j].substr(0, maxPrefix[i] + 1))
+					searchRange[j] = false;
+			}
+		}
 		
-		for (int j = 0; j < words[i].length(); ++j) {
+		//공통된 최대 길이 접미사 이후의 단어부터 비교
+		if(maxPrefix[i] != -1) 
+			answer += maxPrefix[i];
+		
+		for (int j = maxPrefix[i] + 1; j < words[i].length(); ++j) {
 			char searchWord = words[i][j];
-			search.append(to_string(searchWord));
 			answer++;
 
 			//search 문자열로 검색했을 때 검색 결과가 하나만 반환되는지 검사
-			//이 검색 결과를 다음 검색 때 검색 범위로 사용함
-			vector<string> searchResult;
-			for (int k = 0; k < searchRange.size(); ++k) {
-				if (searchRange[k][0] == searchWord) {
-					searchResult.push_back(searchRange[k].substr(1));
+			//이 검색 결과에 포함되는 경우 다음 검색 때 검색 범위로 사용함
+			int searchCnt = 1;
+			for (int k = 0; k < N; ++k) {
+				//자기 자신 스킵
+				if (k == i) continue;
+
+				//검색 범위에 포함된 단어인 경우 검사
+				if (searchRange[k]) {
+					if (words[k].length() > j && words[k][j] == searchWord) {
+						searchCnt++;
+
+						//words[k]와 words[i]는 문자열의 인덱스 0~j까지 공통된 접미사를 가진다는 의미
+						maxPrefix[i] = j;
+						maxPrefix[k] = max(maxPrefix[k],j);
+					}
+					//검색 결과에 포함되지 않으면 검색 범위에서 제외
+					else searchRange[k] = false;
 				}
 			}
-
-			if (searchResult.size() == 1) break;
-			
-			searchRange.clear(); 
-			searchRange.assign(searchResult.begin(), searchResult.end());
+			if (searchCnt == 1) break;
 		}
 	}
 	
