d843702e4e52aea1def5f39d50ec85f97eeaf784
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-13 21:20:27 +0900

    프로그래머스 방금그곡 ing
    채점 결과
    정확성: 80.0
    합계: 80.0 / 100.0

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,102 +1,119 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
-typedef long long int ll;
-const int MAXTIME = 25 * 60;
+//string으로 주어진 음악 -> 각 음 별로 나누어 벡터에 저장
+vector<string> musicToVec(string music) {
+	
+	vector<string> res;
+	for (int i = 0; i < music.length(); ++i) {
+		char note = music[i];
 
-ll timeToll(string time) {
-	ll lltime = 0;
-	lltime += stoll(time.substr(0, 2)) * 60;
-	lltime += stoll(time.substr(3, 2));
-	return lltime;
-}
-
-string llTotime(ll lltime) {
-	ll hour = lltime / 60;
-	ll min = lltime % 60;
-
-	string time = ""; 
-
-	//HH:MM
-	if (hour < 10) time = "0" + to_string(hour);
-	else time = to_string(hour);
-	time += ":";
-	if (min < 10) time = "0" + to_string(min);
-	else time = to_string(min);
-
-	return time;
-}
-
-string solution(int n, int t, int m, vector<string> timetable) {
-
-	vector<ll> crew;
-
-	for (int i = 0; i < timetable.size(); ++i) {
-		ll lltime = timeToll(timetable[i]);
-		crew.push_back(lltime);
-	}
-	sort(crew.begin(), crew.end());
-
-	//셔틀을 타고 사무실로 갈 수 있는 도착 시간 중 제일 늦은 시간
-	//= 마지막 셔틀을 탈 수 있기 위해 도착해야하는 시간
-
-	ll busTime = timeToll("09:00");
-
-	//마지막 셔틀 전까지 태워보낼 수 있는 크루들 태워 보내기
-	while (n > 1) {
-		int seats = m;
-
-		for (int i = 0; i < crew.size(); ++i) {
-			ll crewTime = crew[i];
-			if (crewTime > busTime) break;
-
-			seats--;
-			crew[i] = MAXTIME;
-			if (seats == 0) break;
+		if (note != '#') {
+			res.push_back(to_string(note));
 		}
-		sort(crew.begin(), crew.end());
+		else {
+			string prevNote = res.back();
+			res.pop_back();
 
-		busTime += t;
-		n--;
+			res.push_back(prevNote + to_string(note));
+		}
 	}
+	return res;
+}
 
-	//마지막 셔틀을 기다리는 크루들의 <대기 시간, 명 수>
-	vector<pair<ll, int>> lastCrew;
+//재생된 시간동안 재생된 음악 반환
+vector<string> getPlayedMusic(vector<string> music, int playedLen) {
+	if (playedLen == music.size()) return music;
+	
+	vector<string> res;
+	for (int i = 0; i < playedLen; ++i) {
+		res.push_back(music[i % music.size()]);
+	}
+	return res;
+}
 
-	for (int i = 0; i < crew.size(); ++i) {
-		ll crewTime = crew[i];
-		if (crewTime > busTime) break;
+//재생된 음악에 들은 멜로디가 있는지 검사
+bool melodyInPlayedMusic(vector<string> music, vector<string> melody) {
+	
+	if (melody.size() > music.size()) return false;
 
-		bool inLastCrew = false;
-		for (int j = 0; j < lastCrew.size(); ++j) {
-			if (lastCrew[j].first == crewTime) {
-				lastCrew[j].second++;
-				inLastCrew = true;
+	bool in = false;
+	for (int i = 0; i < music.size() - melody.size(); ++i) {
+		bool flag = true;
+		for (int j = 0; j < melody.size(); ++j) {
+			if (music[i + j] != melody[j]) {
+				flag = false;
 				break;
 			}
 		}
-		if (!inLastCrew) {
-			lastCrew.push_back({ crewTime, 1 });
+		if (flag) in = true;
+	}
+	return in;
+}
+
+//재생된 시간 계산
+int getPlayedTime(string startTime, string endTime) {
+	//HH:MM
+	int start = stoi(startTime.substr(0, 2)) * 60 + stoi(startTime.substr(3, 2));
+	int end = stoi(endTime.substr(0, 2)) * 60 + stoi(endTime.substr(3, 2));
+	
+	return end - start;
+}
+
+bool cmp(pair<string, int> a, pair<string, int> b) {
+	return a.second > b.second;
+}
+
+string solution(string m, vector<string> musicinfos) {
+	
+	vector<string> melody = musicToVec(m);
+
+	//조건에 부합하는 후보 <제목, 재생된 시간>
+	vector<pair<string, int>> candidate;
+
+	for (int i = 0; i < musicinfos.size(); ++i) {
+		string startTime = "";
+		string endTime = "";
+		string name = "";
+		string music = "";
+
+		//musicinfos 파싱
+		string buffer = "";
+		for (int j = 0; j < musicinfos[i].size(); ++j) {
+			char ch = musicinfos[i][j];
+			if (ch == ',') {
+				if (startTime == "") startTime = buffer;
+				else if (endTime == "") endTime = buffer;
+				else if (name == "") name = buffer;
+
+				buffer = "";
+			}
+			else buffer += ch;
 		}
+		music = buffer;
+
+		//조건에 부합하는지 확인
+		int playedTime = getPlayedTime(startTime, endTime);
+		vector<string> playedMusic = getPlayedMusic(musicToVec(music), playedTime);
+
+		if (melodyInPlayedMusic(playedMusic, melody)) 
+			candidate.push_back({ name, playedTime });
 	}
 
-	string answer = "";
-	int seats = m;
-	for (int i = 0; i < lastCrew.size(); ++i) {
-		if (seats - lastCrew[i].second > 1) {
-			seats -= lastCrew[i].second;
-		}
-		else {
-			answer = llTotime(lastCrew[i].first - 1);
-			return answer;
-		}
-	}
+	//조건에 부합하는 후보 없는 경우 (None)
+	if (candidate.empty()) return "(None)";
 
-	answer = llTotime(busTime);
+	//조건에 부합하는 후보 중 가장 길이가 긴 노래 선택
+	stable_sort(candidate.begin(), candidate.end(), cmp);
+	string answer = candidate[0].first;
+
 	return answer;
+}
 
+int main() {
+	solution("ABC", { "12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF" });
+	return 0;
 }
