11d4847fe64f0884401d997f0edb081501873cfa
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-13 21:53:48 +0900

    프로그래머스 방금그곡
    채점 결과
    정확성: 96.7
    합계: 96.7 / 100.0
    
    8번 테케 하나만 통과 못 함

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,55 +1,53 @@
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
-//string으로 주어진 음악 -> 각 음 별로 나누어 벡터에 저장
-vector<string> musicToVec(string music) {
-	
-	vector<string> res;
-	for (int i = 0; i < music.length(); ++i) {
-		char note = music[i];
+map<string, int> notes;
 
-		if (note != '#') {
-			res.push_back(to_string(note));
+//string으로 주어진 음악 -> 각 음 별로 나누어 int벡터에 저장
+vector<int> stringToVec(string music) {
+	vector<int> res;
+
+	for (int i = 0; i < music.length(); ++i) {
+
+		if(i + 1 < music.length() && music[i+1] == '#'){
+			res.push_back(notes[music.substr(i, 2)]);
+			++i;
 		}
 		else {
-			string prevNote = res.back();
-			res.pop_back();
-
-			res.push_back(prevNote + to_string(note));
+			res.push_back(notes[music.substr(i, 1)]);
 		}
 	}
 	return res;
 }
 
 //재생된 시간동안 재생된 음악 반환
-vector<string> getPlayedMusic(vector<string> music, int playedLen) {
+vector<int> getPlayedMusic(vector<int> music, int playedLen) {
 	if (playedLen == music.size()) return music;
 	
-	vector<string> res;
+	vector<int> res;
 	for (int i = 0; i < playedLen; ++i) {
 		res.push_back(music[i % music.size()]);
 	}
 	return res;
 }
 
+
 //재생된 음악에 들은 멜로디가 있는지 검사
-bool melodyInPlayedMusic(vector<string> music, vector<string> melody) {
+bool melodyInPlayedMusic(vector<int> music, vector<int> melody) {
 	
 	if (melody.size() > music.size()) return false;
+	if (melody.size() == music.size()) return equal(melody.begin(), melody.end(), music.begin());
 
 	bool in = false;
 	for (int i = 0; i < music.size() - melody.size(); ++i) {
-		bool flag = true;
-		for (int j = 0; j < melody.size(); ++j) {
-			if (music[i + j] != melody[j]) {
-				flag = false;
-				break;
-			}
+		if (equal(melody.begin(), melody.end(), music.begin() + i)) {
+			in = true;
+			break;
 		}
-		if (flag) in = true;
 	}
 	return in;
 }
@@ -63,22 +61,28 @@ int getPlayedTime(string startTime, string endTime) {
 	return end - start;
 }
 
-bool cmp(pair<string, int> a, pair<string, int> b) {
-	return a.second > b.second;
-}
-
 string solution(string m, vector<string> musicinfos) {
 	
-	vector<string> melody = musicToVec(m);
+	//12개의 음 int로 매핑
+	notes["C"] = 1; notes["C#"] = 2;
+	notes["D"] = 3; notes["D#"] = 4;
+	notes["E"] = 5;
+	notes["F"] = 6; notes["F#"] = 7;
+	notes["G"] = 8; notes["G#"] = 9;
+	notes["A"] = 10; notes["A#"] = 11;
+	notes["B"] = 12;
 
-	//조건에 부합하는 후보 <제목, 재생된 시간>
-	vector<pair<string, int>> candidate;
+	vector<int> melody = stringToVec(m);
+
+	//조건에 부합하는 후보 저장 <-재생된 시간, string>
+	//재생 시간 내림차순으로 정렬하기 위해 -를 붙여 저장
+	vector<pair<int, string>> candidate;
 
 	for (int i = 0; i < musicinfos.size(); ++i) {
 		string startTime = "";
 		string endTime = "";
 		string name = "";
-		string music = "";
+		vector<int> music;
 
 		//musicinfos 파싱
 		string buffer = "";
@@ -88,32 +92,31 @@ string solution(string m, vector<string> musicinfos) {
 				if (startTime == "") startTime = buffer;
 				else if (endTime == "") endTime = buffer;
 				else if (name == "") name = buffer;
-
 				buffer = "";
 			}
 			else buffer += ch;
 		}
-		music = buffer;
+		music = stringToVec(buffer);
 
 		//조건에 부합하는지 확인
 		int playedTime = getPlayedTime(startTime, endTime);
-		vector<string> playedMusic = getPlayedMusic(musicToVec(music), playedTime);
+		vector<int> playedMusic = getPlayedMusic(music, playedTime);
 
 		if (melodyInPlayedMusic(playedMusic, melody)) 
-			candidate.push_back({ name, playedTime });
+			candidate.push_back({ -playedTime, name });
 	}
 
 	//조건에 부합하는 후보 없는 경우 (None)
 	if (candidate.empty()) return "(None)";
 
 	//조건에 부합하는 후보 중 가장 길이가 긴 노래 선택
-	stable_sort(candidate.begin(), candidate.end(), cmp);
-	string answer = candidate[0].first;
+	stable_sort(candidate.begin(), candidate.end());
+	string answer = candidate[0].second;
 
 	return answer;
 }
 
 int main() {
-	solution("ABC", { "12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF" });
+	solution("A#", {"12:00,12:01,HELLO,A#"});
 	return 0;
 }
