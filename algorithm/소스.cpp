9746247b6ca0e90227be0c0fe53faac9ed67f56c
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-16 19:46:16 +0900

    백준 14425 문자열 집합

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -8,11 +8,11 @@ using namespace std;
 // C++ implementation of search and insert
 // operations on Trie
 
-const int NUMBER_SIZE = 10; //0 ~ 9
+const int ALPHABET_SIZE = 26; 
 
 // trie node
 struct TrieNode{
-	struct TrieNode *children[NUMBER_SIZE];
+	struct TrieNode *children[ALPHABET_SIZE];
 
 	// isEndOfWord is true if the node represents
 	// end of a word
@@ -25,7 +25,7 @@ struct TrieNode *getNode(void){
 
 	pNode->isEndOfWord = false;
 
-	for (int i = 0; i < NUMBER_SIZE; i++)
+	for (int i = 0; i < ALPHABET_SIZE; i++)
 		pNode->children[i] = NULL;
 
 	return pNode;
@@ -39,7 +39,7 @@ void insert(struct TrieNode *root, string key){
 
 	for (int i = 0; i < key.length(); i++)
 	{
-		int index = key[i] - '0';
+		int index = key[i] - 'a';
 		if (!pCrawl->children[index])
 			pCrawl->children[index] = getNode();
 
@@ -50,22 +50,20 @@ void insert(struct TrieNode *root, string key){
 	pCrawl->isEndOfWord = true;
 }
 
-//트리에 key가 존재하는 경우 1
-//트리에 key가 다른 단어의 접두사로 존재하는 경우 0
-//트리에 key가 존재하지 않는 경우 -1
-int search(struct TrieNode *root, string key){
+
+bool search(struct TrieNode *root, string key){
 	struct TrieNode *pCrawl = root;
 
 	for (int i = 0; i < key.length(); i++)
 	{
-		int index = key[i] - '0';
+		int index = key[i] - 'a';
 		if (!pCrawl->children[index])
-			return -1;
+			return false;
 
 		pCrawl = pCrawl->children[index];
 	}
 
-	return (pCrawl->isEndOfWord);
+	return pCrawl->isEndOfWord;
 }
 
 bool cmp(string a, string b) {
@@ -77,34 +75,27 @@ int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int t;
-	cin >> t;
-	while (t--) {
-		int n;
-		cin >> n;
+	int n, m;
+	cin >> n >> m;
 
-		vector<string> keys;
-		for (int i = 0; i < n; ++i) {
-			string input;
-			cin >> input;
-			keys.push_back(input);
-		}
-		//keys: string 길이 내림차순으로 정렬
-		sort(keys.begin(), keys.end(), cmp);
+	struct TrieNode *root = getNode();
 
-		struct TrieNode *root = getNode();
+	for (int i = 0; i < n; ++i) {
+		string input;
+		cin >> input;
 
-		// Construct trie
-		bool flag = true;
-		for (int i = 0; i < n; i++) {
-			if (search(root, keys[i]) != -1) {
-				cout << "NO\n";
-				flag = false;
-				break;
-			}
-			insert(root, keys[i]);
-		}
-		if (flag) cout << "YES\n";
+		insert(root, input);
 	}
+
+	int cnt = 0;
+	for (int i = 0; i < m; ++i) {
+		string input;
+		cin >> input;
+
+		if (search(root, input)) ++cnt;
+	}
+
+	cout << cnt;
+
 	return 0;
 }
