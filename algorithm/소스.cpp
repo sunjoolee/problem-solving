1380b5860f3eb6e786543f5d3f61ba11beec1f25
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-16 19:34:12 +0900

    백준 5020 전화번호 목록

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -6,11 +8,11 @@ using namespace std;
 // C++ implementation of search and insert
 // operations on Trie
 
-const int ALPHABET_SIZE = 26;
+const int NUMBER_SIZE = 10; //0 ~ 9
 
 // trie node
 struct TrieNode{
-	struct TrieNode *children[ALPHABET_SIZE];
+	struct TrieNode *children[NUMBER_SIZE];
 
 	// isEndOfWord is true if the node represents
 	// end of a word
@@ -23,7 +25,7 @@ struct TrieNode *getNode(void){
 
 	pNode->isEndOfWord = false;
 
-	for (int i = 0; i < ALPHABET_SIZE; i++)
+	for (int i = 0; i < NUMBER_SIZE; i++)
 		pNode->children[i] = NULL;
 
 	return pNode;
@@ -37,7 +39,7 @@ void insert(struct TrieNode *root, string key){
 
 	for (int i = 0; i < key.length(); i++)
 	{
-		int index = key[i] - 'a';
+		int index = key[i] - '0';
 		if (!pCrawl->children[index])
 			pCrawl->children[index] = getNode();
 
@@ -48,16 +50,17 @@ void insert(struct TrieNode *root, string key){
 	pCrawl->isEndOfWord = true;
 }
 
-// Returns true if key presents in trie, else
-// false
-bool search(struct TrieNode *root, string key){
+//트리에 key가 존재하는 경우 1
+//트리에 key가 다른 단어의 접두사로 존재하는 경우 0
+//트리에 key가 존재하지 않는 경우 -1
+int search(struct TrieNode *root, string key){
 	struct TrieNode *pCrawl = root;
 
 	for (int i = 0; i < key.length(); i++)
 	{
-		int index = key[i] - 'a';
+		int index = key[i] - '0';
 		if (!pCrawl->children[index])
-			return false;
+			return -1;
 
 		pCrawl = pCrawl->children[index];
 	}
@@ -65,21 +68,43 @@ bool search(struct TrieNode *root, string key){
 	return (pCrawl->isEndOfWord);
 }
 
+bool cmp(string a, string b) {
+	return a.length() > b.length();
+}
+
 // Driver
 int main(){
-	// Input keys (use only 'a' through 'z'
-	// and lower case)
-	string keys[] = { "the", "a", "there", "answer", "any", "by", "bye", "their" };
-	int n = sizeof(keys) / sizeof(keys[0]);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-	struct TrieNode *root = getNode();
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
 
-	// Construct trie
-	for (int i = 0; i < n; i++) 
-		insert(root, keys[i]);
+		vector<string> keys;
+		for (int i = 0; i < n; ++i) {
+			string input;
+			cin >> input;
+			keys.push_back(input);
+		}
+		//keys: string 길이 내림차순으로 정렬
+		sort(keys.begin(), keys.end(), cmp);
 
-	// Search for different keys
-	search(root, "the") ? cout << "Yes\n" : cout << "No\n";
-	search(root, "these") ? cout << "Yes\n" : cout << "No\n";
+		struct TrieNode *root = getNode();
+
+		// Construct trie
+		bool flag = true;
+		for (int i = 0; i < n; i++) {
+			if (search(root, keys[i]) != -1) {
+				cout << "NO\n";
+				flag = false;
+				break;
+			}
+			insert(root, keys[i]);
+		}
+		if (flag) cout << "YES\n";
+	}
 	return 0;
 }
