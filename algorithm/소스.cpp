38e3380803e779503998fdfd8d332b74f623f263
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-16 19:05:41 +0900

    트라이(Trie) C++ 구현
    출처
    https://www.geeksforgeeks.org/trie-insert-and-search/

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,120 +1,94 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <map>
-#include <memory.h>
 #include <algorithm>
+
 using namespace std;
 
-/*
-struct Trie {
-	bool finish; // 끝나는 지점을 표시해줌 
-	Trie* next[26]; // 26가지 알파벳에 대한 트라이 
-					
-	// 생성자 
-	Trie() : finish(false) { 
-		memset(next, 0, sizeof(next)); 
-	} 
+// C++ implementation of search and insert
+// operations on Trie
 
-	// 소멸자 
-	~Trie() { 
-		for (int i = 0; i < 26; i++) 
-			if (next[i]) delete next[i]; 
-	} 
-	
-	// 트라이에 문자열 삽입 
-	void insert(const char* key) {
+const int ALPHABET_SIZE = 26;
 
-		// 문자열이 끝나는 지점일 경우 표시 
-		if (*key == '\0') finish = true; 
+// trie node
+struct TrieNode
+{
+	struct TrieNode *children[ALPHABET_SIZE];
 
-		else {
-			int curr = *key - 'A';
-			if (next[curr] == NULL) {
-				// 탐색이 처음되는 지점일 경우 동적할당 
-				next[curr] = new Trie();
-			}
-			// 다음 문자 삽입 
-			next[curr]->insert(key + 1); 
-		} 
-	}
-	
-	// 트라이에서 문자열 찾기 
-	Trie* find(const char* key) { 
-
-		// 문자열이 끝나는 위치를 반환 
-		if (*key == '\0') return this;
-
-		int curr = *key - 'A'; 
-		
-		// 찾는 값이 존재하지 않음 
-		if (next[curr] == NULL) return NULL; 
-
-		// 다음 문자를 탐색
-		return next[curr]->find(key + 1); 
-		
-	} 
+	// isEndOfWord is true if the node represents
+	// end of a word
+	bool isEndOfWord;
 };
 
-*/
+// Returns new trie node (initialized to NULLs)
+struct TrieNode *getNode(void)
+{
+	struct TrieNode *pNode = new TrieNode;
 
-struct TrieNode {
-	bool isEnd;
-	map<char, TrieNode*> children;
+	pNode->isEndOfWord = false;
 
-	TrieNode() : isEnd(false) {}
-};
+	for (int i = 0; i < ALPHABET_SIZE; i++)
+		pNode->children[i] = NULL;
 
-//Trie 자료구조에 단어 추가
-void recursiveAdd(TrieNode* curNode, string word) {
-	if (word.length() == 0) {
-		curNode->isEnd = true;
-	}
-	else {
-		int ch = word[0] - 'a';
-		if (curNode->children.find(ch) == curNode->children.end()) {
-			curNode->children[ch] = &TrieNode();
-		}
-		recursiveAdd(curNode->children[ch], word.substr(1));
-	}
+	return pNode;
 }
 
-void add(TrieNode* root, string word) {
-	if (word.length() == 0) return;
-	recursiveAdd(root, word);
+// If not present, inserts key into trie
+// If the key is prefix of trie node, just
+// marks leaf node
+void insert(struct TrieNode *root, string key)
+{
+	struct TrieNode *pCrawl = root;
+
+	for (int i = 0; i < key.length(); i++)
+	{
+		int index = key[i] - 'a';
+		if (!pCrawl->children[index])
+			pCrawl->children[index] = getNode();
+
+		pCrawl = pCrawl->children[index];
+	}
+
+	// mark last node as leaf
+	pCrawl->isEndOfWord = true;
 }
 
-//Trie 자료구조에서 단어 검색
-bool recursiveSearch(TrieNode* curNode, string word) {
-	if (word.length() == 0) {
-		return curNode->isEnd;
-	}
-	else {
-		char ch = word[0];
-		if (curNode->children.find(ch) == curNode->children.end()){
+// Returns true if key presents in trie, else
+// false
+bool search(struct TrieNode *root, string key)
+{
+	struct TrieNode *pCrawl = root;
+
+	for (int i = 0; i < key.length(); i++)
+	{
+		int index = key[i] - 'a';
+		if (!pCrawl->children[index])
 			return false;
-		}
-		return recursiveSearch(curNode->children[ch], word.substr(1));
+
+		pCrawl = pCrawl->children[index];
 	}
+
+	return (pCrawl->isEndOfWord);
 }
 
-bool search(TrieNode* root, string word) {
-	if (word.length() == 0) return true;
-	return recursiveSearch(root, word);
-}
+// Driver
+int main()
+{
+	// Input keys (use only 'a' through 'z'
+	// and lower case)
+	string keys[] = { "the", "a", "there",
+					"answer", "any", "by",
+					 "bye", "their" };
+	int n = sizeof(keys) / sizeof(keys[0]);
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+	struct TrieNode *root = getNode();
 
-	TrieNode root;
+	// Construct trie
+	for (int i = 0; i < n; i++)
+		insert(root, keys[i]);
 
-	add(&root, "baby");
-	add(&root, "boy");
-
-	cout << search(&root, "baby");
-	cout << search(&root, "boo");
-
+	// Search for different keys
+	search(root, "the") ? cout << "Yes\n" :
+		cout << "No\n";
+	search(root, "these") ? cout << "Yes\n" :
+		cout << "No\n";
 	return 0;
 }
-
