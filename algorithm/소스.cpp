a6d84457726ac1ebac53615077d6b25ceadbc976
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-17 20:58:16 +0900

    프로그래머스 오픈채팅방

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,105 +1,61 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-// C++ implementation of search and insert
-// operations on Trie
+vector<string> solution(vector<string> record) {
 
-const int ALPHABET_SIZE = 26; 
+	//<uID, name>
+	map<string, string> m;
+	//<Enter 0/Leave 1, uID> 
+	queue<pair<int, string>> q;
 
-// trie node
-struct TrieNode{
-	struct TrieNode *children[ALPHABET_SIZE];
+	for (int i = 0; i < record.size(); ++i) {
+		vector<string> rec;
 
-	// isEndOfWord is true if the node represents
-	// end of a word
-	bool isEndOfWord;
-};
+		string buffer = "";
+		for (int j = 0; j < record[i].length(); ++j) {
+			if (record[i][j] == ' ') {
+				rec.push_back(buffer);
+				buffer = "";
+			}
+			else buffer += record[i][j];
+		}
+		rec.push_back(buffer);
 
-// Returns new trie node (initialized to NULLs)
-struct TrieNode *getNode(void){
-	struct TrieNode *pNode = new TrieNode;
-
-	pNode->isEndOfWord = false;
-
-	for (int i = 0; i < ALPHABET_SIZE; i++)
-		pNode->children[i] = NULL;
-
-	return pNode;
-}
-
-// If not present, inserts key into trie
-// If the key is prefix of trie node, just
-// marks leaf node
-void insert(struct TrieNode *root, string key){
-	struct TrieNode *pCrawl = root;
-
-	for (int i = 0; i < key.length(); i++)
-	{
-		int index = key[i] - 'a';
-		if (!pCrawl->children[index])
-			pCrawl->children[index] = getNode();
-
-		pCrawl = pCrawl->children[index];
+		if (rec[0] == "Enter") {
+			m[rec[1]] = rec[2];
+			q.push({ 0, rec[1] });
+		}
+		if (rec[0] == "Leave") {
+			q.push({ 1, rec[1] });
+		}
+		if (rec[0] == "Change") {
+			m[rec[1]] = rec[2];
+		};
 	}
 
-	// mark last node as leaf
-	pCrawl->isEndOfWord = true;
+	vector<string> answer;
+	while (!q.empty()) {
+		int flag = q.front().first;
+		string uId = q.front().second;
+		q.pop();
+
+		if (flag == 1) {
+			string ans = m[uId] + "님이 들어왔습니다.";
+			answer.push_back(ans);
+		}
+		else {
+			string ans = m[uId] + "님이 나갔습니다.";
+			answer.push_back(ans);
+		}
+	}
+	return answer;
 }
 
-//트리에 key가 존재하는 경우 1
-//트리에 key가 다른 단어의 접두사로 존재하는 경우 0
-//트리에 key가 존재하지 않는 경우 -1
-int search(struct TrieNode *root, string key){
-	struct TrieNode *pCrawl = root;
-
-	for (int i = 0; i < key.length(); i++)
-	{
-		int index = key[i] - 'a';
-		if (!pCrawl->children[index])
-			return -1;
-
-		pCrawl = pCrawl->children[index];
-	}
-
-	return pCrawl->isEndOfWord;
-}
-
-bool cmp(string a, string b) {
-	return a.length() > b.length();
-}
-
-// Driver
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-
-	int n, m;
-	cin >> n >> m;
-
-	struct TrieNode *root = getNode();
-
-	for (int i = 0; i < n; ++i) {
-		string input;
-		cin >> input;
-
-		insert(root, input);
-	}
-	
-
-	//포함되어 있는 문자열 중 적어도 하나의 접두사의 수
-	int cnt = 0;
-	for (int i = 0; i < m; ++i) {
-		string input;
-		cin >> input;
-
-		if (search(root, input) != -1) ++cnt;
-	}
-
-	cout << cnt;
-
-	return 0;
+int main() {
+	solution({ "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan" });
 }
