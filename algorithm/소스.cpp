7275b8a4cf08d26debdb0cdd9ae36d3b631cba22
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-17 22:50:53 +0900

    프로그래머스 실패율

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,124 +1,42 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
-
 using namespace std;
 
-const int MAXVAL = 987654321;
-
-map<int, pair<int, int>> node;
-vector<int> traverseRes;
-
-struct TreeNode{
-	int id;
-	TreeNode* left;
-	TreeNode* right;
-};
-
-TreeNode* newNode(int id) {
-	TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
-	temp->id = id;
-	temp->left = temp->right = NULL;
-	return temp;
+bool cmp(pair<int, double> a, pair<int, double> b) {
+	if (a.second > b.second) return true;
+	if (a.second == b.second) return a.first < b.first;
+	return false;
 }
 
-void makeTree(TreeNode* node, vector<int> leftCand, vector<int> rightCand){
-	if (leftCand.empty() && rightCand.empty()) return;
+vector<int> solution(int N, vector<int> stages) {
+	
+	vector<pair<int, double>> failRate;
 
-	vector<int> leftCand2;
-	vector<int> rightCand2;
-	//왼쪽 자식 찾기
-	if (!leftCand.empty()) {
-		sort(leftCand.begin(), leftCand.end(), cmpID);
-		node->left = newNode(leftCand[0]);
+	for (int stage = 1; stage < N + 1; ++stage) {
+		double  sido= 0;
+		double  silpae= 0;
 
-		leftCand2.clear();
-		rightCand2.clear();
-		for (int i = 0; i < leftCand.size(); ++i) {
-			if (globalNodeInfo[leftCand[i]].first < globalNodeInfo[leftCand[0]].first) leftCand2.push_back(i);
-			if (globalNodeInfo[leftCand[i]].first > globalNodeInfo[leftCand[0]].first)  rightCand2.push_back(i);
+		for (int i = 0; i < stages.size(); ++i){ 
+			if (stages[i] >= stage) sido++;
+			if (stages[i] == stage) silpae++;
 		}
-		makeTree(node->left, leftCand2, rightCand2);
+		
+		if (sido == 0 || silpae == 0) 
+			failRate.push_back({ stage, 0 });
+		else 
+			failRate.push_back({stage, silpae / sido});
 	}
 
-	//오른쪽 자식 찾기
-	if (!rightCand.empty()) {
-		sort(rightCand.begin(), rightCand.end(), cmpID);
-		node->right = newNode(rightCand[0]);
-
-		leftCand2.clear();
-		rightCand2.clear();
-		for (int i = 0; i < rightCand.size(); ++i) {
-			if (globalNodeInfo[rightCand[i]].first < globalNodeInfo[rightCand[0]].first) leftCand2.push_back(i);
-			if (globalNodeInfo[rightCand[i]].first > globalNodeInfo[rightCand[0]].first) rightCand2.push_back(i);
-		}
-		makeTree(node->right, leftCand2, rightCand2);
-	}
-	return;
-}
-
-
-//전위 순회: 뿌리 > 왼쪽자식 > 오른쪽 자식
-void traverse1(TreeNode* curNode) {
-	traverseRes.push_back(curNode->id + 1);
-
-	if (curNode->left != NULL) {
-		traverse1(curNode->left);
-	}
-	if (curNode->right != NULL) {
-		traverse1(curNode->right);
-	}
-}
-
-//후위 순회: 왼쪽자식 > 오른쪽 자식 > 뿌리
-void traverse2(TreeNode* curNode) {
-
-	if (curNode->left != NULL) {
-		traverse2(curNode->left);
-	}
-	if (curNode->right != NULL) {
-		traverse2(curNode->right);
-	}
-
-	traverseRes.push_back(curNode->id + 1);
-}
-
-vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
-
-	//전역변수 초기화
-	//y좌표가 가장 작은 노드 = 루트 노드 
-	int rootId;
-	int minY = 
-
-	for (int i = 0; i < nodeinfo.size(); ++i) {
-		node[i] = { nodeinfo[i][0], nodeinfo[i][1]};
-	}
+	sort(failRate.begin(), failRate.end(), cmp);
 	
-	
-
-
-	TreeNode* root = newNode(0);
-	vector<int> leftCand;
-	vector<int> rightCand;
-	for (int i = 0; i < globalNodeInfo.size(); ++i) {
-		if (globalNodeInfo[i].first < globalNodeInfo[root->id].first) leftCand.push_back(i);
-		if (globalNodeInfo[i].first > globalNodeInfo[root->id].first) rightCand.push_back(i);
+	vector<int> answer;
+	for (int i = 0; i < failRate.size(); ++i){
+		answer.push_back(failRate[i].first);
 	}
-
-	makeTree(root, leftCand, rightCand);
-
-
-	//순회 결과 저장
-	vector<vector<int>> answer;
-
-	traverseRes.clear();
-	traverse1(root);
-	answer.push_back(traverseRes);
-	
-	traverseRes.clear();
-	traverse2(root);
-	answer.push_back(traverseRes);
-
 	return answer;
+}
+
+int main() {
+	solution(4, { 4, 4, 4, 4, 4 });
 }
