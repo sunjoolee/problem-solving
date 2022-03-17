faab178f15f4ed96e8468544fd3d80e766ed52e2
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-17 22:29:46 +0900

    프로그래머스 길 찾기 게임
    ing

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,61 +1,124 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-vector<string> solution(vector<string> record) {
+const int MAXVAL = 987654321;
 
-	//<uID, name>
-	map<string, string> m;
-	//<Enter 0/Leave 1, uID> 
-	queue<pair<int, string>> q;
+map<int, pair<int, int>> node;
+vector<int> traverseRes;
 
-	for (int i = 0; i < record.size(); ++i) {
-		vector<string> rec;
+struct TreeNode{
+	int id;
+	TreeNode* left;
+	TreeNode* right;
+};
 
-		string buffer = "";
-		for (int j = 0; j < record[i].length(); ++j) {
-			if (record[i][j] == ' ') {
-				rec.push_back(buffer);
-				buffer = "";
-			}
-			else buffer += record[i][j];
-		}
-		rec.push_back(buffer);
-
-		if (rec[0] == "Enter") {
-			m[rec[1]] = rec[2];
-			q.push({ 0, rec[1] });
-		}
-		if (rec[0] == "Leave") {
-			q.push({ 1, rec[1] });
-		}
-		if (rec[0] == "Change") {
-			m[rec[1]] = rec[2];
-		};
-	}
-
-	vector<string> answer;
-	while (!q.empty()) {
-		int flag = q.front().first;
-		string uId = q.front().second;
-		q.pop();
-
-		if (flag == 1) {
-			string ans = m[uId] + "님이 들어왔습니다.";
-			answer.push_back(ans);
-		}
-		else {
-			string ans = m[uId] + "님이 나갔습니다.";
-			answer.push_back(ans);
-		}
-	}
-	return answer;
+TreeNode* newNode(int id) {
+	TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
+	temp->id = id;
+	temp->left = temp->right = NULL;
+	return temp;
 }
 
-int main() {
-	solution({ "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan" });
+void makeTree(TreeNode* node, vector<int> leftCand, vector<int> rightCand){
+	if (leftCand.empty() && rightCand.empty()) return;
+
+	vector<int> leftCand2;
+	vector<int> rightCand2;
+	//왼쪽 자식 찾기
+	if (!leftCand.empty()) {
+		sort(leftCand.begin(), leftCand.end(), cmpID);
+		node->left = newNode(leftCand[0]);
+
+		leftCand2.clear();
+		rightCand2.clear();
+		for (int i = 0; i < leftCand.size(); ++i) {
+			if (globalNodeInfo[leftCand[i]].first < globalNodeInfo[leftCand[0]].first) leftCand2.push_back(i);
+			if (globalNodeInfo[leftCand[i]].first > globalNodeInfo[leftCand[0]].first)  rightCand2.push_back(i);
+		}
+		makeTree(node->left, leftCand2, rightCand2);
+	}
+
+	//오른쪽 자식 찾기
+	if (!rightCand.empty()) {
+		sort(rightCand.begin(), rightCand.end(), cmpID);
+		node->right = newNode(rightCand[0]);
+
+		leftCand2.clear();
+		rightCand2.clear();
+		for (int i = 0; i < rightCand.size(); ++i) {
+			if (globalNodeInfo[rightCand[i]].first < globalNodeInfo[rightCand[0]].first) leftCand2.push_back(i);
+			if (globalNodeInfo[rightCand[i]].first > globalNodeInfo[rightCand[0]].first) rightCand2.push_back(i);
+		}
+		makeTree(node->right, leftCand2, rightCand2);
+	}
+	return;
+}
+
+
+//전위 순회: 뿌리 > 왼쪽자식 > 오른쪽 자식
+void traverse1(TreeNode* curNode) {
+	traverseRes.push_back(curNode->id + 1);
+
+	if (curNode->left != NULL) {
+		traverse1(curNode->left);
+	}
+	if (curNode->right != NULL) {
+		traverse1(curNode->right);
+	}
+}
+
+//후위 순회: 왼쪽자식 > 오른쪽 자식 > 뿌리
+void traverse2(TreeNode* curNode) {
+
+	if (curNode->left != NULL) {
+		traverse2(curNode->left);
+	}
+	if (curNode->right != NULL) {
+		traverse2(curNode->right);
+	}
+
+	traverseRes.push_back(curNode->id + 1);
+}
+
+vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
+
+	//전역변수 초기화
+	//y좌표가 가장 작은 노드 = 루트 노드 
+	int rootId;
+	int minY = 
+
+	for (int i = 0; i < nodeinfo.size(); ++i) {
+		node[i] = { nodeinfo[i][0], nodeinfo[i][1]};
+	}
+	
+	
+
+
+	TreeNode* root = newNode(0);
+	vector<int> leftCand;
+	vector<int> rightCand;
+	for (int i = 0; i < globalNodeInfo.size(); ++i) {
+		if (globalNodeInfo[i].first < globalNodeInfo[root->id].first) leftCand.push_back(i);
+		if (globalNodeInfo[i].first > globalNodeInfo[root->id].first) rightCand.push_back(i);
+	}
+
+	makeTree(root, leftCand, rightCand);
+
+
+	//순회 결과 저장
+	vector<vector<int>> answer;
+
+	traverseRes.clear();
+	traverse1(root);
+	answer.push_back(traverseRes);
+	
+	traverseRes.clear();
+	traverse2(root);
+	answer.push_back(traverseRes);
+
+	return answer;
 }
