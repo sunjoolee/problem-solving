ce4da24ed2d9b6f6c1bb9b353261114385e67421
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-21 16:26:58 +0900

    프로그래머스 후보키

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,42 +1,72 @@
 #include <string>
 #include <vector>
+#include <set>
 #include <algorithm>
+
 using namespace std;
 
-bool cmp(pair<int, double> a, pair<int, double> b) {
-	if (a.second > b.second) return true;
-	if (a.second == b.second) return a.first < b.first;
-	return false;
+typedef long long ll;
+
+int bitCount(int x) {
+	if (x == 0) return 0;
+	return (x % 2) + bitCount(x / 2);
 }
 
-vector<int> solution(int N, vector<int> stages) {
-	
-	vector<pair<int, double>> failRate;
+int solution(vector<vector<string>> relation) {
+	int answer = 0;
 
-	for (int stage = 1; stage < N + 1; ++stage) {
-		double  sido= 0;
-		double  silpae= 0;
+	//찾은 후보키 속성 집합들
+	vector<ll> candidateKey;
 
-		for (int i = 0; i < stages.size(); ++i){ 
-			if (stages[i] >= stage) sido++;
-			if (stages[i] == stage) silpae++;
+	//후보키 속성 집합의 크기 작은 것부터 찾기
+	for (int size = 1; size <= relation[0].size(); ++size) {
+
+		//속성의 모든 부분 집합
+		for (ll subset = 1LL; subset <= (1 << relation[0].size()) - 1; subset++) {
+			if (bitCount(subset) != size) continue;
+
+			//1. 최소성 검사
+			//이미 찾은 후보키 속석의 집합 포함하고 있다면 continue
+			bool shouldContinue = false;
+			for (int i = 0; i < candidateKey.size(); ++i) {
+				ll intersection = candidateKey[i] & subset;
+				if (intersection == candidateKey[i]) {
+					shouldContinue = true;
+					break;
+				}
+			}
+			if (shouldContinue) continue;
+
+			//2. 유일성 검사
+			bool unique = true;
+			set<string> relationString;
+
+			for (int i = 0; i < relation.size(); ++i) {
+				string str = "";
+				for (int key = 0; key < relation[0].size(); ++key) {
+					if (subset & (1 << key)) str += relation[i][key];
+				}
+				if (relationString.find(str) != relationString.end()) {
+					unique = false;
+					break;
+				}
+				relationString.insert(str);
+			}
+			if (!unique) continue;
+
+			candidateKey.push_back(subset);
+			answer++;
 		}
-		
-		if (sido == 0 || silpae == 0) 
-			failRate.push_back({ stage, 0 });
-		else 
-			failRate.push_back({stage, silpae / sido});
 	}
 
-	sort(failRate.begin(), failRate.end(), cmp);
-	
-	vector<int> answer;
-	for (int i = 0; i < failRate.size(); ++i){
-		answer.push_back(failRate[i].first);
-	}
 	return answer;
 }
 
 int main() {
-	solution(4, { 4, 4, 4, 4, 4 });
+	solution({ {"100", "ryan", "music", "2"},
+		{"200", "apeach", "math", "2"},
+		{"300", "tube", "computer", "3"},
+		{"400", "con", "computer", "4"},
+		{"500", "muzi", "music", "3"},
+		{"600", "apeach", "music", "2"} });
 }
