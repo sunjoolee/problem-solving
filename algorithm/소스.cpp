c8ec20b8205de8269fe7122b9a8236ecaad308d6
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-28 17:06:14 +0900

    프로그래머스 기둥과 보 ing
    로직을 바꿔봤는데 정답률은 똑같네
    
    채점 결과
    정확성: 23.2
    합계: 23.2 / 100.0

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -11,14 +11,52 @@ const int pillarEnd = 1;
 const int boStart = 2;
 const int boEnd = 3;
 
+//map[x][y][0] = 1 : (x,y)가 기둥의 시작 좌표
+//map[x][y][1] = 1:  (x,y)가 기둥의 끝 좌표
+//map[x][y][2] = 1 : (x,y)가 보의 시작 좌표
+//map[x][y][3] = 1 : (x,y)가 보의 끝 좌표
+vector<vector<vector<int>>> map(N, vector<vector<int>>(N, vector<int>(4, 0)));
+
+//잘 설치되었는지 확인하는 조건은 문제에 나와있대로 하면 됨
+//그러나, 삭제해도 되는 구조물인지 확인하는 조건은 직접 생각해야함 & 경우의 수가 많아 짜기 어려움
+//따라서, 삭제해도 되는지 안되는지 확인하려 하지 말고, 일단 삭제하고 나머지 구조물들이 잘 설치되었는지 확인하자
+
+
+//map에 설치된 모든 구조물들이 잘 설치되었는지 확인
+bool check(int n) {
+
+	for (int x = 0; x <=n ; ++x) {
+		for (int y = 0; y <= n; ++y) {
+
+			//기둥
+			if (map[x][y][pillarStart] == 1) {
+				//바닥 위에 설치되어있는 경우
+				if (x == 0) continue;
+				//보의 한쪽 끝에 설치되어있는 경우
+				else if (map[x][y][boStart] == 1 || map[x][y][boEnd] == 1) continue;
+				//기둥 위에 설치되어있는 경우
+				else if (map[x][y][pillarEnd] == 1) continue;
+
+				//조건 만족 못함 
+				return false;
+			}
+			//보
+			else if(map[x][y][boStart] == 1) {
+				//한쪽 끝이 기둥 위인 경우
+				if (map[x][y][pillarEnd] == 1 || map[x][y + 1][pillarEnd] == 1) continue;
+				//양쪽 끝부분이 다른 보와 동시에 연결된 경우
+				else if (map[x][y][boEnd] == 1 && map[x][y + 1][boStart] == 1) continue;
+
+				//조건 만족 못함
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
 	vector<vector<int>> answer;
-
-	//map[x][y][0] = 1 : (x,y)가 기둥의 시작 좌표
-	//map[x][y][1] = 1:  (x,y)가 기둥의 끝 좌표
-	//map[x][y][2] = 1 : (x,y)가 보의 시작 좌표
-	//map[x][y][3] = 1 : (x,y)가 보의 끝 좌표
-	vector<vector<vector<int>>> map(N, vector<vector<int>>(N, vector<int>(4, 0)));
 
 	for (int i = 0; i < build_frame.size(); ++i) {
 		int y = build_frame[i][0]; 
@@ -30,71 +68,41 @@ vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
 
 		//설치
 		if (b == 1) {
-			//기둥
+			//기둥 설치
 			if (a == 0) {
-				//바닥 위에 설치하는 경우
-				if (x == 0) {
-					map[x][y][pillarStart] = 1;
-					map[x + 1][y][pillarEnd] = 1;
-				}
-				//보의 한쪽 끝에 설치하는 경우
-				else if (map[x][y][boStart] == 1 || map[x][y][boEnd] == 1) {
-					map[x][y][pillarStart] = 1;
-					map[x + 1][y][pillarEnd] = 1;
-				}
-				//기둥 위에 설치하는 경우
-				else if (map[x][y][pillarEnd] == 1) {
-					map[x][y][pillarStart] = 1;
-					map[x + 1][y][pillarEnd] = 1;
-				}
+				map[x][y][pillarStart] = 1;
+				map[x+1][y][pillarEnd] = 1;
+				if (check(n)) continue;
+				map[x][y][pillarStart] = 0;
+				map[x + 1][y][pillarEnd] = 0;
 			}
-			//보
+			//보 설치
 			else {
-				//한쪽 끝이 기둥 위인 경우
-				if (map[x][y][pillarEnd] == 1 || map[x][y + 1][pillarEnd] == 1) {
-					map[x][y][boStart] = 1; 
-					map[x][y + 1][boEnd] = 1;
-				}
-				//양쪽 끝부분이 다른 보와 동시에 연결된 경우
-				else if (map[x][y][boEnd] == 1 && map[x][y + 1][boStart] == 1) {
-					map[x][y][boStart] = 1;
-					map[x][y + 1][boEnd] = 1;
-				}
+				map[x][y][boStart] = 1;
+				map[x][y + 1][boEnd] = 1;
+				if (check(n)) continue;
+				map[x][y][boStart] = 0;
+				map[x][y + 1][boEnd] = 0;
 			}
+			
 		}
 		//삭제
 		else {
 			//삭제할 기둥이 있는 경우
-			if (a == 0 && map[x][y][pillarStart] == 1) {
-				//위에 기둥이 설치되어있는 경우 삭제 안됨
-				if (map[x + 1][y][pillarStart] == 1) continue;
-
-				//기둥을 삭제했을 때 연결되어있었던 보가 있었다면
-				if (map[x + 1][y][boStart] == 1) { //기둥 오른쪽으로 연결된 보
-					//(한쪽 끝이 기둥 위에 있어야 함) 또는 (양쪽 끝이 다른 보와 동시에 연결되어있어야 함)
-					if ((map[x + 1][y + 1][pillarEnd] != 1) && (map[x + 1][y][boEnd] != 1 || map[x + 1][y + 1][boStart] != 1)) continue;
-				}
-				if (map[x + 1][y][boEnd] == 1) { //기둥 왼쪽으로 연결된 보
-					//(한쪽 끝이 기둥 위에 있어야 함) 또는 (양쪽 끝이 다른 보와 동시에 연결되어있어야 함)
-					if ((map[x + 1][y - 1][pillarEnd] != 1) && (map[x + 1][y][boStart] != 1 || map[x + 1][y - 1][boEnd] != 1)) continue;
-				}
-				//기둥 삭제
+			if (a == 0 && map[x][y][pillarStart] == 1){
 				map[x][y][pillarStart] = 0;
 				map[x + 1][y][pillarEnd] = 0;
+				if (check(n)) continue;
+				map[x][y][pillarStart] = 1;
+				map[x + 1][y][pillarEnd] = 1;
 			}
 			//삭제할 보가 있는 경우
 			else if(map[x][y][boStart] == 1) {
-				// 나에게 연결된 보가 양쪽이 보로 연결된 보인 경우 삭제 불가능
-				if (map[x][y + 1][boStart] == 1) { //보 오른쪽으로 연결된 보
-					if (map[x][y + 1][pillarEnd] == 0 && map[x][y + 2][pillarEnd] == 0) continue;
-				}
-				if (map[x][y + 1][boStart] == 1) { //보 왼쪽으로 연결된 보
-					if (map[x][y][pillarEnd] == 0 && map[x][y -1][pillarEnd] == 0) continue;
-				}
-
-				//보 삭제
 				map[x][y][boStart] = 0;
 				map[x][y + 1][boEnd] = 0;
+				if (check(n)) continue;
+				map[x][y][boStart] = 1;
+				map[x][y + 1][boEnd] = 1;
 			}
 		}
 	}
