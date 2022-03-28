8869df3951e0b51af360ea86bc71cb5452b2cca0
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-03-28 16:23:59 +0900

    프로그래머스 기둥과 보 ing
    채점 결과
    정확성: 19.2
    합계: 19.2 / 100.0

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,135 +1,124 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-int N;
+int N = 100;
 
-struct qNode {
-	int cost;
-	//로봇의 위치 좌측상단 좌표로 판단
-	int r;
-	int w;
-	//로봇 가로 방향 0/세로 방향 1
-	int dir;
+const int pillarStart = 0;
+const int pillarEnd = 1;
+const int boStart = 2;
+const int boEnd = 3;
 
-	bool operator<(const qNode node) const {
-		return this->cost < node.cost;
-	}
-};
+vector<vector<int>> solution(int n, vector<vector<int>> build_frame) {
+	vector<vector<int>> answer;
 
-bool inRange(int r, int w) {
-	if (r < 0 || r >= N) return false;
-	if (w < 0 || w >= N) return false;
-	return true;
-}
+	//map[x][y][0] = 1 : (x,y)가 기둥의 시작 좌표
+	//map[x][y][1] = 1:  (x,y)가 기둥의 끝 좌표
+	//map[x][y][2] = 1 : (x,y)가 보의 시작 좌표
+	//map[x][y][3] = 1 : (x,y)가 보의 끝 좌표
+	vector<vector<vector<int>>> map(100, vector<vector<int>>(100, vector<int>(4, 0)));
 
+	for (int i = 0; i < build_frame.size(); ++i) {
+		int y = build_frame[i][0]; 
+		int x = build_frame[i][1];
+		//a = 0 기둥, 1 보
+		int a = build_frame[i][2]; 
+		//b = 0 삭제, 1 설치
+		int b = build_frame[i][3];
 
-int solution(vector<vector<int>> board) {
-	N = board.size();
-	int answer = 987654321;
-
-	//BFS
-	//(0,0),(0,1) 에서 시작
-
-	vector<vector<vector<int>>> visited(N, vector<vector<int>> (N, vector<int>(2, 0)));
-	priority_queue<qNode> q;
-	q.push({ 0, 0, 0, 0 });
-
-	while (!q.empty()) {
-		int curCost = q.top().cost;
-		int curR = q.top().r;
-		int curW = q.top().w;
-		int curDir = q.top().dir;
-		q.pop();
-
-		//지금까지 구한 경로보다 더 긴 경로인 경우 무시
-		if (curCost > answer) continue;
-
-		//종점인지 확인
-		//가로
-		if (curDir == 0){
-			if (curR == N - 1 && curW == N - 2) {
-				answer = min(answer, -curCost);
-				continue;
+		//설치
+		if (b == 1) {
+			//기둥
+			if (a == 0) {
+				//바닥 위에 설치하는 경우
+				if (x == 0) {
+					map[x][y][pillarStart] = 1;
+					map[x + 1][y][pillarEnd] = 1;
+				}
+				//보의 한쪽 끝에 설치하는 경우
+				else if (map[x][y][boStart] == 1 || map[x][y][boEnd] == 1) {
+					map[x][y][pillarStart] = 1;
+					map[x + 1][y][pillarEnd] = 1;
+				}
+				//기둥 위에 설치하는 경우
+				else if (map[x][y][pillarEnd] == 1) {
+					map[x][y][pillarStart] = 1;
+					map[x + 1][y][pillarEnd] = 1;
+				}
+			}
+			//보
+			else {
+				//한쪽 끝이 기둥 위인 경우
+				if (map[x][y][pillarEnd] == 1 || map[x][y + 1][pillarEnd] == 1) {
+					map[x][y][boStart] = 1; 
+					map[x][y + 1][boEnd] = 1;
+				}
+				//양쪽 끝부분이 다른 보와 동시에 연결된 경우
+				else if (map[x][y][boEnd] == 1 && map[x][y + 1][boStart] == 1) {
+					map[x][y][boStart] = 1;
+					map[x][y + 1][boEnd] = 1;
+				}
 			}
 		}
-		//세로
-		else if (curR == N - 2 && curW == N - 1){
-			answer = min(answer, -curCost);
-			continue;
-		}
-
-		//이미 방문한 노드
-		if (visited[curR][curW][curDir] == 1) continue;
-		visited[curR][curW][curDir] = 1;
-
-		//다음 노드 방문
-		//가로
-		if (curDir == 0) {
-			//좌로 이동
-			if (inRange(curR, curW - 1) && !board[curR][curW-1] && !visited[curR][curW - 1][0]){
-				q.push({ curCost -1, curR, curW - 1, 0});
-			}
-			//우로 이동
-			if (inRange(curR, curW + 2) && !board[curR][curW +2] && !visited[curR][curW + 2][0]) {
-				q.push({ curCost - 1, curR, curW + 1, 0});
-			}
-			//위로 회전
-			if (inRange(curR - 1, curW ) && inRange(curR - 1, curW + 1) && !board[curR-1][curW] && !board[curR-1][curW + 1]){
-				if(!visited[curR-1][curW + 1][1]) 
-					q.push({ curCost - 1, curR - 1, curW + 1, 1});
-				if (!visited[curR - 1][curW][1])
-					q.push({ curCost - 1, curR - 1, curW, 1});
-			}
-			//아래로 회전
-			if (inRange(curR + 1, curW) && inRange(curR + 1, curW + 1) && !board[curR + 1][curW] && !board[curR + 1][curW + 1]) {
-				if (!visited[curR + 1][curW + 1][1])
-					q.push({ curCost - 1, curR , curW + 1, 1 });
-				if (!visited[curR + 1][curW][1])
-					q.push({ curCost - 1, curR , curW, 1});
-			}
-		}
-		//세로
+		//삭제
 		else {
-			//위로 이동
-			if (inRange(curR - 1, curW) && !board[curR - 1][curW] &&!visited[curR - 1][curW][1]) {
-				q.push({ curCost - 1, curR - 1, curW, 1});
+			//삭제할 기둥이 있는 경우
+			if (a == 0 && map[x][y][pillarStart] == 1) {
+				//위에 기둥이 설치되어있는 경우 삭제 안됨
+				if (map[x + 1][y][pillarStart] == 1) continue;
+
+				//기둥을 삭제했을 때 연결되어있었던 보가 있었다면
+				if (map[x + 1][y][boStart] == 1) { //기둥 오른쪽으로 연결된 보
+					//(한쪽 끝이 기둥 위에 있어야 함) 또는 (양쪽 끝이 다른 보와 동시에 연결되어있어야 함)
+					if ((map[x + 1][y + 1][pillarEnd] != 1) && (map[x + 1][y][boEnd] != 1 || map[x + 1][y + 1][boStart] != 1)) continue;
+				}
+				if (map[x + 1][y][boEnd] == 1) { //기둥 왼쪽으로 연결된 보
+					//(한쪽 끝이 기둥 위에 있어야 함) 또는 (양쪽 끝이 다른 보와 동시에 연결되어있어야 함)
+					if ((map[x + 1][y - 1][pillarEnd] != 1) && (map[x + 1][y][boStart] != 1 || map[x + 1][y - 1][boEnd] != 1)) continue;
+				}
+				//기둥 삭제
+				map[x][y][pillarStart] = 0;
+				map[x + 1][y][pillarEnd] = 0;
 			}
-			//아래로 이동
-			if (inRange(curR + 2, curW) && !board[curR + 2][curW ] && !visited[curR + 2][curW][1]) {
-				q.push({ curCost - 1, curR + 1, curW, 1});
-			}
-			//좌로 회전
-			if (inRange(curR, curW-1) && inRange(curR + 1, curW -1) && !board[curR][curW - 1] && !board[curR+1][curW - 1]) {
-				if (!visited[curR][curW - 1][0])
-					q.push({ curCost - 1, curR, curW - 1, 0});
-				if (!visited[curR + 1][curW - 1][0])
-					q.push({ curCost - 1, curR + 1, curW - 1, 0});
-			}
-			//우로 회전
-			if (inRange(curR, curW + 1) && inRange(curR + 1, curW + 1)&& !board[curR][curW + 1] && !board[curR+1][curW + 1]) {
-				if (!visited[curR][curW ][0])
-					q.push({ curCost - 1, curR , curW , 0 });
-				if (!visited[curR + 1][curW][0])
-					q.push({ curCost - 1, curR + 1, curW, 0});
+			//삭제할 보가 있는 경우
+			else if(map[x][y][boStart] == 1) {
+				// 나에게 연결된 보가 양쪽이 보로 연결된 보인 경우 삭제 불가능
+				if (map[x][y + 1][boStart] == 1) { //보 오른쪽으로 연결된 보
+					if (map[x][y + 1][pillarEnd] == 0 && map[x][y + 2][pillarEnd] == 0) continue;
+				}
+				if (map[x][y + 1][boStart] == 1) { //보 왼쪽으로 연결된 보
+					if (map[x][y][pillarEnd] == 0 && map[x][y -1][pillarEnd] == 0) continue;
+				}
+
+				//보 삭제
+				map[x][y][boStart] = 0;
+				map[x][y + 1][boEnd] = 0;
 			}
 		}
 	}
 
+	for (int x = 0; x <= n; ++x) {
+		for (int y = 0; y <= n; ++y) {
+			if (map[y][x][pillarStart] == 1) answer.push_back({ x, y, 0 });
+			if (map[y][x][boStart] == 1) answer.push_back({ x,y, 1 });
+		}
+	}
 
 	return answer;
 }
 
 int main() {
-	solution({
-		{0, 0, 0, 0, 0, 0, 1},
-		{1, 1, 1, 1, 0, 0, 1},
-		{0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 1, 1, 1, 1, 0},
-		{0, 1, 1, 1, 1, 1, 0},
-		{0, 0, 0, 0, 0, 1, 1},
-		{0, 0, 1, 0, 0, 0, 0} });
+	solution(5, {
+		{0, 0, 0, 1},
+		{2, 0, 0, 1},
+		{4, 0, 0, 1},
+		{0, 1, 1, 1},
+		{1, 1, 1, 1},
+		{2, 1, 1, 1},
+		{3, 1, 1, 1},
+		{2, 0, 0, 0},
+		{1, 1, 1, 0},
+		{2, 2, 0, 1} });
 }
