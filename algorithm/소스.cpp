6807799e8cf146ffd326e519c47375021e31cd51
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-05-05 12:03:47 +0900

    프로그래머스 광고 삽입 ing

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,173 +1,92 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <map>\
-#include <math.h>
 #include <algorithm>
 
 using namespace std;
 
-const string emptyBoard = "0000000000000000";
-
-int dirR[4] = { 0, 0, 1, -1 };
-int dirC[4] = { 1, -1, 0, 0 };
-
-struct qNode {
-	int r;
-	int c;
-	int moves;
-	string board;
-	pair<int, int> select;
-};
-
-struct mapNode {
-	int r, c;
-	string board;
-
-	bool operator ==(const mapNode &var) const {
-		if (r == var.r && c == var.c && board == var.board) return true;
-		return false;
-	}
-
-	bool operator <(const mapNode &var) const{
-		if (r < var.r) return true;
-		if (r > var.r) return false;
-
-		if (c < var.c) return true;
-		if (c > var.c) return false;
-
-		return board < var.board;
-	}
-};
-
-//(r, c) -> (i, j) 이동 최소 조작 횟수
-int getMoves(int r, int c, int i, int j) {
-	if (r == i && c == j) return 0;
-	else if (r == i || c == j) return 1;
-	else if (i == 0 || i == 3 || j == 0 || j == 3) return 2;
-	return min(abs(r - i), abs(c - j)) + 1;
+//HH:MM:SS
+int time_to_int(string time) {
+	string H = time.substr(0, 2);
+	string M = time.substr(3, 2);
+	string S = time.substr(6, 2);
+	return stoi(S) + (60 * stoi(M)) + (360 * stoi(H));
 }
 
-bool inRange(int i, int j) {
-	if (0 > i || i >= 4) return false;
-	if (0 > j || j >= 4) return false;
-	return true;
+string int_to_time(int n) {
+	string H = to_string(n / 360);
+	n = n % 360;
+	if (H.length == 1) H = "0" + H;
+
+	string M = to_string(n / 60);
+	n = n % 60;
+	if (M.length == 1) M = "0" + M;
+
+	string S = to_string(n);
+	if (S.length == 1) S = "0" + S;
+
+	return H + ":" + M + ":" + S;
 }
 
-int BFS(string board, int r, int c) {
-	map <mapNode, int> visited;
-	queue<qNode> q;
 
-	q.push({ r, c, 0, board, {-1, -1}});
+string solution(string play_time, string adv_time, vector<string> logs) {
+	vector<pair<int, int>> log_time;
+	vector<int> all_time;
 
-	int answer = 987654321;
+	int play_time_int = time_to_int(play_time);
+	all_time.push_back(0);
+	all_time.push_back(play_time_int);
 
-	while (!q.empty()) {
-		int r = q.front().r;
-		int c = q.front().c;
-		int moves = q.front().moves;
-		string curBoard= q.front().board;
-		pair<int, int> select = q.front().select;
-		q.pop();
+	//H1:M1:S1-H2:M2:S2
+	for (int i = 0; i < logs.size(); ++i) {
+		string log_time_start = logs[i].substr(0, 8); 
+		int log_time_start_int = time_to_int(log_time_start);
 
-		if (visited.find({ r, c, curBoard }) != visited.end()) {
-			if (visited[{ r, c, curBoard }] < moves) continue;
-		}
-		visited[{r, c, curBoard}] = moves;
+		string log_time_end = logs[i].substr(10, 8);
+		int log_time_end_int = time_to_int(log_time_end);
 
-		if (curBoard == emptyBoard) {
-			answer = min(answer, moves);
-			continue;
-		}
-		
-		//현재 좌표 카드인 경우 현재 좌표 선택
-		if (curBoard[(4 * r) + c] != '0') {
-			if (select.first == -1) {
-				q.push({ r, c, moves +1, curBoard, {r, c} });
-			}
-			else {
-				if (r == select.first && c == select.second) continue;
-
-				if (curBoard[(4 * r) + c] == curBoard[(4 * select.first) + select.second]) {
-					char tmp = curBoard[(4 * r) + c];
-					curBoard[(4 * r) + c] = curBoard[(4 * select.first) + select.second] = '0';
-					q.push({ r,c, moves + 1, curBoard, {-1, -1} });
-					curBoard[(4 * r) + c] = curBoard[(4 * select.first) + select.second] = tmp;
-				}
-			}
-		}
-
-		//다음으로 이동할 좌표
-		//방향키
-		for (int i = 0; i < 4; ++i) {
-			int nextR = r + dirR[i];
-			int nextC = c + dirC[i];
-			if (!inRange(nextR, nextC)) continue;
-			
-
-			//다음 좌표 카드이든 아니든 다음 좌표로 이동
-			if (visited.find({ nextR, nextC, curBoard }) != visited.end()) {
-				if (visited[{nextR, nextC, curBoard }] > moves + getMoves(r, c, nextR, nextC)) {
-					q.push({ nextR, nextC, moves + getMoves(r, c, nextR, nextC), curBoard, {-1, -1} });
-				}
-			}
-
-			//다음 좌표 카드인 경우
-			if (curBoard[(4 * nextR) + nextC] != '0') {
-
-				//다음 좌표로 이동 후 카드 선택
-				//처음 선택하는 경우
-				if (select.first == -1) {
-					q.push({ nextR, nextC, moves + getMoves(r, c, nextR, nextC) + 1, curBoard, {nextR, nextC} });
-				}
-
-				//두번째로 선택하는 경우
-				else {
-					if (curBoard[(4 * nextR) + nextC] == curBoard[(4 * select.first) + select.second]) {
-						char tmp = curBoard[(4 * nextR) + nextC];
-						curBoard[(4 * nextR) + nextC] = curBoard[(4 * select.first) + select.second] = '0';
-						q.push({ nextR, nextC, moves + getMoves(r, c, nextR, nextC) + 1, curBoard, {-1, -1} });
-						curBoard[(4 * nextR) + nextC] = curBoard[(4 * select.first) + select.second] = tmp;
-					}
-				}
-			}
-		}
-
-		//Ctrl + 방향키
-		for (int i = 0; i < 4; ++i) {
-			int nextR = r + dirR[i];
-			int nextC = c + dirC[i];
-			if (!inRange(nextR, nextC))continue;
-
-			while (true) {
-				//다음 좌표 카드아닌 경우 계속 이동
-				if (curBoard[(4 * nextR) + nextC] == '0') continue;
-				//다음 좌표 카드인 경우 이동 멈춤
-				if (curBoard[(4 * nextR) + nextC] != '0') break;
-
-				nextR += dirR[i];
-				nextC += c + dirC[i];
-			}
-		}
-
+		log_time.push_back({log_time_start_int, log_time_end_int});
+		all_time.push_back(log_time_start_int);
+		all_time.push_back(log_time_end_int);
 	}
+
+	sort(log_time.begin(), log_time.end());
 	
-	return answer;
-}
+	sort(all_time.begin(), all_time.end()); 
+	all_time.erase(unique(all_time.begin(), all_time.end()), all_time.end());
 
-int solution(vector<vector<int>> board, int r, int c) {
 
-	string boardStr = "";
-	for (int i = 0; i < 4; ++i) {
-		for (int j = 0; j < 4; ++j) {
-			boardStr += to_string(board[i][j]);
+	vector<int> all_time_cnt(all_time.size(), 0);
+	
+	int max_cnt = -1;
+	//가장 시청자 수 많은 구간 인덱스
+	int all_time_max_idx = -1;
+
+	for (int i = 0; i < all_time.size() - 1; ++i) {
+		//i번째 구간 = all_time[i] ~ all_time[i+1]
+		//구간 내 시청자 수 계산
+		for (int j = 0; j < log_time.size(); ++j) {
+			if (all_time[i + 1] <= log_time[j].first) break;
+
+			if (log_time[j].first <= all_time[i] && all_time[i + 1] <= log_time[j].second) {
+				all_time_cnt[i]++;
+				if (all_time_cnt[i] > max_cnt) {
+					max_cnt = all_time_cnt[i];
+					all_time_max_idx = i;
+				}
+			}
 		}
 	}
 
-	int answer = BFS(boardStr, r, c);
-	return answer;
-}
+	//가장 시청자 수 많은 구간에 광고 배치
+	int ans = all_time[all_time_max_idx];
 
-int main() {
-	solution({ {1, 0, 0, 3},{2, 0, 0, 0},{0, 0, 0, 2},{3, 0, 1, 0} }, 1, 0);
+	int adv_time_int = time_to_int(adv_time);
+	int time_len = all_time[all_time_max_idx + 1] - all_time[all_time_max_idx];
+
+	while (time_len >= adv_time_int) {
+		if()
+	}
+
+	string answer = "";
+	return answer;
 }
