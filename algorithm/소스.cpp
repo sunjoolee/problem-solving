d15f5aed6d9d8d1aedd851ba1e7985e889e0852d
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-05-05 12:41:57 +0900

    프로그래머스 순위 검색
    ing
    정확성 100
    효율성 0

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,75 +1,79 @@
 #include <string>
 #include <vector>
-#include <set>
+#include <map>
 #include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
 
+map<string, vector<int>> m;
+
+void insertInMap(string key, int idx, vector<string> jogun, int score) {
+	if (idx == jogun.size()) {
+		if (m.find(key) == m.end()) 
+			m[key] = vector<int>(1, score);
+		else 
+			m[key].push_back(score);
+		
+		return;
+	}
+
+	insertInMap(key + jogun[idx], idx+1, jogun, score);
+	insertInMap(key + "-", idx+1, jogun, score);
+}
+
 vector<int> solution(vector<string> info, vector<string> query) {
+	vector<int> answer;
 
-	ll cppBitmask = 0LL;
-	ll javaBitmask = 0LL; 
-	ll pythonBitmask = 0LL; 
-	ll backendBitmask = 0LL;
-	ll frontendBitmask = 0LL;
-	ll juniorBitmask = 0LL;
-	ll seniorBitmask = 0LL;
-	ll chickenBitmask = 0LL;
-	ll pizzaBitmask = 0LL;
-	vector<int> score(info.size());
-
+	//"java backend junior pizza 150"
 	for (int i = 0; i < info.size(); ++i) {
+		vector<string> jogun;
+		int score;
+
 		string buffer = "";
 		for (int j = 0; j < info[i].length(); ++j) {
 			if (info[i][j] == ' ') {
-				if(buffer == "cpp") cppBitmask |= (1 << i);
-				else if (buffer == "java") javaBitmask |= (1 << i);
-				else if (buffer == "python") pythonBitmask |= (1 << i);
-				else if (buffer == "backend") backendBitmask |= (1 << i);
-				else if (buffer == "frontend") frontendBitmask |= (1 << i);
-				else if (buffer == "senior") seniorBitmask |= (1 << i);
-				else if (buffer == "junior") juniorBitmask |= (1 << i);
-				else if (buffer == "chicken") chickenBitmask |= (1 << i);
-				else if (buffer == "pizza") pizzaBitmask |= (1 << i);
+				jogun.push_back(buffer);
 				buffer = "";
 			}
 			else buffer += info[i][j];
 		}
-		score[i] = stoi(buffer);
+		score = stoi(buffer);
+
+		//m에 삽입
+		insertInMap("", 0, jogun, score);
 	}
 
-	vector<int> answer;
+	
+	//"cpp and - and senior and pizza 250"
 	for (int i = 0; i < query.size(); ++i) {
-		
-		int cnt = 0;
-		ll bitmask = (1 << (info.size())) - 1;
+		string jogun = "";
+		int score;
 
 		string buffer = "";
 		for (int j = 0; j < query[i].length(); ++j) {
 			if (query[i][j] == ' ') {
-				if (buffer == "cpp") bitmask &= cppBitmask;
-				else if (buffer == "java") bitmask &= javaBitmask;
-				else if (buffer == "python") bitmask &= pythonBitmask;
-				else if (buffer == "backend")bitmask &= backendBitmask;
-				else if (buffer == "frontend") bitmask &= frontendBitmask;
-				else if (buffer == "senior")bitmask &= seniorBitmask;
-				else if (buffer == "junior") bitmask &= juniorBitmask;
-				else if (buffer == "chicken") bitmask &= chickenBitmask;
-				else if (buffer == "pizza") bitmask &= pizzaBitmask;
+				if(buffer != "and") jogun = jogun + buffer;
 				buffer = "";
 			}
 			else buffer += query[i][j];
 		}
-		int X = stoi(buffer);
-		for (int j = 0; j < info.size(); ++j) {
-			if (bitmask & (1 << j)) {
-				if (score[j] >= X) cnt++;
-			}
-		}
+		score = stoi(buffer);
 
-		answer.push_back(cnt);
+		//query 조건에 맞는 후보들의 점수
+		vector<int> hoobo = m[jogun];
+		
+		//오름차순 정렬
+		sort(hoobo.begin(), hoobo.end());
+
+		//lower_bound = 찾으려는 key 값보다 같거나 큰 숫자가 배열 몇 번째에서 처음 등장하는지 
+		answer.push_back(hoobo.size() - (lower_bound(hoobo.begin(), hoobo.end(), score) - hoobo.begin()));
 	}
+
 	return answer;
+}
+
+int main() {
+	solution({ "java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50" }, { "java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150" });
 }
