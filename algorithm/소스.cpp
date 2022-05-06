f76bfa89831ed6df703404a44b7fa07d4aae123f
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-05-06 23:33:50 +0900

    백준 1700 멀티탭 스케줄링

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,92 +1,107 @@
-#include <string>
-#include <vector>
+#include <iostream>
 #include <algorithm>
-
+#include <vector>
+#include <queue>
+#include <map>
 using namespace std;
 
-//HH:MM:SS
-int time_to_int(string time) {
-	string H = time.substr(0, 2);
-	string M = time.substr(3, 2);
-	string S = time.substr(6, 2);
-	return stoi(S) + (60 * stoi(M)) + (360 * stoi(H));
-}
 
-string int_to_time(int n) {
-	string H = to_string(n / 360);
-	n = n % 360;
-	if (H.length == 1) H = "0" + H;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-	string M = to_string(n / 60);
-	n = n % 60;
-	if (M.length == 1) M = "0" + M;
+	//n: 멀티탭 구멍의 개수
+	//k: 전기용품 총 사용 횟수
+	int n, k;
+	cin >> n >> k;
 
-	string S = to_string(n);
-	if (S.length == 1) S = "0" + S;
+	//전기용품 사용 순서 
+	vector<int> vec;
 
-	return H + ":" + M + ":" + S;
-}
+	//vecmap[전기용품]: 전기용품 사용되는 시점 저장된 queue
+	//교체할 전기용품을 선택할 때 가장 나중에 사용되는 전기용품이 무엇인지 보기 위함
+	//따라서 전기용품을 사용할 때 queue pop 해줌 
+	map<int, queue<int>> vecmap;
 
+	//전기용품 사용 순서 입력 받기
+	int num;
+	for (int i = 0; i < k; ++i) {
+		cin >> num;
+		vec.push_back(num);
+		
+		//전기용품 사용 시점 저장
 
-string solution(string play_time, string adv_time, vector<string> logs) {
-	vector<pair<int, int>> log_time;
-	vector<int> all_time;
+		if (vecmap.find(num) == vecmap.end())
+			vecmap[num] = queue<int>();
 
-	int play_time_int = time_to_int(play_time);
-	all_time.push_back(0);
-	all_time.push_back(play_time_int);
-
-	//H1:M1:S1-H2:M2:S2
-	for (int i = 0; i < logs.size(); ++i) {
-		string log_time_start = logs[i].substr(0, 8); 
-		int log_time_start_int = time_to_int(log_time_start);
-
-		string log_time_end = logs[i].substr(10, 8);
-		int log_time_end_int = time_to_int(log_time_end);
-
-		log_time.push_back({log_time_start_int, log_time_end_int});
-		all_time.push_back(log_time_start_int);
-		all_time.push_back(log_time_end_int);
+		vecmap[num].push(vec.size() - 1);
 	}
 
-	sort(log_time.begin(), log_time.end());
-	
-	sort(all_time.begin(), all_time.end()); 
-	all_time.erase(unique(all_time.begin(), all_time.end()), all_time.end());
+	//멀티탭에 꽂혀있는 전기용품 
+	vector<int> plug;
 
+	//최소 교체 횟수
+	int answer = 0;
 
-	vector<int> all_time_cnt(all_time.size(), 0);
-	
-	int max_cnt = -1;
-	//가장 시청자 수 많은 구간 인덱스
-	int all_time_max_idx = -1;
-
-	for (int i = 0; i < all_time.size() - 1; ++i) {
-		//i번째 구간 = all_time[i] ~ all_time[i+1]
-		//구간 내 시청자 수 계산
-		for (int j = 0; j < log_time.size(); ++j) {
-			if (all_time[i + 1] <= log_time[j].first) break;
-
-			if (log_time[j].first <= all_time[i] && all_time[i + 1] <= log_time[j].second) {
-				all_time_cnt[i]++;
-				if (all_time_cnt[i] > max_cnt) {
-					max_cnt = all_time_cnt[i];
-					all_time_max_idx = i;
-				}
+	//전기용품 사용 순서 순회
+	for (int i = 0; i < k; ++i) {
+		num = vec[i];
+		
+		//멀티탭에 꽂혀있는지 확인
+		bool flag = false;
+		for (int i = 0; i < plug.size(); ++i) {
+			if (plug[i] == num) {
+				flag = true;
+				break;
 			}
 		}
+		//꽂혀있는 경우 그대로 사용
+		if (flag) {
+			vecmap[num].pop();
+			continue;
+		}
+
+		//꽂혀있지 않는 경우
+
+		//멀티탭 자리 있으면 꽂는다
+		if (plug.size() < n) {
+			plug.push_back(num);
+			vecmap[num].pop();
+			continue;
+		}
+
+		//멀티탭 자리 없으면 전기용품 교체해야함
+
+		//가장 나중에 사용되는 전기용품의 멀티탭 위치
+		int maxIdx = 0;
+		//가장 나중에 사용되는 전기용품의 사용되는 시점 
+		int maxpos = -1;
+
+		//멀티탭에 꽂혀있는 전기용품 중 가장 나중에 사용되는 것 찾기
+		for (int i = 0; i < plug.size(); ++i) {
+
+			//전기용품의 사용되는 시점
+			//만약 앞으로 전기용품이 사용될 예정이 없다면 k로 설정 
+			int pos;
+			if (vecmap[plug[i]].empty()) {
+				pos = k;
+			}
+			else pos = vecmap[plug[i]].front();
+
+			if (pos > maxpos) {
+				maxpos = pos;
+				maxIdx = i;
+			}
+		}
+
+		//가장 나중에 사용되는 전기용품을 교체
+		// -> 가장 나중에 사용되는 전기용품의 멀티탭 위치에 꽂는다 
+		plug[maxIdx] = num;
+		vecmap[num].pop();
+
+		++answer;
 	}
 
-	//가장 시청자 수 많은 구간에 광고 배치
-	int ans = all_time[all_time_max_idx];
-
-	int adv_time_int = time_to_int(adv_time);
-	int time_len = all_time[all_time_max_idx + 1] - all_time[all_time_max_idx];
-
-	while (time_len >= adv_time_int) {
-		if()
-	}
-
-	string answer = "";
-	return answer;
+	cout << answer;
+	return 0;
 }
