81411fff508a3472cca40c584adb98f86081f8d9
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-05-20 13:34:18 +0900

    백준 1005 ACM Craft

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,86 +3,104 @@
 #include <queue>
 using namespace std;
 
+typedef long long ll;
+
 const int MAXN = 1001;
 
-int n;
+//건물을 짓는데 걸리는 시간
+ll delay[MAXN] = { 0 };
+
+//건물을 짓기 시작하는 시점
+ll construct[MAXN] = { 0 };
 
 //inDegree[i]: 정점i에 들어가는 간선의 수
 int inDegree[MAXN] = { 0 };
+
 //vector<int> child[i]: 노드i를 선행 노드로 하는 노드들의 집합
 vector<int> child[MAXN];
 
+
 //위상 정렬 함수
-void topologySort() {
-	int result[MAXN];
+int topologySort(int n, int w) {
+	
 	queue<int> q;
 
 	//진입 차수 0인 정점 큐에 삽입
-	for (int i = 1; i <= n; ++i)
+	for (int i = 1; i <= n; ++i) {
 		if (inDegree[i] == 0) q.push(i);
+	}
 
 	//정렬이 완전히 수행되기까지 n개의 정점을 방문한다
 	for (int i = 1; i <= n; ++i) {
-		//n개의 정점을 방문하기 전에 큐가 비어버리면 
-		//사이클이 발생한 것이다
-		if (q.empty()) {
-			cout << "0";
-			return;
-		}
+
+		//n개의 정점을 방문하기 전에 큐가 비어버리면 사이클이 발생한 것
+		if (q.empty()) return -1;
+		
 
 		//진입 차수가 0인 정점 선택
 		int parent = q.front();
-		result[i] = parent;
+
+		//승리하기 위해 건설해야하는 건물인 경우
+		if (parent == w) return construct[w] + delay[w];
 
 		//선택된 정점과 여기에 부속된 모든 간선들 삭제
 		q.pop();
 		for (int i = 0; i < child[parent].size(); ++i) {
 			int childnode = child[parent][i];
+			inDegree[childnode]--;
+
+			//child 건물을 짓기 시작하는 시점
+			construct[childnode] = max(construct[childnode], construct[parent] + delay[parent]);
 
 			//새롭게 진입차수가 0이 된 정점을 큐에 삽입
-			if (--inDegree[childnode] == 0)
+			if (inDegree[childnode] == 0) {
 				q.push(childnode);
+			}
 		}
 	}
-
-	//위상 정렬 결과 출력
-	for (int i = 1; i <= n; ++i) {
-		cout << result[i] << " ";
-	}
-	return;
+	return -1;
 }
 
 
 int main() {
-	//정점의 수
-	cin >> n;
 	
-	//가수 순서 입력받기
-	int m;
-	cin >> m;
-	for (int i = 0; i < m; ++i) {
-		int len;
-		cin >> len;
+	//테스트 케이스 수
+	int t;
+	cin >> t;
+	while(t--){
+		//초기화
+		for (int i = 0; i < MAXN; ++i) {
+			delay[i] = 0;
+			construct[i] = 0;
+			inDegree[i] = { 0 };
+			child[i].clear();
 
-		vector<int> seq;
-		for(int j = 0; j< len; ++j){
-			int input;
-			cin >> input;
-			seq.push_back(input);
+		}
+		//건물 수
+		int n;
+		cin >> n;
+		//건물 규칙 수
+		int k;
+		cin >> k;
+
+		for (int i = 1; i <= n; ++i) {
+			ll d;
+			cin >> d;
+			delay[i] = d;
 		}
 
-		//seq = {1, 4, 3}
-		//child[1] = {4, 3}
-		//child[4] = {3}
-		for (int j = 0; j < len - 1; ++j) {
-			int parent = seq[j];
-			for (int k = j + 1; k < len; ++k) {
-				child[parent].push_back(seq[k]);
-				inDegree[seq[k]]++;
-			}
+		for (int i = 0; i < k; ++i) {
+			int x, y;
+			cin >> x >> y;
+			child[x].push_back(y);
+			inDegree[y]++;
 		}
+
+		//승리하기 위해 건설해야하는 건물 번호
+		int w;
+		cin >> w;
+
+		cout << topologySort(n,w) <<"\n";
 	}
-
-	topologySort();
 	return 0;
 }
