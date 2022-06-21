0cdea9381f9236fcd71a6e8b98a98e460e77719b
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-06-21 19:47:47 +0900

    프로그래머스 사라지는 발판

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,91 +1,73 @@
 #include <string>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
-typedef long long ll;
 
-int rSize;
-int cSize;
-vector<vector<ll>> imosBoard;
+const int IMPOSSIBLE = 987654321;
 
-//skill {type, r1, c1, r2, c2, degree}
-void useImos(vector<int> skill) {
-	int type = skill[0];
-	int r1 = skill[1];
-	int c1 = skill[2];
-	int r2 = skill[3];
-	int c2 = skill[4];
-	ll degree = skill[5];
-	
-	//공격
-	if (type == 1) degree= -degree;
+int R;
+int C;
 
-	//imos법 사용
-	//좌측상단
-	imosBoard[r1][c1] += degree;
-	//우측상단
-	imosBoard[r1][c2 + 1] -= degree;
-	//좌측하단
-	imosBoard[r2+1][c1] -= degree;
-	//우측하단
-	imosBoard[r2+1][c2+1] += degree;
-}
+int rMove[4] = { 0, 0, 1, -1 };
+int cMove[4] = { 1, -1, 0, 0 };
 
-void calcImosBoard() {
-	//가로 방향으로 휩쓸며 누적합 계산
-	for (int i = 0; i < rSize + 1; ++i) {
-		ll sum = 0;
-		for (int j = 0; j < cSize + 1; ++j) {
-			sum += imosBoard[i][j];
-			imosBoard[i][j] = sum;
-		}
-	}
-	//세로 방향으로 휩쓸며 누적합 계산
-	for (int j = 0; j < cSize + 1; ++j) {
-		ll sum = 0;
-		for (int i = 0; i < rSize + 1; ++i) {
-			sum += imosBoard[i][j];
-			imosBoard[i][j] = sum;
-		}
-	}
+bool inRange(int r, int c) {
+	if (r < 0 || r >= R) return false;
+	if (c < 0 || c >= C) return false;
+	return true;
 }
 
 
-int solution(vector<vector<int>> board, vector<vector<int>> skill) {
-	rSize = board.size();
-	cSize = board[0].size();
+//<win, moves>
+pair<int,int> game(vector<vector<int>> board, pair<int, int> aloc, pair<int, int> bloc) {
+	//움직일 차례인데 캐릭터의 상하좌우 주변 4칸이 모두 발판이 없거나 보드 밖이라서 이동할 수 없는 경우, 패배
+	//두 캐릭터가 같은 발판 위에 있을 때, 상대 캐릭터가 다른 발판으로 이동하여 자신의 캐릭터가 서있던 발판이 사라지게 되면 패배
 
-	//imosBoard 0으로 초기화
-	//imosBoard의 크기는 board의 크기보다 가로 세로 1만큼 큼
-	for (int i = 0; i < rSize + 1; ++i) {
-		vector<ll> tmp(cSize + 1, 0);
-		imosBoard.push_back(tmp);
-	}
+	bool cantMove = true;
 
-	for (int i = 0; i < skill.size(); ++i) {
-		useImos(skill[i]);
-	}
-	
-	
-	int answer = 0;
+	int minRet = IMPOSSIBLE;
+	int maxRet = -1;
 
-	calcImosBoard();
-	for (int i = 0; i <rSize; ++i) {
-		for (int j = 0; j < cSize; ++j) {
-			board[i][j] += imosBoard[i][j];
-			if (board[i][j] > 0) ++answer;
+	for (int i = 0; i < 4; ++i) {
+		int nextR = aloc.first + rMove[i];
+		int nextC = aloc.second + cMove[i];
+
+		if (!inRange(nextR, nextC)) continue;
+		if (board[nextR][nextC] == 0) continue;
+		cantMove = false;
+
+		//무조건 이기는 경우 발생한 경우
+		if (aloc.first == bloc.first && aloc.second == bloc.second)
+			return { 1, 1 };
+
+		board[aloc.first][aloc.second] = 0;
+		pair<int, int> gameResult = game(board, bloc, { nextR, nextC });
+		//이긴 경우 최대한 빨리 이기기 위해 플레이
+		if (gameResult.first == 0) {
+			minRet = min(minRet, 1 + gameResult.second);
 		}
+		//진 경우 최대한 느리게 지기 위해 플레이
+		else {
+			maxRet = max(maxRet, 1 + gameResult.second);
+		}
+		board[aloc.first][aloc.second] = 1;
 	}
+
+	if (cantMove) return { 0, 0 };
+
+	//이길 수 있는 경우가 있는 경우
+	if (minRet != IMPOSSIBLE) return { 1, minRet};
+	//이길 수 있는 경우가 없는 경우
+	else return { 0, maxRet };
+}
+
+int solution(vector<vector<int>> board, vector<int> aloc, vector<int> bloc) {
+	R = board.size();
+	C = board[0].size();
+
+	pair<int, int> gameResult = game(board, { aloc[0], aloc[1] }, { bloc[0], bloc[1] });
+
+	int answer = gameResult.second;
 	return answer;
-}
-
-int main() {
-	solution({ {5, 5, 5, 5, 5},
-		{5, 5, 5, 5, 5},
-		{5, 5, 5, 5, 5},
-		{5, 5, 5, 5, 5} }
-		, { {1, 0, 0, 3, 4, 4},
-		{1, 2, 0, 2, 3, 2},
-		{2, 1, 0, 3, 1, 2},
-		{1, 0, 1, 3, 3, 1} });
 }
