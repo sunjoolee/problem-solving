e81df780cf55d1ebd7ac83d533bfb4cfd2b11b5e
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-06-21 10:29:57 +0900

    프로그래머스 파괴되지 않은 건물
    imos

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -6,10 +6,10 @@ typedef long long ll;
 
 int rSize;
 int cSize;
-vector<vector<ll>> Board;
+vector<vector<ll>> imosBoard;
 
 //skill {type, r1, c1, r2, c2, degree}
-void useSkill(vector<int> skill) {
+void useImos(vector<int> skill) {
 	int type = skill[0];
 	int r1 = skill[1];
 	int c1 = skill[2];
@@ -17,34 +17,63 @@ void useSkill(vector<int> skill) {
 	int c2 = skill[4];
 	ll degree = skill[5];
 	
-	for (int r = r1; r <= r2; ++r) {
-		for (int c = c1; c <= c2; ++c) {
-			if (type == 1) Board[r][c] -= degree;
-			else Board[r][c] += degree;
+	//공격
+	if (type == 1) degree= -degree;
+
+	//imos법 사용
+	//좌측상단
+	imosBoard[r1][c1] += degree;
+	//우측상단
+	imosBoard[r1][c2 + 1] -= degree;
+	//좌측하단
+	imosBoard[r2+1][c1] -= degree;
+	//우측하단
+	imosBoard[r2+1][c2+1] += degree;
+}
+
+void calcImosBoard() {
+	//가로 방향으로 휩쓸며 누적합 계산
+	for (int i = 0; i < rSize + 1; ++i) {
+		ll sum = 0;
+		for (int j = 0; j < cSize + 1; ++j) {
+			sum += imosBoard[i][j];
+			imosBoard[i][j] = sum;
+		}
+	}
+	//세로 방향으로 휩쓸며 누적합 계산
+	for (int j = 0; j < cSize + 1; ++j) {
+		ll sum = 0;
+		for (int i = 0; i < rSize + 1; ++i) {
+			sum += imosBoard[i][j];
+			imosBoard[i][j] = sum;
 		}
 	}
 }
+
 
 int solution(vector<vector<int>> board, vector<vector<int>> skill) {
 	rSize = board.size();
 	cSize = board[0].size();
 
-	for (int i = 0; i < rSize; ++i) {
-		vector<ll> tmp;
-		for (int j = 0; j < cSize; ++j) {
-			tmp.push_back(board[i][j]);
-		}
-		Board.push_back(tmp);
+	//imosBoard 0으로 초기화
+	//imosBoard의 크기는 board의 크기보다 가로 세로 1만큼 큼
+	for (int i = 0; i < rSize + 1; ++i) {
+		vector<ll> tmp(cSize + 1, 0);
+		imosBoard.push_back(tmp);
 	}
 
 	for (int i = 0; i < skill.size(); ++i) {
-		useSkill(skill[i]);
+		useImos(skill[i]);
 	}
 	
+	
 	int answer = 0;
+
+	calcImosBoard();
 	for (int i = 0; i <rSize; ++i) {
 		for (int j = 0; j < cSize; ++j) {
-			if (Board[i][j] <= 0) ++answer;
+			board[i][j] += imosBoard[i][j];
+			if (board[i][j] > 0) ++answer;
 		}
 	}
 	return answer;
