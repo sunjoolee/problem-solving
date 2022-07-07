f7fe5f4d23c6675818f39980422b2703c1d5f7da
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-08 08:10:59 +0900

    백준 9076 점수 집계

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,49 +1,28 @@
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <math.h>
 #include <iostream>
 using namespace std;
-
-typedef long long ll;
-
-//색이 n인 점들의 벡터 = vec[n]
-vector<vector<ll>>vec(100000, vector<ll>());
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		ll x;
-		int y;
-		cin >> x >> y;
-		vec[y-1].push_back(x);
-	}
-
-	ll sum = 0;
-	for (int i = 0; i < n; ++i) {
-		if (vec[i].size() < 2) continue;
-
-		sort(vec[i].begin(), vec[i].end());
-
-		for (int j = 0; j < vec[i].size(); ++j) {
-			ll minD;
-			if (j == 0) {
-				minD = abs(vec[i][j] - vec[i][j + 1]);
-			}
-			else if (j == vec[i].size() - 1) {
-				minD = abs(vec[i][j] - vec[i][j - 1]);
-			}
-			else {
-				minD = min(abs(vec[i][j] - vec[i][j + 1]),abs( vec[i][j] - vec[i][j - 1]));
-			}
-			
-			sum += minD;
+	int t;
+	cin >> t;
+	while (t--) {
+		vector<int> score;
+		for (int i = 0; i < 5; ++i) {
+			int input;
+			cin >> input;
+			score.push_back(input);
 		}
+
+		sort(score.begin(), score.end());
+
+		if (abs(score[1] - score[3]) >= 4) cout << "KIN\n";
+		else cout << score[1] + score[2] + score[3] << "\n";
 	}
 
-	cout << sum;
 	return 0;
 }
