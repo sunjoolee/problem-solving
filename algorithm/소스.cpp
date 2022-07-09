5e189564354dd72e101a92b9bcf70f7ecd3d5f6e
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-09 11:51:38 +0900

    백준 8979 올림픽

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,38 +1,55 @@
-
 #include <algorithm>
 #include <vector>
 #include <iostream>
 using namespace std;
 
+struct nation {
+	int num;
+	int gold;
+	int silver;
+	int bronze;
+
+	nation(int n, int g, int s, int b) : num(n), gold(g), silver(s), bronze(b){}
+};
+
+bool cmp(nation a, nation b) {
+	if (a.gold > b.gold) return true;
+	else if (a.gold == b.gold) {
+		if (a.silver > b.silver) return true;
+		else if (a.silver == b.silver) return a.bronze > b.bronze;
+		else return false;
+	}
+	return false;
+}
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int k;
-	cin >> k;
-	for (int i = 0; i < k; ++i) {
-		cout << "Class " << i + 1<<"\n";
-		int n;
-		cin >> n;
+	int N, k;
+	cin >> N >> k;
 
-		vector<int> vec;
-		for (int j = 0; j < n; ++j) {
-			int input;
-			cin >> input;
-			vec.push_back(input);
-		}
-		sort(vec.begin(), vec.end());
-		
-		int gap = -1;
-		for (int j = 0; j < vec.size() - 1; ++j) {
-			gap = max(gap, vec[j + 1] - vec[j]);
-		}
+	vector<nation> vec;
 
-		cout << "Max " << vec[vec.size() - 1];
-		cout << ", ";
-		cout << "Min " << vec[0];
-		cout << ", ";
-		cout << "Largest gap " << gap << "\n";
+	for (int i = 0; i < N; ++i) {
+		int n, g, s, b;
+		cin >> n >> g >> s >> b;
+		vec.push_back(nation(n, g, s, b));
 	}
+	sort(vec.begin(), vec.end(), cmp);
+
+	int rating = 1;
+	if (vec[0].num != k) {
+		for (int i = 1; i < N; ++i) {
+			if ((vec[i - 1].gold != vec[i].gold)
+				|| (vec[i - 1].silver != vec[i].silver)
+				|| (vec[i - 1].bronze != vec[i].bronze)) {
+				rating = i + 1;
+			}
+
+			if (vec[i].num == k) break;
+		}
+	}
+	cout << rating;
 	return 0;
 }
