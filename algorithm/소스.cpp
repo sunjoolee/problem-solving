5caa408c5313dba997a92a1fd094618e35a699a4
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-09 12:01:26 +0900

    백준 15649 N과 M (1)

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,53 +3,44 @@
 #include <iostream>
 using namespace std;
 
-struct nation {
-	int num;
-	int gold;
-	int silver;
-	int bronze;
+int n, m;
+vector<bool> used;
+vector<int> arr;
 
-	nation(int n, int g, int s, int b) : num(n), gold(g), silver(s), bronze(b){}
-};
-
-bool cmp(nation a, nation b) {
-	if (a.gold > b.gold) return true;
-	else if (a.gold == b.gold) {
-		if (a.silver > b.silver) return true;
-		else if (a.silver == b.silver) return a.bronze > b.bronze;
-		else return false;
+void makeArr(int len) {
+	if (len == m) {
+		for (int i = 0; i < len; ++i) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
 	}
-	return false;
+
+	for (int num = 1; num <= n; ++num) {
+		if (used[num] == true) continue;
+		
+		used[num] = true;
+		arr.push_back(num);
+
+		makeArr(len + 1);
+
+		arr.pop_back();
+		used[num] = false;
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int N, k;
-	cin >> N >> k;
+	
+	cin >> n >> m;
 
-	vector<nation> vec;
-
-	for (int i = 0; i < N; ++i) {
-		int n, g, s, b;
-		cin >> n >> g >> s >> b;
-		vec.push_back(nation(n, g, s, b));
+	for (int i = 0; i <= n; ++i) {
+		used.push_back(false);
 	}
-	sort(vec.begin(), vec.end(), cmp);
 
-	int rating = 1;
-	if (vec[0].num != k) {
-		for (int i = 1; i < N; ++i) {
-			if ((vec[i - 1].gold != vec[i].gold)
-				|| (vec[i - 1].silver != vec[i].silver)
-				|| (vec[i - 1].bronze != vec[i].bronze)) {
-				rating = i + 1;
-			}
+	makeArr(0);
 
-			if (vec[i].num == k) break;
-		}
-	}
-	cout << rating;
 	return 0;
 }
