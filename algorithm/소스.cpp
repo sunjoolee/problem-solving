1b9ea910f478d73cba3a7c786e44b5994fa5f08a
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-11 21:56:59 +0900

    백준 1158 요세푸스 문제

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,46 +1,37 @@
+
+#include <algorithm>
+#include <queue>
+#include <vector>
 #include <iostream>
 using namespace std;
-
-typedef long long ll;
-//const int cacheSize = 1000000;
-
-ll a, b, c;
-
-//ll cache[cacheSize] = { 0 };
-
-ll calc(int n) {
-	if (n == 0) return 1;
-	if (n == 1) return a % c;
-	
-	//if (n < cacheSize) {
-	//	if (cache[n] != 0) return cache[n];
-	//}
-
-	ll res;
-	if (n % 2 == 0) {
-		res = calc(n / 2) * calc(n / 2);
-		res %= c;
-	}
-	else {
-		res = calc((n - 1) / 2) * calc((n - 1) / 2);
-		res %= c;
-		res *= a;
-		res %= c;
-	}
-
-	//if (n < cacheSize) {
-	//	cache[n] = res;
-	//}
-	return res;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	
-	cin >> a >> b >> c;
 
-	cout << calc(b);
+	int n, k;
+	cin >> n >> k;
+
+	queue<int> circle;
+	for (int i = 1; i <= n; ++i) {
+		circle.push(i);
+	}
+
+	vector<int> vec;
+	while (!circle.empty()) {
+		for (int i = 0; i < k-1; ++i) {
+			circle.push(circle.front());
+			circle.pop();
+		}
+		vec.push_back(circle.front());
+		circle.pop();
+	}
+
+	cout << "<";
+	for (int i = 0; i < n-1; ++i) {
+		cout << vec[i] << ", ";
+	}
+	cout << vec[n - 1] << ">";
 
 	return 0;
 }
