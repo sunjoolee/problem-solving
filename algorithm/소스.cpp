cd0fb2292578fed96940cf37ce553b3f43016719
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-16 21:39:00 +0900

    백준 1914 하노이 탑
    ing

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,53 +1,24 @@
+#include <queue>
+#include <math.h>
 #include <algorithm>
-#include <vector>
-#include <numeric>
 #include <iostream>
 using namespace std;
 
-typedef long long ll;
-
 int n;
-vector<ll> vec;
 
-ll calcGcd(ll a, ll b) {
-	ll c;
-	while (b != 0) {
-		c = a % b;
-		a = b;
-		b = c;
+int k;
+queue<pair<int, int>> q;
+
+void hanoi(int num, int start, int mid, int end) {
+	if (num == 1) {
+		q.push({ start, end });
+		return;
 	}
-	return a;
-}
 
-ll maxSum(int start, int end) {
-	//배열 크기 1
-	if (start == end) return vec[start];
-	//배열 크기 2 
-	if (start + 1 == end) return vec[start] + vec[end];
-
-	int mid = (end - start + 1) / 2;
-
-	//왼쪽부터 원소를 선택하는 경우
-	ll leftSum = 0;
-	int idx = start;
-	ll gcd = vec[start];
-	while (idx <= (start + mid-1)) {
-		gcd = calcGcd(gcd, vec[idx]);
-		idx++;
-	}
-	leftSum = gcd + maxSum(start + mid, end);
-
-	//오른쪽부터 원소를 선택하는 경우
-	ll rightSum = 0;
-
-	gcd = vec[end];
-	while (idx <= end) {
-		gcd = calcGcd(gcd, vec[idx]);
-		idx++;
-	}
-	rightSum = gcd + maxSum(start, start + mid -1);
-
-	return max(leftSum, rightSum);
+	hanoi(num - 1, start, end, mid);
+	hanoi(1, start, mid, end);
+	hanoi(num - 1, mid, start, end);
+	return;
 }
 
 int main() {
@@ -55,12 +26,16 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 
 	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		int input;
-		cin >> input;
-		vec.push_back(input);
+
+	cout << pow(2,n) - 1 << "\n";
+
+	if (n <= 20) {
+		hanoi(n, 1, 2, 3);
+		while (!q.empty()) {
+			cout << q.front().first << " " << q.front().second << "\n";
+			q.pop();
+		}
 	}
 
-	cout << maxSum(0, n - 1);
 	return 0;
 }
