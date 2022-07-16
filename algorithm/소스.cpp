90cdac5d531888181b4a50b6c528bcb042b5a0bd
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-16 21:53:05 +0900

    백준 1935 후위 표기식2

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,41 +1,74 @@
-#include <queue>
-#include <math.h>
 #include <algorithm>
+#include <string>
+#include <stack>
+#include <vector>
 #include <iostream>
 using namespace std;
-
-int n;
-
-int k;
-queue<pair<int, int>> q;
-
-void hanoi(int num, int start, int mid, int end) {
-	if (num == 1) {
-		q.push({ start, end });
-		return;
-	}
-
-	hanoi(num - 1, start, end, mid);
-	hanoi(1, start, mid, end);
-	hanoi(num - 1, mid, start, end);
-	return;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
+	int n;
 	cin >> n;
 
-	cout << pow(2,n) - 1 << "\n";
+	string str;
+	cin >> str;
 
-	if (n <= 20) {
-		hanoi(n, 1, 2, 3);
-		while (!q.empty()) {
-			cout << q.front().first << " " << q.front().second << "\n";
-			q.pop();
+	vector<double> alpha;
+	for (int i = 0; i < n; ++i) {
+		double input;
+		cin >> input;
+		alpha.push_back(input);
+	}
+
+	stack<double> st;
+	for (int i = 0; i < str.length(); ++i) {
+		char ch = str[i];
+		//연산자인 경우
+		if (ch == '+') {
+			double db2 = st.top();
+			st.pop();
+			double db1 = st.top();
+			st.pop();
+
+			st.push(db1 + db2);
+
+		}
+		else if (ch == '-') {
+			double db2 = st.top();
+			st.pop();
+			double db1 = st.top();
+			st.pop();
+
+			st.push(db1 - db2);
+		}
+		else if (ch == '*') {
+			double db2 = st.top();
+			st.pop();
+			double db1 = st.top();
+			st.pop();
+
+			st.push(db1 * db2);
+		}
+		else if (ch == '/') {
+			double db2 = st.top();
+			st.pop();
+			double db1 = st.top();
+			st.pop();
+			st.push(db1 / db2);
+		}
+		//피연산자인 경우
+		else {
+			//알파벳 -> 대응하는 숫자로 치환하여 스택에 저장
+			st.push(alpha[ch - 'A']);
 		}
 	}
+
+	//소수점 아래 2자리까지 출력
+	cout << fixed;
+	cout.precision(2);
+	cout << st.top();
 
 	return 0;
 }
