95dffa0c59e774b20dc07835b4b0b2a5c8118374
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-18 11:21:20 +0900

    백준 1914 하노이 탑
    해결

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -9,6 +9,49 @@ int n;
 int k;
 queue<pair<int, int>> q;
 
+void hanoiMove() {
+	//2^n
+	vector<short> v1;
+	vector<short> v2;
+
+	vector<short>& before = v1;
+	vector<short>& after = v2;
+	before.push_back(1);
+	for (int i = 0; i < n; ++i) {
+		int carry = 0;
+		for (int j = 0; j < before.size(); ++j) {
+				short mul =before[j] * 2;
+				mul += carry;
+				after.push_back(mul % 10);
+				carry = mul / 10;
+		}
+		if (carry > 0) after.push_back(carry);
+
+		swap(before, after);
+		after.clear();
+	}
+
+	//-1
+	if (v1[0] > 0) v1[0] = v1[0] - 1;
+	else {
+		v1[0] = 9;
+
+		int idx = 1;
+		while (v1[idx] == 0) {
+			v1[idx] = 9;
+			idx++;
+		}
+		v1[idx] = v1[idx] - 1;
+	}
+
+
+	//Ãâ·Â
+	for (int i = v1.size() - 1; i >= 0; --i) {
+		cout << v1[i];
+	}
+	cout << "\n";
+}
+
 void hanoi(int num, int start, int mid, int end) {
 	if (num == 1) {
 		q.push({ start, end });
@@ -27,8 +70,8 @@ int main() {
 
 	cin >> n;
 
-	cout << pow(2,n) - 1 << "\n";
-
+	hanoiMove();
+	
 	if (n <= 20) {
 		hanoi(n, 1, 2, 3);
 		while (!q.empty()) {
