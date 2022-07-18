784c5e93362ce9eea8062dc9c7ce37b7f6a58b29
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-18 18:16:09 +0900

    백준 5464 주차장

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,84 +1,96 @@
-#include <queue>
-#include <math.h>
 #include <algorithm>
+#include <vector>
+#include <queue>
+#include <map>
 #include <iostream>
 using namespace std;
 
-int n;
-
-int k;
-queue<pair<int, int>> q;
-
-void hanoiMove() {
-	//2^n
-	vector<short> v1;
-	vector<short> v2;
-
-	vector<short>& before = v1;
-	vector<short>& after = v2;
-	before.push_back(1);
-	for (int i = 0; i < n; ++i) {
-		int carry = 0;
-		for (int j = 0; j < before.size(); ++j) {
-				short mul =before[j] * 2;
-				mul += carry;
-				after.push_back(mul % 10);
-				carry = mul / 10;
-		}
-		if (carry > 0) after.push_back(carry);
-
-		swap(before, after);
-		after.clear();
-	}
-
-	//-1
-	if (v1[0] > 0) v1[0] = v1[0] - 1;
-	else {
-		v1[0] = 9;
-
-		int idx = 1;
-		while (v1[idx] == 0) {
-			v1[idx] = 9;
-			idx++;
-		}
-		v1[idx] = v1[idx] - 1;
-	}
-
-
-	//출력
-	for (int i = v1.size() - 1; i >= 0; --i) {
-		cout << v1[i];
-	}
-	cout << "\n";
-}
-
-void hanoi(int num, int start, int mid, int end) {
-	if (num == 1) {
-		q.push({ start, end });
-		return;
-	}
-
-	hanoi(num - 1, start, end, mid);
-	hanoi(1, start, mid, end);
-	hanoi(num - 1, mid, start, end);
-	return;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n;
+	int n; //주차공간의 수
+	int m; //차량 수
+	vector<int> R; //주차공간 별 주차 요금
+	vector<int> W; //차량 무게
 
-	hanoiMove();
-	
-	if (n <= 20) {
-		hanoi(n, 1, 2, 3);
-		while (!q.empty()) {
-			cout << q.front().first << " " << q.front().second << "\n";
-			q.pop();
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i) {
+		int r;
+		cin >> r;
+		R.push_back(r);
+	}
+	for (int i = 0; i < m; ++i) {
+		int w;
+		cin >> w;
+		W.push_back(w);
+	}
+
+
+	//주차장 앞 큐
+	queue<int> q;
+
+	//parkinglot[n]: n번 주차공간 비어있으면 0, 차있으면 1
+	vector<int> parkinglot(n, 0);
+
+	//carParked[m]: m번 차가 주차되어있는 주차공간 번호
+	map<int, int> carParked;
+
+	int pay = 0;
+	for (int i = 0; i < (2 * m); ++i) {
+		int car;
+		cin >> car;
+
+		//차가 들어오는 경우
+		if (car > 0) {
+			car--;
+			
+			bool parked = false;
+			//빈 주차공간 찾아서 주차
+			for (int p = 0; p < n; ++p) {
+				if (parkinglot[p] == 0) {
+					carParked[car] = p;
+					parkinglot[p] = 1;
+					parked = true;
+					break;
+				}
+			}
+
+			//빈 주차공간 없는 경우 큐에서 대기
+			if(!parked) q.push(car);
+		}
+
+		//차가 나가는 경우
+		else {
+			car = car * (-1);
+			car--;
+
+			//주차요금 계산
+			int p = carParked[car];
+			pay += (R[p] * W[car]);
+
+			//주차장에서 나감
+			carParked.erase(car);
+			parkinglot[p] = 0;
+
+			//큐에 대기 중인 차 있는 경우
+			if (!q.empty()) {
+
+				//빈 주차공간 있다면 찾아서 주차
+				for (int p = 0; p < n; ++p) {
+					if (parkinglot[p] == 0) {
+						carParked[q.front()] = p;
+						parkinglot[p] = 1;
+						q.pop();
+						break;
+					}
+				}
+			}
 		}
 	}
 
+	cout << pay;
 	return 0;
 }
