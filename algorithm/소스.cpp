4d620deb77c171bc2c2c148cf7df2e01f5d38329
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-18 22:54:02 +0900

    백준 9345 디지털 비디오 디스크(DVDs)

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,48 +3,45 @@
 #include <iostream>
 using namespace std;
 
-const int VEC_SIZE = 1000001;
-
-vector<int> vec (VEC_SIZE, 0);
+vector<int> vec;
 
 const int ROOT = 1;
+vector<pair<int, int>> segmentTree;
 
-vector<int> segmentTree;
-
-//curNode: curStart ~ curEnd 구간 최댓값을 저장하는 노드 번호 
-int buildRecursive(int curNode, int curStart, int curEnd) {
+//curNode: curStart ~ curEnd 구간 <최댓값, 최솟값>을 저장하는 노드 번호 
+pair<int, int> buildRecursive(int curNode, int curStart, int curEnd) {
 	//리프 노드
 	if (curStart == curEnd) {
-		 return segmentTree[curNode] = vec[curStart];
+		return segmentTree[curNode] = { vec[curStart], vec[curStart] };
 	}
 
 	int mid = (curStart + curEnd) / 2;
-	int rightChildNode = buildRecursive(curNode * 2, curStart, mid);
-	int leftChildNode =buildRecursive((curNode * 2) + 1, mid + 1, curEnd);
-	return segmentTree[curNode] = max(leftChildNode,rightChildNode);
+	pair<int, int> rightChildNode = buildRecursive(curNode * 2, curStart, mid);
+	pair<int, int> leftChildNode =buildRecursive((curNode * 2) + 1, mid + 1, curEnd);
+	return segmentTree[curNode] = { max(leftChildNode.first,rightChildNode.first), min(leftChildNode.second,rightChildNode.second) };
 }
 
 
-int queryRecursive(int curNode, int curStart, int curEnd, int qStart, int qEnd) {
+pair<int, int> queryRecursive(int curNode, int curStart, int curEnd, int qStart, int qEnd) {
 	//쿼리 구간에 포함되지 않음
-	if (curEnd < qStart || qEnd < curStart) return 0LL;
+	if (curEnd < qStart || qEnd < curStart) return { 0 , 987654321};
 	
 	//쿼리 구간에 완전히 포함됨
 	if (qStart <= curStart && curEnd <= qEnd) return segmentTree[curNode];
 
 	//쿼리 구간에 부분적으로 포함됨
 	int mid = (curStart + curEnd) / 2;
-	int rightQuery = queryRecursive(curNode * 2, curStart, mid, qStart, qEnd);
-	int leftQuery = queryRecursive((curNode * 2) + 1, mid + 1, curEnd, qStart, qEnd);
-	return max(rightQuery,leftQuery);
+	pair<int, int> rightQuery = queryRecursive(curNode * 2, curStart, mid, qStart, qEnd);
+	pair<int, int> leftQuery = queryRecursive((curNode * 2) + 1, mid + 1, curEnd, qStart, qEnd);
+	return { max(rightQuery.first,leftQuery.first), min(rightQuery.second,leftQuery.second) };
 }
 
-int updateRecursive(int updateNode, int updateVal, int curNode, int curStart, int curEnd) {
+pair<int, int> updateRecursive(int updateNode, int updateVal, int curNode, int curStart, int curEnd) {
 	//리프노드 
 	if (curStart == curEnd) {
 		//업데이트 할 노드인 경우 값 업데이트
 		if (curStart == updateNode)
-			segmentTree[curNode] = updateVal;
+			segmentTree[curNode] = { updateVal, updateVal };
 
 		return segmentTree[curNode];
 	}
@@ -56,39 +53,53 @@ int updateRecursive(int updateNode, int updateVal, int curNode, int curStart, in
 
 	//업데이트 할 노드가 포함됨 -> 구간합 업데이트
 	int mid = (curStart + curEnd) / 2;
-	int rightUpdate = updateRecursive(updateNode, updateVal, curNode * 2, curStart, mid);
-	int leftUpdate = updateRecursive(updateNode, updateVal, (curNode * 2) + 1, mid + 1, curEnd);
-	return segmentTree[curNode] = max(rightUpdate, leftUpdate);
+	pair<int, int> rightUpdate = updateRecursive(updateNode, updateVal, curNode * 2, curStart, mid);
+	pair<int, int> leftUpdate = updateRecursive(updateNode, updateVal, (curNode * 2) + 1, mid + 1, curEnd);
+	return segmentTree[curNode] = { max(rightUpdate.first, leftUpdate.first), min(rightUpdate.second, leftUpdate.second) };
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
-	int n;
-	cin >> n;
-	vector<int> A;
+	int T;
+	cin >> T;
+	while (T--) {
+		vec.clear();
+		segmentTree.clear();
 
-	for (int i = 0; i < n; ++i) {
-		int input;
-		cin >> input;
-		A.push_back(input);
+		int n, k;
+		cin >> n >> k;
+		for (int i = 0; i < n; ++i) {
+			vec.push_back(i);
+		}
+
+		segmentTree.resize(n * 4);
+		buildRecursive(ROOT, 0, n - 1);
+
+		for (int i = 0; i < k; ++i) {
+			int Q;
+			cin >> Q;
+			//DVD 바꿔낌
+			if (Q == 0) {
+				int A, B;
+				cin >> A >> B;
+				updateRecursive(A, vec[B], ROOT, 0, n - 1);
+				updateRecursive(B, vec[A], ROOT, 0, n - 1);
+				swap(vec[A], vec[B]);
+			}
+			//DVD 가져옴
+			else {
+				int r, l;
+				cin >> r >> l;
+				pair<int, int> queryResult = queryRecursive(ROOT, 0, n - 1, r, l);
+				if ((queryResult.first == l) && (queryResult.second == r))
+					cout << "YES\n";
+				else
+					cout << "NO\n";
+			}
+
+		}
 	}
-
-	//LIS 계산
-	//LIS[i]: A[i]부터 시작하는 LIS의 최대 길이
-	vector<int> LIS(n, 0);
-
-	//vec[n]: 숫자 n으로 끝나는 LIS의 최대 길이
-	//n1 ~ n2 구간에 해당하는 segmentTree 값: vec[n1] ~ vec[n2] 중 최댓값 저장 
-	segmentTree.resize(VEC_SIZE * 4);
-	buildRecursive(ROOT, 0, VEC_SIZE - 1);
-
-	for (int i = 0; i < n; ++i) {
-		LIS[i] = queryRecursive(ROOT, 0, VEC_SIZE - 1, 0, A[i] - 1) + 1;
-		updateRecursive(A[i], LIS[i], ROOT, 0, VEC_SIZE - 1);
-	}
-
-	cout << segmentTree[ROOT];
 	return 0;
 }
