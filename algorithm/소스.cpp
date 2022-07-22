fc9a935d61814aede6959d30de00e01b66284b2f
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-22 15:57:58 +0900

    백준 1065 한수
    복습

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,42 +1,38 @@
+#include <iostream>
 #include<algorithm>
 #include <vector>
-#include <iostream>
 using namespace std;
-
-typedef long long ll;
-
-int n;
-ll m;
-vector<ll> trees;
-
-//높이를 h로 지정했을 때 m미터 이상의 나무를 가져갈 수 있는가
-bool check(ll h) {
-	ll takeTree = 0;
-	for (int i = 0; i < n; ++i) {
-		if (trees[i] > h) takeTree += (trees[i] - h);
-	}
-	return takeTree >= m;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i) {
-		ll input;
-		cin >> input;
-		trees.push_back(input);
+	int n;
+	cin >> n;
+
+	int cnt = 0;
+	for (int x = 1; x <= n; ++x) {
+		vector<int> digit;
+		int tmp = x;
+		while (tmp != 0) {
+			digit.push_back(tmp % 10);
+			tmp /= 10;
+		}
+		//등차수열 이루는지 검사
+		if (digit.size() < 3) cnt++;
+		else {
+			bool flag = true;
+			int sub = digit[0] - digit[1];
+			for (int i = 0; i < digit.size() -1; ++i) {
+				if ((digit[i] - digit[i + 1]) != sub) {
+					flag = false;
+					break;
+				}
+			}
+			if (flag) cnt++;
+		}
 	}
 
-	ll high = 1000000000;
-	ll low = 0LL;
-	while (low < high) {
-		ll mid = (high + low + 1) / 2;
-		if (check(mid)) low = mid;
-		else high = mid - 1;
-	}
-
-	cout << high;
+	cout << cnt;
 	return 0;
 }
