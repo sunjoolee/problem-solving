eb88b4bc5e3d030b382806a27bd88f72c18e5882
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-24 16:03:20 +0900

    백준 2961 도영이가 만든 맛있는 음식

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
-#include<algorithm>
+#include <algorithm>
 #include <vector>
+#include <math.h>
+#include <iostream>
 using namespace std;
 
 int main() {
@@ -8,31 +9,38 @@ int main() {
 	cin.tie(NULL); cout.tie(NULL);
 
 	int n;
-	cin >> n;
 
-	int cnt = 0;
-	for (int x = 1; x <= n; ++x) {
-		vector<int> digit;
-		int tmp = x;
-		while (tmp != 0) {
-			digit.push_back(tmp % 10);
-			tmp /= 10;
-		}
-		//등차수열 이루는지 검사
-		if (digit.size() < 3) cnt++;
-		else {
-			bool flag = true;
-			int sub = digit[0] - digit[1];
-			for (int i = 0; i < digit.size() -1; ++i) {
-				if ((digit[i] - digit[i + 1]) != sub) {
-					flag = false;
-					break;
-				}
-			}
-			if (flag) cnt++;
-		}
+	vector<int> vecS;
+	vector<int> vecB;
+	
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		int inputS, inputB;
+		cin >> inputS >> inputB;
+		vecS.push_back(inputS);
+		vecB.push_back(inputB);
 	}
 
-	cout << cnt;
+	int s; //요리의 신맛(곱)
+	int b; //요리의 쓴맛 (합)
+	int ans = 1000000000;
+	
+	int fullSet = (1<<n) - 1; 
+	for (int set = 1; set <= fullSet; set++) {
+		s = -1;
+		b = -1;
+		for (int food = 0; food < n; ++food) {
+			if (set & (1 << food)) {
+				if (s == -1) s = vecS[food];
+				else s *= vecS[food];
+
+				if (b == -1) b = vecB[food];
+				else b += vecB[food];
+			}
+		}
+		ans = min(ans, abs(s - b));
+	}
+
+	cout << ans;
 	return 0;
 }
