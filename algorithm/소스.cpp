da6a090fe031a871a52efab9b47ec9a38502303e
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-07-24 17:15:20 +0900

    백준 9663 N-Queen

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,47 +1,48 @@
 #include <algorithm>
-#include <vector>
 #include <iostream>
 using namespace std;
+
+int n;
+int board[15][15] = { 0 };
+
+int col[15] = { 0 };
+int dialog1[30] = { 0 };
+int dialog2[30] = { 0 };
+
+int cnt = 0;
+
+void nQueen(int curRow) {
+	if (curRow == n) {
+		cnt++;
+		return;
+	}
+
+	for (int curCol = 0; curCol < n; ++curCol) {
+		if (col[curCol]) continue;
+		if (dialog1[curRow - curCol + n -1]) continue;
+		if (dialog2[curRow + curCol]) continue;
+
+		col[curCol] = 1;
+		dialog1[curRow - curCol + n - 1] = 1;
+		dialog2[curRow + curCol] = 1;
+
+		nQueen(curRow + 1);
+
+		col[curCol] = 0;
+		dialog1[curRow - curCol + n - 1] = 0;
+		dialog2[curRow + curCol] = 0;
+	}
+	return;
+}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int n;
 	cin >> n;
-
-	//row[i]: i번째 사람의 키
-	vector<int> row; 
 	
-	//cntVec[x]: 키가 x인 사람의 왼쪽에 x보다 키카 큰 사람이 몇명 있는지
-	vector<int> cntVec; 
-	
-	for (int i = 0; i < n; ++i) {
-		int input;
-		cin >> input;
-		cntVec.push_back(input);
-		//row 초기화
-		row.push_back(i);
-	}
-
-	do {
-		bool correctRow = true;
-		for (int i = 0; i < n; ++i) {
-			int cnt = 0;
-			for (int left = 0; left < i; ++left) {
-				if (row[left] > row[i]) cnt++;
-			}
-			if (cntVec[row[i]] != cnt) {
-				correctRow = false;
-				break;
-			}
-		}
-		if (correctRow) break;
-	} while (next_permutation(row.begin(), row.end()));
-
-	for (int i = 0; i < n; ++i) {
-		cout << row[i] + 1<< " ";
-	}
+	nQueen(0);
+	cout << cnt;
 
 	return 0;
 }
