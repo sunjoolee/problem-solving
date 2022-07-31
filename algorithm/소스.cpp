c69d57523c203c952a3a7774de7227c9ae986f3f
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-08-01 06:30:14 +0900

    백준 2798 블랙잭
    복습

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -3,111 +3,39 @@
 #include <iostream>
 using namespace std;
 
-vector<vector<int>> board(9, vector<int>(9, 0));
-
-vector<vector<bool>> rowCheck(9, vector<bool>(10, false));
-vector<vector<bool>> colCheck(9, vector<bool>(10, false));
-
-//스도투 판 9등분 
-//0 1 2
-//3 4 5
-//6 7 8
-vector<vector<bool>> blockCheck(9, vector<bool>(10, false));
-
-int blockR[9] = { 0, 0, 0, 3, 3, 3, 6, 6, 6 }; 
-int blockC[9] = { 0, 3, 6, 0, 3, 6, 0, 3, 6 };
-
-bool solved() {
-	for (int i = 0; i < 9; ++i) {
-		for (int num = 1; num <= 9; ++num) {
-			if (rowCheck[i][num] == false) return false;
-			if (colCheck[i][num] == false) return false;
-			if (blockCheck[i][num] == false) return false;
-		}
-	}
-	return true;
-}
-
-void solve() {
-	//basecase
-	if (solved()) {
-		for (int i = 0; i < 9; ++i) {
-			for (int j = 0; j < 9; ++j) {
-				cout << board[i][j] << " ";
-			}
-			cout << "\n";
-		}
-		exit(0);
-	}
-
-	for (int i = 0; i < 9; ++i) {
-		for (int j = 0; j < 9; ++j) {
-			cout << board[i][j] << " ";
-		}
-		cout << "\n";
-	}
-	cout << "------------\n";
-
-	for (int blockNum = 0; blockNum < 9; ++blockNum) {
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				int r = blockR[blockNum] + i;
-				int c = blockC[blockNum] + j;
-
-				//스도쿠 빈칸인 경우 가능한 모든 숫자 삽입
-				if (board[r][c] == 0) {
-					for (int num = 1; num <= 9; ++num) {
-							if (rowCheck[r][num]) continue;
-							if (colCheck[c][num]) continue;
-							if (blockCheck[blockNum][num]) continue;
-
-							rowCheck[r][num] = true;
-							colCheck[c][num] = true;
-							blockCheck[blockNum][num] = true;
-
-							board[r][c] = num;
-							solve();
-							board[r][c] = 0;
-
-							rowCheck[r][num] = false;
-							colCheck[c][num] = false;
-							blockCheck[blockNum][num] = false;
-					}
-					return;
-				}
-			}
-		}
-	}
-}
-
-
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	for (int i = 0; i < 9; ++i) {
-		for (int j = 0; j < 9; ++j) {
-			int input;
-			cin >> input;
-			board[i][j] = input;
-
-			rowCheck[i][input] = true;
-			colCheck[j][input] = true;
-		}
+	int N, M;
+	cin >> N >> M;
+	
+	vector<int> card;
+	for (int i = 0; i < N; ++i) {
+		int input;
+		cin >> input;
+		card.push_back(input);
 	}
 
-	for (int blockNum = 0; blockNum < 9; ++blockNum) {
-		for (int i = 0; i < 3; ++i) {
-			for (int j = 0; j < 3; ++j) {
-				int r = blockR[blockNum] + i;
-				int c = blockC[blockNum] + j;
-
-				blockCheck[blockNum][board[r][c]] = true;
+	vector<int> sum;
+	for (int i = 0; i < N - 2; ++i) {
+		for (int j = i + 1; j < N - 1; ++j) {
+			for (int k = j + 1; k < N; ++k) {
+				sum.push_back(card[i] + card[j] + card[k]);
 			}
 		}
 	}
+	sort(sum.begin(), sum.end());
 
-	solve();
+	//M을 넘지 않으면서 M에 최대한 가까운 카드3장의 합
+	int left = 0;
+	int right = sum.size() - 1;
+	while (left < right) {
+		int mid = (left + right + 1) / 2;
+		if (sum[mid] > M) right = mid - 1;
+		else left = mid;
+	}
 
+	cout << sum[right];
 	return 0;
 }
