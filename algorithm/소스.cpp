2d1636bfc5f3bdcb55dac03a8000a4c998a5d947
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-08-01 15:43:39 +0900

    백준 1062 가르침
    비트마스크

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,70 +1,74 @@
 #include <algorithm>
-#include <vector>
 #include <string>
+#include <vector>
 #include <iostream>
 using namespace std;
 
-typedef long long ll;
-
-int k;
-vector<char> sign;
-vector<int> arr;
-bool num[10] = { 0 };
-
-string maxArr = "0";
-string minArr = "9876543210";
-
-//k+1자리 배열이 k개의 부등호 만족하는지 검사
-bool check() {
-	for (int i = 0; i < k; ++i) {
-		if ((sign[i] == '<')&&(arr[i] > arr[i + 1]))
-			return false;
-		if((sign[i] == '>') && (arr[i] < arr[i + 1]))
-			return false;
-	}
-	return true;
-}
-
-void solve (int digit) {
-	//k+1 자리 배열 생성 완료
-	if (digit == k + 1) {
-		if (check()) {
-			string curArr = "";
-			for (int i = 0; i < k + 1; ++i) {
-				curArr += to_string(arr[i]);
-			}
-
-			maxArr = max(maxArr, curArr);
-			minArr = min(minArr, curArr);
-		}
-		return;
-	}
-
-	for (int i = 0; i <= 9; ++i) {
-		if (num[i]) continue;
-		num[i] = true;
-		arr.push_back(i);
-		solve(digit + 1);
-		arr.pop_back();
-		num[i] = false;
-	}
+int bitCount(long long x) {
+	if (x == 0) return 0;
+	return (x % 2) + bitCount(x / 2);
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	cin >> k;
+	int n, k;
+	cin >> n >> k;
 
-	for (int i = 0; i < k; ++i) {
-		char input;
-		cin >> input;
-		sign.push_back(input);
+	//남극의 모든 단어 anta ~ tica
+	//최소 5개의 알파벳 필요 (a, c, i, n, t)
+	if (k < 5) {
+		cout << "0";
+		return 0;
+	}
+	//26개의 알파벳을 모두 배운 경우 모든 단어 읽을 수 있음
+	if (k == 26) {
+		cout << n;
+		return 0;
 	}
 
-	solve(0);
+	//남극의 단어들이 각각 사용하는 알파벳 비트마스크 저장
+	vector<long long> wordsAlpha;
+	//long long maxUsedAlpha = 0LL;
 
-	cout << maxArr << "\n";
-	cout << minArr << "\n";
+	for (int i = 0; i < n; ++i) {
+		string input;
+		cin >> input;
+
+		long long usedAlpha = 0LL;
+		for (int i = 0; i < input.length(); ++i) {
+			usedAlpha |= (1 << (input[i] - 'a'));
+		}
+		//maxUsedAlpha = max(maxUsedAlpha, usedAlpha);
+
+		wordsAlpha.push_back(usedAlpha);
+	}
+
+	
+	//꼭 필요한 5개의 글자
+	long long acint = 0LL;
+	acint |= (1 << ('a' - 'a'));
+	acint |= (1 << ('c' - 'a'));
+	acint |= (1 << ('i' - 'a'));
+	acint |= (1 << ('n' - 'a'));
+	acint |= (1 << ('t' - 'a'));
+
+	//배운 알파벳 비트마스크
+	long long learnAlpha;
+
+	int maxCnt = 0;
+	for (learnAlpha = acint; learnAlpha <= (1<<26); ++learnAlpha) {
+		if ((learnAlpha & acint) != acint) continue;
+		if (bitCount(learnAlpha) > k) continue;
+		
+		int cnt = 0;
+		for (int i = 0; i < n; ++i) {
+			if((learnAlpha & wordsAlpha[i]) == wordsAlpha[i]) cnt++;
+		}
+		maxCnt = max(maxCnt, cnt);
+	}
+
+	cout << maxCnt;
 	return 0;
 }
