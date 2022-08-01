e3b5fe8851c4ab37c93ed1185c20392578f05250
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-08-02 00:16:40 +0900

    백준 1207 종이 자르기

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,74 +1,167 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <set>
 #include <iostream>
 using namespace std;
 
-int bitCount(long long x) {
-	if (x == 0) return 0;
-	return (x % 2) + bitCount(x / 2);
+typedef long long ll;
+
+int L;
+vector<string> board;
+
+bool usedBLock[5] = { 0 };
+vector<vector<string>> block(5, vector<string>());
+
+set<string> visited;
+
+string minAns;
+
+bool inRange(int r, int c) {
+	if (r < 0 || r >= L) return false;
+	if (c < 0 || c >= L) return false;
+	return true;
+}
+
+bool check(int startR, int startC, int blockNum) {
+	for (int i = 0; i < block[blockNum].size(); ++i) {
+		for (int j = 0; j < block[blockNum][i].size(); ++j) {
+			if (!inRange(startR + i, startC + j)) return false;
+
+			if ((block[blockNum][i][j] == '#') && (board[startR + i][startC + j] != '0')) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+void fill(int startR, int startC, int blockNum) {
+	for (int i = 0; i < block[blockNum].size(); ++i) {
+		for (int j = 0; j < block[blockNum][i].size(); ++j) {
+			if (block[blockNum][i][j] == '#')
+				board[startR + i][startC + j] = (char)(blockNum + 1 + '0');
+		}
+	}
+}
+
+void empty(int startR, int startC, int blockNum) {
+	for (int i = 0; i < block[blockNum].size(); ++i) {
+		for (int j = 0; j < block[blockNum][i].size(); ++j) {
+			if (block[blockNum][i][j] == '#')
+				board[startR + i][startC + j] = '0';
+		}
+	}
+}
+
+bool solved() {
+	//블럭 5개 모두 사용하지 않은 경우 답 X
+	for (int i = 0; i < 5; ++i) {
+		if (!usedBLock[i]) return false;
+	}
+
+	for (int i = 0; i < L; ++i) {
+		for (int j = 0; j < L; ++j) {
+			if (board[i][j] == '0') return false;
+		}
+	}
+	return true;
+}
+
+
+void solve() {
+	string curBoard = "";
+	for (int i = 0; i < L; ++i) {
+		curBoard += board[i];
+	}
+
+	//base case
+	if (solved()) {
+		minAns = min(minAns, curBoard);
+		return;
+	}
+
+	if (visited.find(curBoard) != visited.end()) return;
+	visited.insert(curBoard);
+
+	if (curBoard > minAns) return;
+
+	for (int r = 0; r < L; ++r) {
+		for (int c = 0; c < L; ++c) {
+			if (board[r][c] == '0') {
+				for (int blockNum = 0; blockNum < 5; ++blockNum) {
+					if (usedBLock[blockNum]) continue;
+
+					int startC;
+					for (int i = 0; i < block[blockNum][0].size(); ++i) {
+						if (block[blockNum][0][i] == '#') {
+							startC = c - i;
+							break;
+						}
+					}
+
+					if (!check(r, startC, blockNum)) continue;
+
+					usedBLock[blockNum] = true;
+					fill(r, startC, blockNum);
+
+					string nextBoard = "";
+					for (int i = 0; i < L; ++i) {
+						nextBoard += board[i];
+					}
+
+					if ((visited.find(nextBoard) == visited.end()) && (nextBoard < minAns))
+						solve();
+
+					usedBLock[blockNum] = false;
+					empty(r, startC, blockNum);
+				}
+				break;
+			}
+		}
+	}
 }
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-	int n, k;
-	cin >> n >> k;
+	cin >> L;
 
-	//남극의 모든 단어 anta ~ tica
-	//최소 5개의 알파벳 필요 (a, c, i, n, t)
-	if (k < 5) {
-		cout << "0";
-		return 0;
-	}
-	//26개의 알파벳을 모두 배운 경우 모든 단어 읽을 수 있음
-	if (k == 26) {
-		cout << n;
-		return 0;
-	}
-
-	//남극의 단어들이 각각 사용하는 알파벳 비트마스크 저장
-	vector<long long> wordsAlpha;
-	//long long maxUsedAlpha = 0LL;
-
-	for (int i = 0; i < n; ++i) {
-		string input;
-		cin >> input;
-
-		long long usedAlpha = 0LL;
-		for (int i = 0; i < input.length(); ++i) {
-			usedAlpha |= (1 << (input[i] - 'a'));
+	//board 초기화
+	for (int i = 0; i < L; ++i) {
+		board.push_back("");
+		for (int j = 0; j < L; ++j) {
+			board[i] += '0';
 		}
-		//maxUsedAlpha = max(maxUsedAlpha, usedAlpha);
-
-		wordsAlpha.push_back(usedAlpha);
 	}
 
-	
-	//꼭 필요한 5개의 글자
-	long long acint = 0LL;
-	acint |= (1 << ('a' - 'a'));
-	acint |= (1 << ('c' - 'a'));
-	acint |= (1 << ('i' - 'a'));
-	acint |= (1 << ('n' - 'a'));
-	acint |= (1 << ('t' - 'a'));
+	//minAns 초기화
+	minAns = "";
+	string noAns = "";
+	for (int i = 0; i < (L*L); ++i) {
+		minAns += '6';
+		noAns += '6';
+	}
 
-	//배운 알파벳 비트마스크
-	long long learnAlpha;
-
-	int maxCnt = 0;
-	for (learnAlpha = acint; learnAlpha <= (1<<26); ++learnAlpha) {
-		if ((learnAlpha & acint) != acint) continue;
-		if (bitCount(learnAlpha) > k) continue;
-		
-		int cnt = 0;
-		for (int i = 0; i < n; ++i) {
-			if((learnAlpha & wordsAlpha[i]) == wordsAlpha[i]) cnt++;
+	for (int i = 0; i < 5; ++i) {
+		int n, m;
+		cin >> n >> m;
+		for (int j = 0; j < n; ++j) {
+			string input;
+			cin >> input;
+			block[i].push_back(input);
 		}
-		maxCnt = max(maxCnt, cnt);
 	}
 
-	cout << maxCnt;
+	solve();
+
+	if (minAns == noAns) cout << "gg";
+	else {
+		for (int i = 0; i < (L*L); i += L) {
+			cout << minAns.substr(i, L) << "\n";
+		}
+	}
+
 	return 0;
 }
