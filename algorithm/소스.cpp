eabd2aa1cb0c15fc42447561baa9b6f8b0a94838
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-08-02 19:52:23 +0900

    백준 18115 카드 놓기

--- algorithm/소스.cpp
+++ algorithm/소스.cpp
@@ -1,17 +1,8 @@
 #include <algorithm>
 #include <vector>
+#include <queue>
 #include <iostream>
 using namespace std;
-
-bool cmp(pair<int, int> a, pair<int, int> b) {
-	//회의 끝나는 시간 빠른 순으로 정렬
-	if(a.second < b.second) return true;
-	//회의 끝나는 시간 같은 경우
-	//회의 시작하는 시간 빠른 순으로 정렬
-	if (a.second == b.second) return a.first < b.first;
-
-	return false;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -20,22 +11,39 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<pair<int, int>> meeting;
-	for (int i = 0; i < n; ++i) {
-		int start, end;
-		cin >> start >> end;
-		meeting.push_back({ start, end });
-	}
-	sort(meeting.begin(), meeting.end(), cmp);
+	vector<int> A;
 
-	int cnt = 0;
-	int meetingEnd = 0;
 	for (int i = 0; i < n; ++i) {
-		if (meetingEnd <= meeting[i].first) {
-			cnt++;
-			meetingEnd = meeting[i].second;
-		}
+		int input;
+		cin >> input;
+		A.push_back(input);
 	}
-	cout << cnt;
+	
+	deque <int> card;
+	int num = 1;
+	for (int i = n - 1; i >= 0; --i) {
+		if (A[i] == 1) {
+			card.push_front(num++);
+		}
+		else if (A[i] == 2) {
+			if (card.empty()) {
+				card.push_front(num++);
+			}
+			else {
+				int firstCard = card.front();
+				card.pop_front();
+				card.push_front(num++);
+				card.push_front(firstCard);
+			}
+		}
+		else if (A[i] == 3) {
+		card.push_back(num++);
+		}
+		
+	}
+
+	for (int i = 0; i < n; ++i) {
+		cout << card[i] << " ";
+	}
 	return 0;
 }
